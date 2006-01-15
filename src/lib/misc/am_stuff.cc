d42417d018d2b95d63d432d4edc359631a41f779
Robert J. Harrison <rjharrison@gmail.com>
2006-01-15 01:03:41 +0000

    forgot to check this in with rest of initial AM stuff

---
+++ src/lib/misc/am_stuff.cc
@@ -0,0 +1,21 @@
+
+
+#include <misc/communicator.h>
+
+namespace madness {
+  static long _am_nchild_registered=0;
+
+  void am_barrier_handler(Communicator& comm, ProcessID proc, const AMArg& arg) {
+    _am_nchild_registered++;
+  };
+
+  long am_barrier_nchild_registered() {
+    return _am_nchild_registered;
+  };
+
+  void am_barrier_zero_nchild_registered() {
+    _am_nchild_registered = 0;
+  };
+}
+
+
