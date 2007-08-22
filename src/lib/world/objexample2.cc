6ca46344f32f23c93555228b727d8760d2d667fc
Robert J. Harrison <rjharrison@gmail.com>
2007-08-22 23:49:30 +0000

    another worldobj example

---
+++ src/lib/world/objexample2.cc
@@ -0,0 +1,33 @@
+#define WORLD_INSTANTIATE_STATIC_TEMPLATES
+#include <world/world.h>
+
+using namespace madness;
+
+class Foo : public WorldObject<Foo> {
+    const int bar;
+public:
+    Foo(World& world, int bar) 
+        : WorldObject<Foo>(world), bar(bar) {process_pending();};
+
+    int get() const {return bar;};
+};
+
+
+int main(int argc, char** argv) {
+    MPI::Init(argc, argv);
+    madness::World world(MPI::COMM_WORLD);
+
+    Foo a(world,world.rank()), b(world,world.rank()*10);
+
+    for (ProcessID p=0; p<world.size(); p++) {
+        Future<int> futa = a.send(p,&Foo::get);
+        Future<int> futb = b.send(p,&Foo::get);
+        // Could work here until the results are available
+        MADNESS_ASSERT(futa.get() == p);
+        MADNESS_ASSERT(futb.get() == p*10);
+    }
+    world.gop.fence();
+    if (world.rank() == 0) print("OK!");
+
+    MPI::Finalize();
+}
