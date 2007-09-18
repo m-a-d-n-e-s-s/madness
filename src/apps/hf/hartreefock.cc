4751f97615fb119556d4e5f0fb67ea5b2ba3fd19
Scott Thornton <wsttiger@gmail.com>
2007-09-18 18:48:57 +0000

    further work on hartree fock stuff

--- src/apps/hf/hartreefock.cc
+++ src/apps/hf/hartreefock.cc
@@ -4,7 +4,7 @@ namespace madness
 {
 
   //***************************************************************************
-  HartreeFock::HartreeFock(World& world, functorT V, std::vector<funcT> phis)
+  HartreeFock::HartreeFock(World& world, funcT V, std::vector<funcT> phis)
     : _world(world), _V(V), _phis(phis)
   {
     ones = functorT(new OnesFunctor());
@@ -19,9 +19,43 @@ namespace madness
   //***************************************************************************
   
   //***************************************************************************
-  void HartreeFock::hartree_fock()
+  void HartreeFock::hartree_fock(int maxits)
   {
-  
+    for (int it = 0; it < maxits; it++)
+    {
+      for (std::vector<funcT>::iterator pi = _phis.begin();
+            pi != _phis.end(); ++pi)
+      {
+        // Get psi from collection
+        funcT psi = (*pi);
+        // Calculate nuclear contribution
+        funcT pnuclear = _V*psi;
+        // Calculate the Coulomb contribution to the Fock operator (J)
+        funcT pcoulomb = calculate_coulomb(psi);
+        // Calculate the Exchange contribution to the Fock operator (K)
+        funcT pexchange = calculate_exchange(psi);
+        // Get new wavefunction
+        //funcT pfunc = pnuclear + 2.0 * pcoulomb - pexchange;
+        funcT pfunc = FunctionFactory<double,3>(_world).functor(zeros);
+        // Create the free-particle Green's function operator
+        
+        // Apply the Green's function operator (stubbed)
+        funcT tmp = FunctionFactory<double,3>(_world).functor(zeros);
+        // (Not sure whether we have to do this mask thing or not!)
+        for (std::vector<funcT>::iterator pj = _phis.begin();
+              pj != pi; ++pj)
+        {
+          // Project out the lower states
+          if (pi != pj)
+          {
+            // Get other wavefunction
+            funcT psij = (*pj);
+            double overlap = 0.0;
+            //tmp -= overlap*psij;
+          }
+        }
+      }
+    }
   }
   //***************************************************************************
 
@@ -40,7 +74,7 @@ namespace madness
         funcT& phij = (*pj);
         // Compute the density
         funcT prod = phij*phij;
-        // Transform Coulomb operator into a function
+        // Transform Coulomb operator into a function (stubbed)
         funcT Vc = FunctionFactory<double,3>(_world).functor(zeros);
         // Note that we are not using psi
         // The density is built from all of the wavefunctions. The contribution
@@ -71,8 +105,8 @@ namespace madness
           funcT& phij = (*pj);
           // NOTE that psi is involved in this calculation
           funcT prod = phij*psi;
-          // Transform Coulomb operator into a function
-          funcT Vex = FunctionFactory<double,3>(_world).functor(zeros);;
+          // Transform Coulomb operator into a function (stubbed)
+          funcT Vex = FunctionFactory<double,3>(_world).functor(zeros);
           // NOTE that the index is j.
           //rfunc += Vex*phij;
         }
