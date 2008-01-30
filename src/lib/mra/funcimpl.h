fa5fc85d8a147f4f0654d457318a0bcfbc78b2db
Scott Thornton <wsttiger@gmail.com>
2008-01-30 22:29:17 +0000

    Fixed part of trace_local(). Multiplied by the sqrt(cell_volume). Only works for compressed!

--- src/lib/mra/funcimpl.h
+++ src/lib/mra/funcimpl.h
@@ -2102,7 +2102,7 @@ namespace madness {
                     if (node.has_coeff()) sum += node.coeff()(v0);
                 }
             }
-            return sum;
+            return sum*sqrt(cell_volume);
         }
 
 
