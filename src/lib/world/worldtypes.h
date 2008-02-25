7692e8c42d1353114d132c131454c817d37ec40e
Robert J. Harrison <rjharrison@gmail.com>
2008-02-25 17:20:39 +0000

    missing file

---
+++ src/lib/world/worldtypes.h
@@ -0,0 +1,10 @@
+#ifndef WORLD_TYPES_H
+#define WORLD_TYPES_H
+
+
+typedef int ProcessID; ///< Used to clearly identify process number/rank
+typedef int Tag;       ///< Used to clearly identify message tag/type
+
+
+#endif
+
