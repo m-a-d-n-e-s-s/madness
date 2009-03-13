1401aea7ae868f889c3fe99f22dd58e22d324c32
Scott Thornton <wsttiger@gmail.com>
2009-03-13 22:19:53 +0000

    added ability to refine to a point in simulation space - delta function

--- src/lib/mra/indexit.h
+++ src/lib/mra/indexit.h
@@ -7,52 +7,90 @@
 #include <vector>
 #include <world/print.h>
 
-namespace madness {
+namespace madness
+{
 
     /// Facilitates iteration through a multidimension index space
-    class IndexIterator {
+    class IndexIterator
+    {
     private:
         std::vector<long> n; ///< User specified upper limits for each dimension
         std::vector<long> i; ///< Current index
         bool finished;
-        
+
     public:
-        template <typename V>
-        IndexIterator(const V& limits) : n(limits.size()), i(limits.size(),0), finished(false) 
+        /// Iterates dimension d from 0 to limts[d]-1 inclusive
+        template<typename V>
+            IndexIterator(const V& limits) :
+                n(limits.size()), i(limits.size(), 0), finished(false)
+            {
+                for (unsigned int d = 0; d < n.size(); d++)
+                    n[d] = limits[d];
+            }
+
+            /// Iterates dimension d from 0 to limts[d]-1 inclusive
+            IndexIterator(int ndim, const long limits[]) :
+            n(ndim), i(ndim, 0), finished(false)
         {
-            for (unsigned int d=0; d<n.size(); d++) n[d] = limits[d];
+            for (unsigned int d = 0; d < n.size(); d++)
+                n[d] = limits[d];
         }
 
-        IndexIterator(int ndim, const long limits[]) : n(ndim), i(ndim,0), finished(false)
+        /// Iterates all dimensions from 0 to top-1 inclusive
+        IndexIterator(int ndim, long top) :
+             n(ndim,top), i(ndim, 0), finished(false)
+         {
+         }
+
+        IndexIterator&
+        reset()
         {
-            for (unsigned int d=0; d<n.size(); d++) n[d] = limits[d];
+            for (unsigned int d = 0; d < n.size(); d++)
+                i[d] = 0;
+            finished = false;
+            return *this;
         }
 
-	IndexIterator& reset() {
-		for (unsigned int d=0; d<n.size(); d++) i[d] = 0;
-		finished = false;
-		return *this;
-	}
-     
-        long operator[](int d) const {MADNESS_ASSERT(!finished); return i[d];}
-        
-        const std::vector<long>& operator*() const {MADNESS_ASSERT(!finished); return i;}
-        
-        operator bool() const {return !finished;}
-        
-        IndexIterator& operator++() {
-            for (int d=n.size()-1; d>=0; d--) {
+        long
+        operator[](int d) const
+        {
+            MADNESS_ASSERT(!finished);
+            return i[d];
+        }
+
+        const std::vector<long>&
+        operator*() const
+        {
+            MADNESS_ASSERT(!finished);
+            return i;
+        }
+
+        operator bool() const
+        {
+            return !finished;
+        }
+
+        IndexIterator&
+        operator++()
+        {
+            for (int d = n.size() - 1; d >= 0; d--)
+            {
                 i[d]++;
-                if (i[d] < n[d]) return *this;
-                else i[d] = 0;
+                if (i[d] < n[d])
+                    return *this;
+                else
+                    i[d] = 0;
             }
             finished = true;
             return *this;
         }
 
-        static void test() {
-            Vector<int,4> n(3);
-            for (IndexIterator it(n); it; ++it) {
+        static void
+        test()
+        {
+            Vector<int, 4> n(3);
+            for (IndexIterator it(n); it; ++it)
+            {
                 print(*it);
             }
         }
