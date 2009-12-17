1f5032ff02fe28d6eb864c21b0d991af7c9789d6
Robert J. Harrison <rjharrison@gmail.com>
2009-12-17 05:37:49 +0000

    initial BGP port

--- src/lib/world/atomicint.h
+++ src/lib/world/atomicint.h
@@ -5,10 +5,9 @@
 
 /// \brief Implements AtomicInteger
 
-namespace madness {
-    
-#define MADATOMIC_USE_X86_ASM
-    //#define MADATOMIC_USE_GCC
+//#define MADATOMIC_USE_X86_ASM
+//#define MADATOMIC_USE_GCC
+#define MADATOMIC_USE_BGP
     
 #ifdef MADATOMIC_USE_GCC
 #  ifdef GCC_ATOMICS_IN_BITS
@@ -18,9 +17,15 @@ namespace madness {
 #  endif
 #endif
     
+#ifdef MADATOMIC_USE_BGP
+#  include <bpcore/bgp_atomic_ops.h>
+#endif
+    
 #ifdef MADATOMIC_USE_AIX
 #  include <sys/atomic_op.h>
 #endif
+    
+namespace madness {
     
     /// An integer with atomic set, get, read+inc, read+dec, dec+test operations
     
@@ -29,26 +34,37 @@ namespace madness {
     /// Conciously modeled after the TBB API to prepare for switching to it.
     class AtomicInt {
     private:
+
+#ifdef MADATOMIC_USE_BGP
+        typedef _BGP_Atomic atomic_int;
+#else
         typedef volatile int atomic_int;
-        
+#endif
         atomic_int value;
-        int exchange_and_add(atomic_int* p, int i) {
+
+        inline int exchange_and_add(int i) {
 #ifdef MADATOMIC_USE_GCC
-            return __gnu_cxx::__exchange_and_add(p,i);
+            return __gnu_cxx::__exchange_and_add(&value,i);
 #elif defined(MADATOMIC_USE_X86_ASM)
-            __asm__ __volatile__("lock; xaddl %0,%1" :"=r"(i) : "m"(*p), "0"(i));
+            __asm__ __volatile__("lock; xaddl %0,%1" :"=r"(i) : "m"(value), "0"(i));
             return i;
 #elif defined(MADATOMIC_USE_AIX)
-            return fetch_and_add(p,i);
+            return fetch_and_add(&value,i);
+#elif defined(MADATOMIC_USE_BGP)
+            return _bgp_fetch_and_add(&value,i);
 #else 
-            error ... atomic exchange_and_add operator must be implemented for this platform;
+#error ... atomic exchange_and_add operator must be implemented for this platform;
 #endif
         }
-        
+
     public:
         /// Returns the value of the counter with fence ensuring subsequent operations are not moved before the load
         operator int() const volatile {
-            int result = value;
+#if defined(MADATOMIC_USE_BGP)
+            int result = value.atom;
+#else
+	    int result = value;
+#endif
             __asm__ __volatile__ ("" : : : "memory");
             return result;
         }
@@ -56,8 +72,12 @@ namespace madness {
         /// Sets the value of the counter with fence ensuring preceding operations are not moved after the store
         int operator=(int other) {
             __asm__ __volatile__ ("" : : : "memory");
-            value = other;
-            return other;
+#if defined(MADATOMIC_USE_BGP)
+            value.atom = other;
+#else
+	    value = other;
+#endif
+	    return other;
         }
         
         /// Sets the value of the counter with fences ensuring operations are not moved either side of the load+store
@@ -68,12 +88,12 @@ namespace madness {
         
         /// Decrements the counter and returns the original value
         int operator--(int) {
-            return exchange_and_add(&value, -1);
+            return exchange_and_add(-1);
         }
         
         /// Increments the counter and returns the original value
         int operator++(int) {
-            return exchange_and_add(&value,  1);
+            return exchange_and_add(1);
         }
         
         /// Decrements the counter and returns true if the new value is zero
