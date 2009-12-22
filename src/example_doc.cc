2724a0d53594b4b8d0040dbb3ca0e5fd066581e9
Robert J. Harrison <rjharrison@gmail.com>
2009-12-22 20:26:52 +0000

    examples of doxygen documentation

---
+++ src/example_doc.cc
@@ -0,0 +1,11 @@
+
+/// \file src/example_doc.cc
+/// \brief Illustrates how to group documentation between files
+/// \ingroup exampledoc
+
+/// Brief documentation on class member coming from example_doc.cc
+
+/// Full documentation on class member coming from example_doc.cc
+/// @param[in] mary Mary's current bank balance.
+void ExampleClass2::fred(double mary);
+
