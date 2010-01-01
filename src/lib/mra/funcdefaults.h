43be8024cd7e7b08b58a9a5a33f3828374d137e8
Robert J. Harrison <rjharrison@gmail.com>
2010-01-01 20:04:41 +0000

    documentation and added redistribute member

--- src/lib/mra/funcdefaults.h
+++ src/lib/mra/funcdefaults.h
@@ -35,6 +35,7 @@
 
 /// \file funcdefaults.h
 /// \brief Provides FunctionDefaults and utilities for coordinate transformation
+/// \ingroup mra
 
 #include <world/world.h>
 #include <world/array.h>
@@ -60,6 +61,7 @@ namespace madness {
     ///
     /// N.B.  Ultimately, we may need to make these defaults specific to each
     /// world, as should be all global state.
+    /// \ingroup mra
     template <int NDIM>
     class FunctionDefaults {
     private:
@@ -96,131 +98,210 @@ namespace madness {
         /// Used to set defaults to k=7, thresh=1-5, for a unit cube [0,1].
         static void set_defaults(World& world);
 
+        /// Returns the default wavelet order
         static int get_k() {
             return k;
-        };
+        }
+
+        /// Sets the default wavelet order
+
+        /// Existing functions are unaffacted.
         static void set_k(int value) {
             k=value;
             MADNESS_ASSERT(k>0 && k<=MAXK);
         }
 
+        /// Returns the default threshold
         static double get_thresh() {
             return thresh;
         }
+
+        /// Sets the default threshold
+
+        /// Existing functions are unaffected
         static void set_thresh(double value) {
             thresh=value;
         }
 
+        /// Returns the default initial projection level
         static int get_initial_level() {
             return initial_level;
         }
+
+        /// Sets the default initial projection level
+
+        /// Existing functions are unaffected
         static void set_initial_level(int value) {
             initial_level=value;
             MADNESS_ASSERT(value>0 && value<MAXLEVEL);
         }
 
+        /// Gets the default maximum adaptive refinement level
         static int get_max_refine_level() {
             return max_refine_level;
         }
+
+        /// Sets the default maximum adaptive refinement level
+
+        /// Existing functions are unaffected
         static void set_max_refine_level(int value) {
             max_refine_level=value;
             MADNESS_ASSERT(value>0 && value<MAXLEVEL);
         }
 
+        /// Gets the default truncation mode
         static int get_truncate_mode() {
             return truncate_mode;
         }
+
+        /// Sets the default truncation mode
+
+        /// Existing functions are unaffected
         static void set_truncate_mode(int value) {
             truncate_mode=value;
             MADNESS_ASSERT(value>=0 && value<3);
         }
 
+        /// Gets the default adaptive refinement flag
         static bool get_refine() {
             return refine;
         }
+        
+        /// Sets the default adaptive refinement flag
+
+        /// Existing functions are unaffected
         static void set_refine(bool value) {
             refine=value;
         }
 
+        /// Gets the default adaptive autorefinement flag
         static bool get_autorefine() {
             return autorefine;
         }
+
+        /// Sets the default adaptive autorefinement flag
+
+        /// Existing functions are unaffected
         static void set_autorefine(bool value) {
             autorefine=value;
         }
 
+        /// Gets the default debug flag (is this used anymore?)
         static bool get_debug() {
             return debug;
         }
+
+        /// Sets the default debug flag (is this used anymore?)
+
+        /// Not sure if this does anything useful
         static void set_debug(bool value) {
             debug=value;
         }
 
+        /// Gets the default truncate on project flag
         static bool get_truncate_on_project() {
             return truncate_on_project;
         }
+
+        /// Sets the default truncate on project flag
+
+        /// Existing functions are unaffected
         static void set_truncate_on_project(bool value) {
             truncate_on_project=value;
         }
 
+        /// Gets the random load balancing for integral operators flag
         static bool get_apply_randomize() {
             return apply_randomize;
         }
+
+        /// Sets the random load balancing for integral operators flag
         static void set_apply_randomize(bool value) {
             apply_randomize=value;
         }
 
+
+        /// Gets the random load balancing for projection flag
         static bool get_project_randomize() {
             return project_randomize;
         }
+
+        /// Sets the random load balancing for projection flag
         static void set_project_randomize(bool value) {
             project_randomize=value;
         }
 
+        /// Soon to meet the dust
         static const Tensor<int>& get_bc() {
             return bc;
         }
+
+        /// Soon to meet the dust
         static void set_bc(const Tensor<int>& value) {
             bc=copy(value);
             MADNESS_ASSERT(bc.dim(0)==NDIM && bc.dim(1)==2 && bc.ndim()==2);
         }
 
+        /// Gets the user cell for the simulation
         static const Tensor<double>& get_cell() {
             return cell;
         }
+
+        /// Gets the user cell for the simulation
+
+        /// Existing functions are probably rendered useless
         static void set_cell(const Tensor<double>& value) {
             cell=copy(value);
             recompute_cell_info();
         }
+
+        /// Sets the user cell to be cubic with each dimension having range \c [lo,hi]
+
+        /// Existing functions are probably rendered useless
         static void set_cubic_cell(double lo, double hi) {
             cell(_,0)=lo;
             cell(_,1)=hi;
             recompute_cell_info();
         }
 
+        /// Returns the width of each user cell dimension
         static const Tensor<double>& get_cell_width() {
             return cell_width;
         }
 
+        /// Returns the reciprocal of the width of each user cell dimension
         static const Tensor<double>& get_rcell_width() {
             return rcell_width;
         }
 
-        static double get_cell_volume() {
-            return cell_volume;
-        }
-
+        /// Returns the minimum width of any user cell dimension
         static double get_cell_min_width() {
             return cell_min_width;
         }
 
+        /// Returns the volume of the user cell
+        static double get_cell_volume() {
+            return cell_volume;
+        }
+
+        /// Returns the default process map
         static SharedPtr< WorldDCPmapInterface< Key<NDIM> > >& get_pmap() {
             return pmap;
         }
 
+        /// Sets the default process map (does \em not redistribute existing functions)
+
+        /// Existing functions are probably rendered useless
         static void set_pmap(const SharedPtr< WorldDCPmapInterface< Key<NDIM> > >& value) {
             pmap=value;
         }
+
+        /// Sets the default process map and redistributes all functions using the old map
+        static void redistribute(World& world, const SharedPtr< WorldDCPmapInterface< Key<NDIM> > >& newpmap) {
+            pmap->redistribute(world,newpmap);
+            pmap = newpmap;
+        }
+
     };
 
 
