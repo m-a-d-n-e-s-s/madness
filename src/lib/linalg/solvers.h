b9b74ef8c03ac79829d04fe5de5f38230c949d60
Robert J. Harrison <rjharrison@gmail.com>
2010-01-05 21:15:37 +0000

    cleaned up documentation

--- src/lib/linalg/solvers.h
+++ src/lib/linalg/solvers.h
@@ -47,33 +47,52 @@
 namespace madness {
 
     /*!
-      Solves the KAIN equations for coefficients to compute the next vector
-      
+      \brief Solves non-linear equation using KAIN (returns coefficients to compute next vector)
+
       \ingroup solvers
 
-      \verbatim
-      Wish to solve f(x)=0 with x and f(x) vectors 1..n
-      
-      Define (with i,j referring to the Krylov subspace and m the current iteration):
-      
-      Q(i,j) = <xi|fj>
-      A(i,j) = <xi-xm | fj-fm> = Q(i,j) - Q(m,j) - Q(i,m) + Q(m,m)
-      b(i) =-<xi-xm | fm> = -Q(i,m) + Q(m,m)
-      
-      We solve the equation
-      
+      The Krylov-accelerated inexact-Newton method employs directional
+      derivatives to estimate the Jacobian in the subspace and
+      separately computes updates in the subspace and its complement.
+
+      We wish to solve the non-linear equations \f$ f(x)=0 \f$ where
+      \f$ f \f$ and \f$ x \f$ are vectors of the same dimension (e.g.,
+      consider both being MADNESS functions).  
+
+      Define the following matrices and vector (with \f$ i \f$ and \f$
+      j \f$ denoting previous iterations in the Krylov subspace and
+      \f$ m \f$ the current iteration):
+      \f{eqnarray*}{
+      Q_{i j} & = & \langle x_i \mid f_j \rangle \\
+      A_{i j} & = & \langle x_i - x_m \mid f_j - f_m \rangle = Q_{i j} - Q_{m j} - Q_{i m} + Q{m m} \\
+      b_i & =& -\langle x_i - x_m \mid f_m \rangle = -Q_{i m} + Q_{m m} 
+      \f}
+      The subspace equation is of dimension \f$ m \f$ (assuming iterations 
+      are indexed from zero) and is given by
+      \f[
       A c = b
-      
-      and the correction to vector m
-      
-      . C
-      .   interior = sum(i<m)[ c(i)*(x(i)-x(m)) ] = sum(i<m)[c(i)*x(i)] - x[m]*sum(i<m)[c(i)]
-      .   exterior = -f(m) - sum(i<m)[ c(i)*(f(i)-f(m)) ] = -f(m) - sum(i<m)[c(i)*f(i)] + f(m)*sum(i<m)[c(i)]
-      . New vector
-      .   define C = sum(i<m)(c(i))  (note less than)
-      .   define c(m) = 1.0 - C
-      .   xnew = sum(i<=m) [ c(i)*(x(i) - f(i)) ]
-      \endverbatim
+      \f]
+      The interior and exterior updates may be combined into one simple expression
+      as follows. First, define an additional element of the solution vector
+      \f[
+      c_m = 1 - \sum_{i<m} c_i
+      \f]
+      and then the new vector (guess for next iteration) is given by
+      \f[
+      x_{m+1} = \sum_{i \le m}{c_i ( x_i - f_i)}
+      \f]
+
+      To employ the solver, each iteration
+      -# Compute the additional row and column of the matrix \f$ Q \f$
+      that is the inner product between solution vectors (\f$ x_i \f$) and residuals
+      (\f$ f_j \f$).
+      -# Call this routine to compute the coefficients \f$ c \f$ and from these 
+      compute the next solution vector
+      -# Employ step restriction or line search as necessary to ensure stable/robust solution.
+
+      @param[in] Q The matrix of inner products between subspace vectors and residuals.
+      @param[in] rcond Threshold for discarding small singular values in the subspace equations.
+      @return Vector for computing next solution vector
     */
     template <typename T>
     Tensor<T> KAIN(const Tensor<T>& Q, double rcond=1e-12) {
@@ -127,11 +146,11 @@ namespace madness {
 
     /// \ingroup solvers
     struct SolverTargetInterface {
-        /// Should return true if the Jacobian is implemented
-        virtual bool provides_jacobian() const = 0;
-
         /// Should return the resdiual (vector F(x)) 
         virtual Tensor<double> residual(const Tensor<double>& x) = 0;
+
+        /// Override this to return \c true if the Jacobian is implemented
+        virtual bool provides_jacobian() const {return false;}
 
         /// Some solvers require the jacobian or are faster if an analytic form is available
         
@@ -151,17 +170,15 @@ namespace madness {
     };
 
 
-
     /// The interface to be provided by functions to be optimized
 
     /// \ingroup solvers
     struct OptimizationTargetInterface {
-
-        /// Should return true if the gradient is implemented
-        virtual bool provides_gradient() const = 0;
-
         /// Should return the value of the objective function
         virtual double value(const Tensor<double>& x) = 0;
+
+        /// Override this to return true if the derivative is implemented
+        virtual bool provides_gradient() const {return false;}
 
         /// Should return the derivative of the function
         virtual Tensor<double> gradient(const Tensor<double>& x) {
@@ -176,7 +193,6 @@ namespace madness {
             gradient = this->gradient(x);
         }
 
-
         /// Numerical test of the derivative ... optionally prints to stdout, returns max abs error
         double test_gradient(Tensor<double>& x, double value_precision, bool doprint=true);
 
@@ -184,7 +200,7 @@ namespace madness {
     };
 
 
-    /// The interface to be provided by solvers
+    /// The interface to be provided by solvers ... NOT USED ANYWHERE?
 
     /// \ingroup solvers
     struct SolverInterface {
@@ -206,7 +222,7 @@ namespace madness {
     };
 
 
-    /// Optimization via steepest descent
+    /// Unconstrained minimization via steepest descent
 
     /// \ingroup solvers
     class SteepestDescent : public OptimizerInterface {
@@ -238,7 +254,7 @@ namespace madness {
     /// Optimization via quasi-Newton (BFGS or SR1 update)
 
     /// \ingroup solvers
-    /// This is presently not 
+    /// This is presently not a low memory algorithm ... we really need one!
     class QuasiNewton : public OptimizerInterface {
     private:
         std::string update;              // One of BFGS or SR1
@@ -265,20 +281,32 @@ namespace madness {
                     double tol = 1e-6,
                     double value_precision = 1e-12,
                     double gradient_precision = 1e-12);
-        
+
+        /// Choose update method (currently only "BFGS" or "SR1")
         void set_update(const std::string& method);
 
-        bool optimize(Tensor<double>& x);
+        /// Runs the optimizer
 
+        /// @return True if converged
+        bool optimize(Tensor<double>& x, int maxiter=20);
+
+        /// After running the optimizer returns true if converged
+
+        /// @return True if converged
         bool converged() const;
 
+        /// Value of objective function
+
+        /// @return Value of objective function
         double value() const;
 
+        /// Value of gradient norm
+
+        /// @return Norm of gradient of objective function
         double gradient_norm() const;
 
 	virtual ~QuasiNewton() {}
     };
-
 }
 
 #endif // MADNESS_LINALG_SOLVERS_H__INCLUDED
