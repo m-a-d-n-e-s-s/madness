380cc2e44df975b70efaa5155002f3eb3453592c
Nick Vence <nickvence@gmail.com>
2010-12-21 01:41:46 +0000

    spawnNLM.py doesn't include 100

--- src/apps/nick/spawnNLM.py
+++ src/apps/nick/spawnNLM.py
@@ -2,6 +2,6 @@
 import sys
 if(len(sys.argv) != 2):
     sys.exit("Requires nMAX as an argument");
-for n in range(0,int(sys.argv[1])+1):
+for n in range(2,int(sys.argv[1])+1):
     for l in range (0, n):
         print n, l, "0"
