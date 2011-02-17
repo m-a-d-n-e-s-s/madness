8f8c453e12f303ec968c081a0313c68573779591
Eduard Valeyev <Edward.Valeev@gmail.com>
2011-02-17 20:30:11 +0000

    synced up interface to gemm BLAS functions used in cblas.cc with their declaration in cblas.h and other uses (e.g. in tensor/mtxmq.h).

--- src/lib/linalg/cblas.cc
+++ src/lib/linalg/cblas.cc
@@ -38,8 +38,8 @@
 
 #include <linalg/cblas.h>
 
-typedef std::complex<float> float_complex;
-typedef std::complex<double> double_complex;
+typedef std::complex<float> complex_float;
+typedef std::complex<double> complex_double;
 
 extern "C" void xerbla_(char *message, integer *info, int length) {
     std::fprintf(stderr,
@@ -50,24 +50,6 @@ extern "C" void xerbla_(char *message, integer *info, int length) {
 
 /// \file cblas.cc
 /// \brief This file provides gemm template BLAS.
-
-extern "C" void dgemm_(const char *opa, const char *opb, const long *m, const long *n, const long *k,
-                           const double *alpha, const double *a, const long *lda, const double *b, const long *ldb,
-                           const double *beta, double *c, const long *ldc);
-
-extern "C" void sgemm_(const char *opa, const char *opb, const long *m, const long *n, const long *k,
-                           const float *alpha, const float *a, const long *lda, const float *b, const long *ldb,
-                           const float *beta, float *c, const long *ldc);
-
-extern "C" void zgemm_(const char *opa, const char *opb, const long *m, const long *n, const long *k,
-                           const double_complex *alpha,
-                           const double_complex *a, const long *lda, const double_complex *b, const long *ldb,
-                           const double_complex *beta, double_complex *c, const long *ldc);
-
-extern "C" void cgemm_(const char *opa, const char *opb, const long *m, const long *n, const long *k,
-                           const float_complex *alpha,
-                           const float_complex *a, const long *lda, const float_complex *b, const long *ldb,
-                           const float_complex *beta, float_complex *c, const long *ldc);
 
 namespace madness {
 
@@ -81,7 +63,8 @@ namespace madness {
                            };
         dgemm_(op[transa], op[transb], &m, &n, &k,
                &alpha, a, &lda, b, &ldb,
-               &beta,  c, &ldc);
+               &beta,  c, &ldc,
+               1, 1);
     }
 
     template <> void gemm<float> (bool transa, bool transb,
@@ -93,31 +76,36 @@ namespace madness {
                            };
         sgemm_(op[transa], op[transb], &m, &n, &k,
                &alpha, a, &lda, b, &ldb,
-               &beta,  c, &ldc);
+               &beta,  c, &ldc,
+               1, 1);
     }
 
-    template <> void gemm<double_complex> (bool transa, bool transb,
+    // complex_double == complex_real8
+    template <> void gemm<complex_double> (bool transa, bool transb,
                                            integer m, integer n, integer k,
-                                           double_complex alpha, const double_complex* a, integer lda,
-                                           const double_complex* b, integer ldb,
-                                           double_complex beta, double_complex* c, integer ldc) {
+                                           complex_double alpha, const complex_double* a, integer lda,
+                                           const complex_double* b, integer ldb,
+                                           complex_double beta, complex_double* c, integer ldc) {
         const char *op[] = {"n","t"
                            };
         zgemm_(op[transa], op[transb], &m, &n, &k,
                &alpha, a, &lda, b, &ldb,
-               &beta,  c, &ldc);
+               &beta,  c, &ldc,
+               1, 1);
     }
 
-    template <> void gemm<float_complex> (bool transa, bool transb,
+    // complex_float == complex_real4
+    template <> void gemm<complex_float> (bool transa, bool transb,
                                           integer m, integer n, integer k,
-                                          float_complex alpha, const float_complex* a, integer lda,
-                                          const float_complex* b, integer ldb,
-                                          float_complex beta, float_complex* c, integer ldc) {
+                                          complex_float alpha, const complex_float* a, integer lda,
+                                          const complex_float* b, integer ldb,
+                                          complex_float beta, complex_float* c, integer ldc) {
         const char *op[] = {"n","t"
                            };
         cgemm_(op[transa], op[transb], &m, &n, &k,
                &alpha, a, &lda, b, &ldb,
-               &beta,  c, &ldc);
+               &beta,  c, &ldc,
+               1, 1);
     }
 
 }
