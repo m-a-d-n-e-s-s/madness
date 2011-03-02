a0a3ae289498154dd53f408db6b39589b069580d
Justus Calvin <justus.c79@gmail.com>
2011-03-02 14:47:27 +0000

    Moved implementation of textfsar.h into textfsar.cc

--- src/lib/world/textfsar.h
+++ src/lib/world/textfsar.h
@@ -40,6 +40,7 @@
 /// \brief Implements archive wrapping text file stream
 
 #include <fstream>
+#include <cstring>
 #include <world/archive.h>
 
 namespace madness {
@@ -51,9 +52,10 @@ namespace madness {
 
         public:
             TextFstreamOutputArchive(const char* filename = 0,
-                                     std::ios_base::openmode mode=std::ios_base::binary |
-                                                                  std::ios_base::out | std::ios_base::trunc) {
-                if (filename) open(filename, mode);
+                    std::ios_base::openmode mode=std::ios_base::binary | std::ios_base::out | std::ios_base::trunc)
+            {
+                if (filename)
+                    open(filename, mode);
             }
 
             template <class T>
@@ -74,74 +76,32 @@ namespace madness {
             }
 
             template <class T>
-            typename madness::enable_if< madness::is_serializable<T>, void >::type
+            typename madness::enable_if< madness::is_serializable<T> >::type
             store(const T* t, long n) const {
-                for (long i=0; i<n; ++i) os << t[i] << std::endl;
+                for (long i=0; i<n; ++i)
+                    os << t[i] << std::endl;
             }
 
-            void store(const char* t, long /*n*/) const {
-                // store character string, escaping &, < and > along the way
-                while (*t) {
-                    char c = *t++;
-                    if (c == '\\') {
-                        os.put('\\');
-                        os.put('\\');
-                    }
-                    else if (c == '<') {
-                        os.put('\\');
-                        os.put('l');
-                    }
-                    else if (c == '>') {
-                        os.put('\\');
-                        os.put('r');
-                    }
-                    else {
-                        os.put(c);
-                    }
-                }
-                os << std::endl;
-            };
+            void store(const char* t, long /*n*/) const;
 
             void store(const unsigned char* t, long n) const {
-                for (long i=0; i<n; ++i) os << (unsigned int) t[i] << std::endl;
+                for (long i=0; i<n; ++i)
+                    os << (unsigned int) t[i] << std::endl;
             }
 
-            void open(const char* filename,
-                      std::ios_base::openmode mode = std::ios_base::out |
-                                                     std::ios_base::trunc) {
-                os.open(filename, mode);
-                os.setf(std::ios::scientific);
-                os.precision(17);
-                char tag[256];
-                os << "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>" << std::endl;
-                sprintf(tag,"<archive major_version=\"%d\" minor_version=\"%d\">",
-                        ARCHIVE_MAJOR_VERSION, ARCHIVE_MINOR_VERSION);
-                os << tag << std::endl;
-                os << "<typemap>" << std::endl;
-                for (int i=0; i<256; ++i) {
-                    sprintf(tag,"%d \"%s\"",i,archive_type_names[i]);
-                    store(tag,strlen(tag)); // Must use store to escape characters
-                }
-                os << "</typemap>" << std::endl;
-            };
+            void open(const char* filename, std::ios_base::openmode mode = std::ios_base::out | std::ios_base::trunc);
 
-
-            void close() {
-                if (os.is_open()) {
-                    os << "</archive>" << std::endl;
-                    os.close();
-                }
-            };
-
+            void close();
 
             void flush() {
-                if (os.is_open()) os.flush();
-            };
+                if (os.is_open())
+                    os.flush();
+            }
 
             ~TextFstreamOutputArchive() {
                 close();
-            };
-        };
+            }
+        }; // class TextFstreamOutputArchive
 
 
         /// Wraps an archive around a text file stream for input
@@ -150,17 +110,14 @@ namespace madness {
             mutable std::ifstream is;
 
             // Eat EOL after each entry to enable char-by-char read of strings
-            void eat_eol() const {
-                char eol;
-                is.get(eol);
-                if (eol != '\n')
-                    MADNESS_EXCEPTION("TextFstreamInputArchive: eat_eol: indigestion", static_cast<int>(eol));
-            };
+            void eat_eol() const;
 
         public:
             TextFstreamInputArchive(const char* filename = 0,
-                                    std::ios_base::openmode mode = std::ios_base::in) {
-                if (filename) open(filename, mode);
+                    std::ios_base::openmode mode = std::ios_base::in)
+            {
+                if (filename)
+                    open(filename, mode);
             }
 
             template <class T>
@@ -184,99 +141,48 @@ namespace madness {
             }
 
             template <class T>
-            inline
-            void check_end_tag() const {
+            inline void check_end_tag() const {
                 check_start_tag<T>(true);
             }
 
             template <class T>
-            typename madness::enable_if< madness::is_serializable<T>, void >::type
+            typename madness::enable_if< madness::is_serializable<T> >::type
             load(T* t, long n) const {
                 for (long i=0; i<n; ++i) is >> t[i];
                 eat_eol();
             }
 
-            void load(unsigned char* t, long n) const {
-                for (long i=0; i<n; ++i) {
-                    unsigned int x;
-                    is >> x;
-                    t[i] = (unsigned char) x;
-                }
-                eat_eol();
-            };
+            void load(unsigned char* t, long n) const;
 
-            void load(char* t, long n) const {
-                for (long i=0; i<n; ++i) {
-                    char c0;
-                    is.get(c0);
-                    if (c0 == '\\') {
-                        char c1;
-                        is.get(c1);
-                        if (c1 == '\\') {
-                            t[i] = '\\';
-                        }
-                        else if (c1 == 'l') {
-                            t[i] = '<';
-                        }
-                        else if (c1 == 'r') {
-                            t[i] = '>';
-                        }
-                        else {
-                          MADNESS_EXCEPTION("TextFstreamInputArchive: malformed string?", static_cast<int>(c1));
-                        }
-                    }
-                    else {
-                        t[i] = c0;
-                    }
-                }
-                eat_eol();
-            };
+            void load(char* t, long n) const;
 
             void open(const char* filename,
-                      std::ios_base::openmode mode = std::ios_base::in) {
-                is.open(filename, mode);
-                char buf[256];
-                is.getline(buf,256);		// skip xml header
-                is.getline(buf,256);
-
-                char tag[256];
-                sprintf(tag,"<archive major_version=\"%d\" minor_version=\"%d\">",
-                        ARCHIVE_MAJOR_VERSION, ARCHIVE_MINOR_VERSION);
-                if (strcmp(buf,tag)) {
-                    std::cout << "TextFstreamInputArchive: not an archive/bad version?" << std::endl;
-                    std::cout << "Found this: " << buf;
-                    std::cout << "Expected  : " << tag;
-                    MADNESS_EXCEPTION("TextFstreamInputArchive: not an archive/bad version?", 1);
-                }
-
-                // For now just skip over typemap
-                for (int i=0; i<258; ++i) is.getline(buf,256);
-            };
+                      std::ios_base::openmode mode = std::ios_base::in);
 
             void close() {
                 is.close();
-            };
-        };
+            }
+        }; // class TextFstreamInputArchive
 
         template <class T>
         struct ArchivePrePostImpl<TextFstreamOutputArchive,T> {
             static void preamble_store(const TextFstreamOutputArchive& ar) {
                 ar.store_start_tag<T>();
-            };
+            }
             static inline void postamble_store(const TextFstreamOutputArchive& ar) {
                 ar.store_end_tag<T>();
-            };
-        };
+            }
+        }; // struct ArchivePrePostImpl<TextFstreamOutputArchive,T>
 
         template <class T>
         struct ArchivePrePostImpl<TextFstreamInputArchive,T> {
             static inline void preamble_load(const TextFstreamInputArchive& ar) {
                 ar.check_start_tag<T>();
-            };
+            }
             static inline void postamble_load(const TextFstreamInputArchive& ar) {
                 ar.check_end_tag<T>();
-            };
-        };
+            }
+        }; // struct ArchivePrePostImpl<TextFstreamInputArchive,T>
     }
 }
 
