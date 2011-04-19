d40472d0fb8111949cc178490061321ce166c6d5
Nick Vence <nickvence@gmail.com>
2011-04-19 20:11:15 +0000

    input2 now expected for projPsi

--- src/apps/nick/projPsi.cc
+++ src/apps/nick/projPsi.cc
@@ -184,7 +184,7 @@ void projectL(World& world, const double L, const int wf, const int n, const int
             if (i==0 || i==(n-1)) ifEndPti = 0.5;
             const double r = i*dr + 1e-10;
             const complexd YlPsir = YlPsi(i,l);
-            P(l) += real(YlPsir*conj(YlPsir))r*r*dr*ifEndPti;            
+            P(l) += real(YlPsir*conj(YlPsir))r*r*dr*ifEndPti;
             if(printR) PRINT( real(YlPsir) << "\t");
         }
         PRINTLINE("");
@@ -481,15 +481,14 @@ void loadParameters(World& world, double& thresh, int& kMAD, double& L, double &
                 ///      v  = sqrt(2 n omega - Z^2)
                 /// dMAX    = v tMAX
                 PRINTLINE( "omega = " << omega );
-                // Add logic based on the Keldysh parameter
-                while( 2*nPhoton*omega - Z*Z < 0.0 ) nPhoton++;
+                while( 2*(nPhoton*omega - Z*Z) < 0.0 ) nPhoton++; //just in case nPhoton is too small
                 PRINTLINE("nPhoton = " << nPhoton);
                 PRINTLINE("The following shoud be positive: 2*nPhoton*omega - Z*Z = " << 2*nPhoton*omega - Z*Z);
-                double dMAX = std::sqrt( 2*nPhoton*omega - Z*Z) * tMAX;
+                double dMAX = std::sqrt( 2*(nPhoton*omega - Z*Z)) * tMAX;
                 PRINTLINE("dMAX = " << dMAX );
                 if ( cutoff < dMAX ) cutoff = 0.0;
                 while( cutoff < dMAX ) {
-                    cutoff += L/32;
+                    cutoff += L/128;
                 }
                 PRINTLINE( "cutoff = " << cutoff );
             }
@@ -520,7 +519,7 @@ int main(int argc, char**argv) {
     int    wf        = 0;
     int    lMAX      = 0;
     loadParameters2(world, nGrid, th, phi, wf, kMomentum, lMAX, nPhoton);
-    loadParameters(world, thresh, kMAD, L, Z, nPhoton,  cutoff);
+    loadParameters(world, thresh, kMAD, L, Z, nPhoton, cutoff);
     FunctionDefaults<NDIM>::set_k(kMAD);                 // Wavelet order
     FunctionDefaults<NDIM>::set_thresh(thresh);       // Accuracy
     FunctionDefaults<NDIM>::set_cubic_cell(-L, L);
@@ -534,9 +533,9 @@ int main(int argc, char**argv) {
     try {
         std::vector<std::string> boundList;
         std::vector<std::string> unboundList;
-        //loadList(world, boundList, unboundList);
-        //projectPsi(world, boundList, unboundList, Z, cutoff);
-        projectL(world, L, wf, nGrid, lMAX, cutoff);
+        loadList(world, boundList, unboundList);
+        projectPsi(world, boundList, unboundList, Z, cutoff);
+        //projectL(world, L, wf, nGrid, lMAX, cutoff);
         //zSlice(world, n1, L, th, phi, wf);
         //testIntegral(world, L, Z, kMomentum);
         //debugSlice(world, n, L, Z, kMomentum);
