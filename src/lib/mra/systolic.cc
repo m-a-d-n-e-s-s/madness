3d7aede0bf193aced10f3563fbda87f8b5a40861
Jeff Hammond <jeff.science@gmail.com>
2012-01-15 02:11:08 +0000

    Jeff: changed Init to Init_thread because (1) Init was not defined in stubmpi.h and (2) MADNESS is a threaded code and it violates the MPI standard to initialize with Init, which is equivalent to MPI_THREAD_SINGLE

--- src/lib/mra/systolic.cc
+++ src/lib/mra/systolic.cc
@@ -617,7 +617,15 @@ namespace madness {
 using namespace madness;
 
 int main(int argc, char** argv) {
-    MPI::Init(argc, argv);
+#ifdef SERIALIZE_MPI
+    int required = MPI::THREAD_SERIALIZED;
+#else
+    int required = MPI::THREAD_MULTIPLE;
+#endif
+    int provided = MPI::Init_thread(argc, argv, required);
+    if (provided < required && MPI::COMM_WORLD.Get_rank() == 0) {
+        std::cout << "!! Warning: MPI::Init_thread did not provide requested functionality " << required << " " << provided << std::endl;
+    }
     madness::World world(MPI::COMM_WORLD);
 
     redirectio(world);
