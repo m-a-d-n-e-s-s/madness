f6af4f476c0df2f2cfa71a727efa1a9d8088ead8
Jeff Hammond <jeff.science@gmail.com>
2012-02-26 16:26:03 +0000

    modified tuning test so that it builds anywhere

--- src/lib/tensor/bgp_tuning/test_Zmtxmq.cc
+++ src/lib/tensor/bgp_tuning/test_Zmtxmq.cc
@@ -33,51 +33,51 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <cmath>
 #include <complex>
-
-//using namespace std;
 
 typedef std::complex<double> double_complex;
 
+#ifdef __bgp__
 extern "C" {
 #include "dcmf.h"
 }
-#include "mpi.h"
+#endif
 
+#include "rdtsc.h"
+
+#ifdef HPM
+#include <mpi.h>
 extern "C" void HPM_Init(void);           // initialize the UPC unit
 extern "C" void HPM_Start(char *label);   // start counting in a block marked by the label
 extern "C" void HPM_Stop(char *label);    // stop counting in a block marked by the label
 extern "C" void HPM_Print(void);          // print counters for all blocks
 extern "C" void HPM_Print_Flops(void);
+#endif
 
-extern "C" void zgemm(const char *transa, const char *transb,
-                   const int *m, const int *n, const int *k,
-                   const double_complex *alpha, const double_complex *a, const int *lda,
-                   const double_complex *b, const int *ldb, const double_complex *beta,
-                   double_complex *c, const int *ldc, int la, int lb);
+#define ZGEMM_FNAME zgemm_
+
+extern "C" void ZGEMM_FNAME(const char *transa, const char *transb, const int *m, const int *n, const int *k,
+                            const double_complex *alpha, const double_complex *a, const int *lda,
+                            const double_complex *b, const int *ldb, const double_complex *beta,
+                            double_complex *c, const int *ldc, int la, int lb);
 
 void mTxmq(long dimi, long dimj, long dimk, double_complex* c, const double_complex* a, const double_complex* b);
 void mTxm_tune(long dimi, long dimj, long dimk, double_complex* c, const double_complex* a, const double_complex* b);
 
 void mTxm_zgemm(long ni, long nj, long nk, double_complex* c, const double_complex* a, const double_complex*b ) {
-      int fni=ni;
-      int fnj=nj;
-      int fnk=nk;
+  int fni=ni;
+  int fnj=nj;
+  int fnk=nk;
   double_complex one=1.0;
-  zgemm("n","t",&fnj,&fni,&fnk,&one,b,&fnj,a,&fni,&one,c,&fnj,1,1);
+  ZGEMM_FNAME("n","t",&fnj,&fni,&fnk,&one,b,&fnj,a,&fni,&one,c,&fnj,1,1);
 }
 
 double_complex ran()
 {
   static unsigned long seed = 76521;
-
-  seed = seed*1812433253 + 12345;
-  double d1 = double(seed & 0x7fffffff)*4.6566128752458e-10;
-  seed = seed*1812433253 + 12345;
-  double d2 = double(seed & 0x7fffffff)*4.6566128752458e-10;
-
-  return double_complex(d1,d2);
+  seed = seed *1812433253 + 12345;
+  return ((double_complex) (seed & 0x7fffffff)) * 4.6566128752458e-10;
 }
 
 void ran_fill(int n, double_complex *a) {
@@ -96,18 +96,15 @@ void mTxm(long dimi, long dimj, long dimk,
     }
 }
 
-inline long long rdtsc() {
-  long long x = DCMF_Timebase();
-  return x;
-}
-
 void timer(const char* s, long ni, long nj, long nk, double_complex *a, double_complex *b, double_complex *c) {
   double fastest=0.0, fastest_zgemm=0.0, fastest_tune=0.0;
 
   double nflop = 2.0*ni*nj*nk;
   long loop;
 
+#ifdef HPM
   HPM_Start("mTxmq");
+#endif
   for (loop=0; loop<30; ++loop) {
     double rate;
     long long start = rdtsc();
@@ -116,9 +113,13 @@ void timer(const char* s, long ni, long nj, long nk, double_complex *a, double_c
     rate = nflop/start;
     if (rate > fastest) fastest = rate;
   }
+#ifdef HPM
   HPM_Stop("mTxmq");
+#endif
 
+#ifdef HPM
   HPM_Start("mTxmq_zgemm");
+#endif
   for (loop=0; loop<30; ++loop) {
     double rate;
     long long start = rdtsc();
@@ -127,9 +128,13 @@ void timer(const char* s, long ni, long nj, long nk, double_complex *a, double_c
     rate = nflop/start;
     if (rate > fastest_zgemm) fastest_zgemm = rate;
   }
+#ifdef HPM
   HPM_Stop("mTxmq_zgemm");
+#endif
 
-  HPM_Start("ZmTxmq_tune");
+#ifdef HPM
+  HPM_Start("mTxmq_tune");
+#endif
   for (loop=0; loop<30; ++loop) {
     double rate;
     long long start = rdtsc();
@@ -138,7 +143,9 @@ void timer(const char* s, long ni, long nj, long nk, double_complex *a, double_c
     rate = nflop/start;
     if (rate > fastest_tune) fastest_tune = rate;
   }
-  HPM_Stop("ZmTxmq_tune");
+#ifdef HPM
+  HPM_Stop("mTxmq_tune");
+#endif
 
   printf("%20s %3ld %3ld %3ld %8.2f %8.2f %8.2f\n",s, ni,nj,nk, fastest, fastest_zgemm, fastest_tune);
 }
@@ -149,7 +156,9 @@ void trantimer(const char* s, long ni, long nj, long nk, double_complex *a, doub
   double nflop = 3.0*2.0*ni*nj*nk;
   long loop;
 
+#ifdef HPM
   HPM_Start("mTxmq");
+#endif
   for (loop=0; loop<30; ++loop) {
     double rate;
     long long start = rdtsc();
@@ -160,9 +169,13 @@ void trantimer(const char* s, long ni, long nj, long nk, double_complex *a, doub
     rate = nflop/start;
     if (rate > fastest) fastest = rate;
   }
+#ifdef HPM
   HPM_Stop("mTxmq");
+#endif
 
+#ifdef HPM
   HPM_Start("mTxmq_zgemm");
+#endif
   for (loop=0; loop<30; ++loop) {
     double rate;
     long long start = rdtsc();
@@ -173,9 +186,13 @@ void trantimer(const char* s, long ni, long nj, long nk, double_complex *a, doub
     rate = nflop/start;
     if (rate > fastest_zgemm) fastest_zgemm = rate;
   }
+#ifdef HPM
   HPM_Stop("mTxmq_zgemm");
+#endif
 
-  HPM_Start("ZmTxmq_tune");
+#ifdef HPM
+  HPM_Start("mTxmq_tune");
+#endif
   for (loop=0; loop<30; ++loop) {
     double rate;
     long long start = rdtsc();
@@ -186,7 +203,9 @@ void trantimer(const char* s, long ni, long nj, long nk, double_complex *a, doub
     rate = nflop/start;
     if (rate > fastest_tune) fastest_tune = rate;
   }
-  HPM_Stop("ZmTxmq_tune");
+#ifdef HPM
+  HPM_Stop("mTxmq_tune");
+#endif
 
   printf("%20s %3ld %3ld %3ld %8.2f %8.2f %8.2f\n",s, ni,nj,nk, fastest, fastest_zgemm, fastest_tune);
 }
@@ -199,9 +218,10 @@ int main(int argc, char **argv) {
 
     double_complex *a, *b, *c, *d;
 
+#ifdef HPM
     MPI_Init(&argc, &argv);
-
     HPM_Init();
+#endif
 
     posix_memalign((void **) &a, 16, nkmax*nimax*sizeof(double_complex));
     posix_memalign((void **) &b, 16, nkmax*njmax*sizeof(double_complex));
@@ -210,7 +230,6 @@ int main(int argc, char **argv) {
 
     ran_fill(nkmax*nimax, a);
     ran_fill(nkmax*njmax, b);
-
 
 /*     ni = nj = nk = 2; */
 /*     for (i=0; i<ni*nj; ++i) d[i] = c[i] = 0.0; */
@@ -225,22 +244,22 @@ int main(int argc, char **argv) {
 /*     return 0; */
 
     printf("Starting to test Zmtxmq ... \n");
-    for (ni=1; ni<12; ni+=1) {
-        for (nj=1; nj<12; nj+=1) {
-            for (nk=1; nk<12; nk+=1) {
+    for (ni=2; ni<60; ni+=2) {
+        for (nj=2; nj<100; nj+=6) {
+            for (nk=2; nk<100; nk+=6) {
                 for (i=0; i<ni*nj; ++i) d[i] = c[i] = 0.0;
                 mTxm (ni,nj,nk,c,a,b);
                 mTxmq(ni,nj,nk,d,a,b);
                 for (i=0; i<ni*nj; ++i) {
-                    double err = abs(d[i]-c[i]);
+                    double err = std::abs(d[i]-c[i]);
                     /* This test is sensitive to the compilation options.
                        Be sure to have the reference code above compiled
                        -msse2 -fpmath=sse if using GCC.  Otherwise, to
                        pass the test you may need to change the threshold
                        to circa 1e-13.
                     */
-                    if (err > 2e-14) {
-                        printf("test_mtxmq: error %ld %ld %ld %e\n",ni,nj,nk,err);
+                    if (err > 1e-15) {
+                        printf("test_Zmtxmq: error %ld %ld %ld %e\n",ni,nj,nk,err);
                         exit(1);
                     }
                 }
@@ -250,14 +269,15 @@ int main(int argc, char **argv) {
     printf("... OK!\n");
 
     for (ni=2; ni<60; ni+=2) timer("(m*m)T*(m*m)", ni,ni,ni,a,b,c);
-    for (m=1; m<=30; m+=1) timer("(m*m,m)T*(m*m)", m*m,m,m,a,b,c);
-    for (m=1; m<=30; m+=1) trantimer("tran(m,m,m)", m*m,m,m,a,b,c);
-    for (m=1; m<=20; m+=1) timer("(20*20,20)T*(20,m)", 20*20,m,20,a,b,c);
+    for (m=2; m<=30; m+=2) timer("(m*m,m)T*(m*m)", m*m,m,m,a,b,c);
+    for (m=2; m<=30; m+=2) trantimer("tran(m,m,m)", m*m,m,m,a,b,c);
+    for (m=2; m<=20; m+=2) timer("(20*20,20)T*(20,m)", 20*20,m,20,a,b,c);
 
+#ifdef HPM
     HPM_Print();
     HPM_Print_Flops();
-
     MPI_Finalize();
+#endif
 
     return 0;
 }
