d4c21ba868bed6c3fe090f76037e2453f715a398
Robert J. Harrison <rjharrison@gmail.com>
2013-02-22 19:28:10 +0000

    sharing can be good

--- src/lib/mra/legendre.h
+++ src/lib/mra/legendre.h
@@ -43,6 +43,8 @@ namespace madness {
     extern void load_quadrature(World& world, const char* dir);
     extern void legendre_polynomials(double x, long order, double *p);
     extern void legendre_scaling_functions(double x, long k, double *p);
+    extern void initialize_legendre_stuff();
+
     extern bool gauss_legendre(int n, double xlo, double xhi, double *x, double *w);
     extern bool gauss_legendre_numeric(int n, double xlo, double xhi, double *x, double *w);
     extern bool gauss_legendre_test(bool print=false);
--- src/lib/mra/startup.cc
+++ src/lib/mra/startup.cc
@@ -109,9 +109,7 @@ namespace madness {
         load_quadrature(world, data_dir);
 
         // This to init static data while single threaded
-        double djunk[2];
-        legendre_scaling_functions(0.0,0,djunk);
-
+        initialize_legendre_stuff();
 	
         //if (world.rank() == 0) print("testing coeffs, etc.");
         MADNESS_ASSERT(gauss_legendre_test());
