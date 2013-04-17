59fed1e80ef745b7936f974108e1d35434c61b0b
Eduard Valeyev <Edward.Valeev@gmail.com>
2013-04-17 16:33:52 +0000

    Minor update to MADNESS manners in presense of others.

--- src/lib/world/world.cc
+++ src/lib/world/world.cc
@@ -168,7 +168,8 @@ namespace madness {
         ThreadPool::begin();        // Must have thread pool before any AM arrives
         RMI::begin();               // Must have RMI while still running single threaded
 
-        if (SafeMPI::COMM_WORLD.Get_rank() == 0) std::cout << "Runtime initialized with " << ThreadPool::size() << " threads in the pool and affinity " << sbind << "\n";
+        if (SafeMPI::COMM_WORLD.Get_rank() == 0)
+          std::cout << "MADNESS runtime initialized with " << ThreadPool::size() << " threads in the pool and affinity " << sbind << "\n";
 
 #ifdef HAVE_PAPI
         begin_papi_measurement();
