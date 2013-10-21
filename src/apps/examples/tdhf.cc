51a446f809b23419886da0bb5b3d037523d4727f
Florian Bischoff <3ru6ruWu@gmail.com>
2013-10-21 13:40:54 +0000

    rudimentary cis working with more than one electron pair

--- src/apps/examples/tdhf.cc
+++ src/apps/examples/tdhf.cc
@@ -62,10 +62,10 @@ using namespace madness;
 /// generalized gradient approximations,Ó Mol. Phys., vol. 103, no. 2, pp. 413Ð424, 2005.
 /// @param[in]		amo	the unperturbed orbitals
 /// @param[in]		omega	the lowest excitation energy
-/// @param[inout]	xp	the response function
+/// @param[inout]	x	the response function
 /// @param[in]		hf 	the HF reference object
 /// @param[in]	iteration	the current iteration
-double iterate_CCS(World& world, const vecfuncT& amo, double& omega, vecfuncT& xp,
+double iterate_CCS(World& world, const vecfuncT& amo, double& omega, vecfuncT& x,
 		HartreeFock& hf, const int iteration) {
 
 	// for convenience
@@ -79,25 +79,29 @@ double iterate_CCS(World& world, const vecfuncT& amo, double& omega, vecfuncT& x
     real_function_3d vlocal = hf.get_nuclear_potential() + hf.get_coulomb_potential();
 
     // make the potential for V0*xp
-	vecfuncT Vx=mul(world,vlocal,xp);
+	vecfuncT Vx=mul(world,vlocal,x);
 
 	// and the exchange potential is K xp
-	vecfuncT Kx=hf.get_calc().apply_hf_exchange(world,hf.get_calc().aocc,amo,xp);
+	vecfuncT Kx=hf.get_calc().apply_hf_exchange(world,hf.get_calc().aocc,amo,x);
 
 	// sum up: V0 xp = V_loc xp - K xp
 	vecfuncT V0=sub(world,Vx,Kx);
 
 	// now construct the two-electron contribution Gamma, cf Eqs. (7,8)
 	vecfuncT Gamma(nmo);
+	real_function_3d rhoprime=real_factory_3d(world);
+
+	// the Coulomb part Eq. (7) and Eq. (3)
+	for (int i=0; i<nmo; ++i) rhoprime+=x[i]*amo[i];
+	real_function_3d pp=2.0*(*poisson)(rhoprime);
+
 	for (int p=0; p<nmo; ++p) {
 
 		// the Coulomb part Eq. (7) and Eq. (3)
-		real_function_3d rhoprime=real_factory_3d(world);
-		for (int i=0; i<nmo; ++i) rhoprime+=xp[i]*amo[i];
-		Gamma[p]=2.0*(*poisson)(rhoprime)*amo[p];
+		Gamma[p]=pp*amo[p];
 
 		// the exchange part Eq. (8)
-		for (int i=0; i<nmo; ++i) Gamma[p]-=xp[i]*(*poisson)(amo[i]*amo[p]);
+		for (int i=0; i<nmo; ++i) Gamma[p]-=x[i]*(*poisson)(amo[i]*amo[p]);
 
 		// project out the zeroth-order density Eq. (4)
 		Gamma[p]-=rho0(Gamma[p]);
@@ -113,37 +117,43 @@ double iterate_CCS(World& world, const vecfuncT& amo, double& omega, vecfuncT& x
         double eps = hf.orbital_energy(p) + omega;
         if(eps > 0){
             if(world.rank() == 0) print("bsh: warning: positive eigenvalue", p, eps);
-            eps = -0.3;
+            eps = -0.03;
         }
         bsh[p] = poperatorT(BSHOperatorPtr3D(world, sqrt(-2.0 * eps),  lo, bsh_eps));
     }
     vecfuncT GVphi=apply(world,bsh,Vphi);
+
 
 	// update the excitation energy omega
 	double t1=0.0;
 	double t2=0.0;
 	for (int p=0; p<amo.size(); ++p) {
 		// remove factor 2 from Vphi coming from the BSH application
-		t1+=0.5*(inner(Vphi[p],GVphi[p]) - inner(Vphi[p],xp[p]));
+		t1+=0.5*(inner(Vphi[p],GVphi[p]) - inner(Vphi[p],x[p]));
 		double n=GVphi[p].norm2();
 		t2+=n*n;
 	}
 	double delta=-t1/t2;
 	// do some damping for the first few iterations
-	if (iteration<5) omega+=0.1*delta;
-	else if (iteration<10) omega+=0.3*delta;
-	else omega+=delta;
+	if (iteration<5) ;
+	else if (iteration<8) omega+=0.05*delta;
+	else if (iteration<10) omega+=0.1*delta;
+	else omega+=0.3*delta;
 	if (world.rank()==0) print("t1, t2, delta, omega",t1,t2,delta,omega);
 
 	// update the x vector: orthogonalize against the occupied space
-	for (int p=0; p<nmo; ++p) {
-		xp[p]=GVphi[p] - rho0(GVphi[p]);
+	for (int p=0; p<nmo; ++p) GVphi[p] -= rho0(GVphi[p]);
 
-		// Is there any reason to normalize the transition density?
-		// The results are identical either way.
-//		double norm=xp[p].norm2();
-//		xp[p].scale(1.0/norm);
+	// normalize the transition density for all
+	double norm2=0.0;
+	for (int p=0; p<nmo; ++p) {
+		x[p]=GVphi[p];
+		double norm=x[p].norm2();
+		norm2+=norm*norm;
+		std::cout << "norm(x_"<<p<<")  " << norm << std::endl;
 	}
+	for (int p=0; p<nmo; ++p) x[p].scale(1.0/sqrt(norm2));
+
 
 	return omega;
 }
@@ -159,6 +169,7 @@ int main(int argc, char** argv) {
     }
     startup(world,argc,argv);
     std::cout.precision(6);
+    typedef std::vector<functionT> vecfuncT;
 
     // take the HF orbitals to start
     const std::string input="input";
@@ -169,16 +180,22 @@ int main(int argc, char** argv) {
     	print("\n\n starting TDHF section \n");
     	print("nuclear repulsion: ", hf.get_calc().molecule.nuclear_repulsion_energy());
     	print("hf energy:         ", hf_energy);
+    	print("orbital energies:  ");
+    	for (int i=0; i<hf.get_calc().amo.size(); ++i) print("     ",hf.get_calc().aeps[i]);
     }
 
     // get a virtual orbital as a start guess for the transition density
     const int nmo=hf.get_calc().amo.size();
-    vecfuncT xp(1);
-    xp[0]=hf.get_calc().ao[nmo];
+    vecfuncT xp(nmo);
+    for (int i=0; i<nmo; ++i) xp[i]=real_factory_3d(world);
+//    for (int i=0; i<hf.get_calc().ao.size(); ++i) xp[0]+=hf.get_calc().ao[i];
+
+    // guess a HOMO-LUMO excitation
+    xp[nmo-1]=hf.get_calc().ao[nmo];
 
     // guess an excitation energy for the h2 molecule
-    double omega=0.4;
-    for (int iter=0; iter<100; ++iter) {
+    double omega=0.2;
+    for (int iter=0; iter<30; ++iter) {
     	iterate_CCS(world,hf.get_calc().amo,omega,xp,hf,iter);
     }
 
