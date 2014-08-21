2be6e6ce3d26fc2fe7a008307b295a4d0c48a0bb
Robert J. Harrison <rjharrison@gmail.com>
2014-08-21 09:00:02 -0400

    generic distributed sorting code

---
+++ src/madness/world/binsorter.h
@@ -0,0 +1,67 @@
+#include <madness/world/world.h>
+#include <madness/world/worldobj.h>
+#include <vector>
+
+namespace madness {
+
+    /// A parallel bin sort across MPI processes
+    template <typename T, typename inserterT>
+    class BinSorter : public WorldObject< BinSorter<T,inserterT> > {
+        World* pworld;
+        inserterT inserter;
+        const std::size_t bufsize;
+        std::vector<T>* bins;
+        
+        void flush(int owner) {
+            this->send(owner, &BinSorter<T,inserterT>::sorter,  bins[owner]);
+            bins[owner].clear();
+        }
+        
+        void sorter(const std::vector<T>& v) {
+            for(typename std::vector<T>::const_iterator it = v.begin(); it != v.end(); ++it) {
+                inserter(*it);
+            }
+        }
+        
+    public:
+        /// Constructs the sorter object 
+
+        /// @param[in] world The world object that must persist during the existence of this object
+        /// @param[in] inserter User provides this routine to process an item of data on remote end
+        /// @param[in] bufize Size of bin (in units of T)
+        BinSorter(World& world, inserterT inserter, int bufsize=1024)
+            : WorldObject<BinSorter>(world)
+            , pworld(&world)
+            , inserter(inserter)
+            , bufsize(bufsize)
+            , bins(new std::vector<T>[world.size()])
+        {
+            for (int i=0; i<world.size(); i++) {
+                bins[i].reserve(bufsize); // Maybe not a good idea on large process counts?
+            }
+            WorldObject< BinSorter<T,inserterT> >::process_pending();
+        }
+        
+        virtual ~BinSorter() {
+            finish();
+            delete [] bins;
+        }
+        
+        /// Invoke to complete the sort, flush all buffers, and ensure communication/processing is complete
+        void finish() {
+            for (int i=0; i<pworld->size(); i++) {
+                flush(i);
+            }
+            pworld->gop.fence();
+        }
+        
+        /// Application calls this to add a value to the bin for process owner
+        void insert(ProcessID p, const T& value) {
+            bins[p].push_back(value);
+            if (bins[p].size() >= bufsize) flush(p);
+        }
+    };
+}
+      
+    
+
