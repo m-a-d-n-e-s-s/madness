ad0a4daa84a094b3edee2677ac0af35083b91696
Jakob Kottmann <jakob.kottmann@student.hu-berlin.de>
2014-10-08 16:25:54 +0200

    improved rydberg guess

--- src/apps/chem/TDA.cc
+++ src/apps/chem/TDA.cc
@@ -328,6 +328,7 @@ void TDA::add_diffuse_functions(vecfuncT &mos) {
 		}
 		std::cout << "exponent is " << rydberg_exponent_ << std::endl;
 		std::cout << "signs\n" << signs << std::endl;
+		if (signs.back() == 0) signs.back()=1;
 
 		// make the diffuse functions with the right signs
 		real_function_3d diffuse_tmp;
