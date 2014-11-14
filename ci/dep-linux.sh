45019920ee5babc7f070f7cfe40f03b55c8c1f72
justusc <justusc@vt.edu>
2014-11-14 14:36:07 -0500

    Removed libxc install from CI.

--- ci/dep-linux.sh
+++ ci/dep-linux.sh
@@ -3,6 +3,7 @@
 # Exit on error
 set -e
 
-sudo add-apt-repository ppa:hogliux/misstep -y
+#sudo add-apt-repository ppa:hogliux/misstep -y
 sudo apt-get update -qq
-sudo apt-get install -qq -y cmake libopenblas-dev liblapack-dev  libgoogle-perftools0 mpich2 libxc-devel libtbb-dev
+#sudo apt-get install -qq -y libxc-devel
+sudo apt-get install -qq -y cmake libopenblas-dev liblapack-dev  libgoogle-perftools0 mpich2 libtbb-dev
