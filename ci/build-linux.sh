62da70d9764f71fb73783ec1dc5dc5da5bc84dd6
justusc <justusc@vt.edu>
2014-11-14 15:23:11 -0500

    - Added environment variables for compilers in CI. - Updated library dependencies in CI.

--- ci/build-linux.sh
+++ ci/build-linux.sh
@@ -3,13 +3,19 @@
 # Exit on error
 set -e
 
+# Environment variables
+export MPICH_CC=$CC
+export MPICH_CXX=$CXX
+export MPICC=/usr/bin/mpicc.mpich2
+export MPICXX=/usr/bin/mpicxx.mpich2
+
 # Configure MADNESS
 ./autogen.sh 
 ./configure \
     --enable-debugging --disable-optimization --enable-warning --disable-optimal \
     --enable-never-spin \
-    LIBS="-llapack -lopenblas" \
-    MPICC=/usr/bin/mpicc.mpich2 MPICXX=/usr/bin/mpicxx.mpich2
+    LIBS="-L/usr/lib/lapack -llapack -L/usr/lib/openblas-base -lopenblas -ltcmalloc_minimal -ltbb -lpthread"
+    
 make
 
 # Run unit tests
