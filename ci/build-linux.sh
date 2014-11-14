c5c94f6e4aa1d47af09b2a929f463a6af22d1a0e
justusc <justusc@vt.edu>
2014-11-14 15:03:17 -0500

    Modified the configure command for CI.

--- ci/build-linux.sh
+++ ci/build-linux.sh
@@ -5,7 +5,11 @@ set -e
 
 # Configure MADNESS
 ./autogen.sh 
-./configure --enable-never-spin
+./configure \
+    --enable-debugging --disable-optimization --enable-warning --disable-optimal \
+    --enable-never-spin \
+    LIBS="-llapack -lopenblas" \
+    MPICC=/usr/bin/mpicc.mpich2 MPICXX=/usr/bin/mpicxx.mpich2
 make
 
 # Run unit tests
