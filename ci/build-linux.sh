464572ed3461a5ebd35e7d1483f57f7d77513cf4
justusc <justusc@vt.edu>
2014-11-17 09:28:45 -0500

    CI dependency script now installs libxc from source.

--- ci/build-linux.sh
+++ ci/build-linux.sh
@@ -7,6 +7,7 @@ set -ev
 if [ "$CXX" = "g++" ]; then
     export CC=gcc-4.7
     export CXX=g++-4.7
+    export F77=gfortran-4.7
 fi
 export MPICH_CC=$CC
 export MPICH_CXX=$CXX
@@ -19,7 +20,7 @@ export MPICXX=/usr/bin/mpicxx.mpich2
     --enable-debugging --disable-optimization --enable-warning --disable-optimal \
     --enable-never-spin \
     CXXFLAGS="-std=c++11" \
-    LIBS="-L/usr/lib/lapack -L/usr/lib/openblas-base -llapack -lopenblas -ltcmalloc_minimal -ltbb -lpthread"
+    LIBS="-L/usr/lib/lapack -L/usr/lib/openblas-base -llapack -lopenblas -lpthread"
     
 make
 
--- ci/dep-linux.sh
+++ ci/dep-linux.sh
@@ -13,10 +13,23 @@ sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -qq
 
 # Install packages
-#sudo apt-get install -qq -y libxc-devel
 
 if [ "$CXX" = "g++" ]; then
-   sudo apt-get install -qq gcc-4.7 g++-4.7
+    sudo apt-get install -qq -y gcc-4.7.2 g++-4.7.2 gfortran-4.7.2
+    export CC=gcc-4.7
+    export CXX=g++-4.7
+    export FC=gfortran-4.7
+else
+    sudo apt-get install -qq -y gfortran
+    export FC=gfortran
 fi
+
+wget -O libxc-2.2.1.tar.gz "http://www.tddft.org/programs/octopus/down.php?file=libxc/libxc-2.2.1.tar.gz"
+tar -xzf libxc-2.2.1.tar.gz
+cd libxc-2.2.1
+autoreconf -i
+./configure --prefix=/usr/local
+make
+sudo make install
 
 sudo apt-get install -qq -y cmake libopenblas-dev liblapack-dev libgoogle-perftools-dev mpich2 libtbb-dev
