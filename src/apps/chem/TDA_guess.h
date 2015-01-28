fe01cbbcd54acc45b23d7eed07e0863665db3cc1
Jakob Kottmann <jakob.kottmann@student.hu-berlin.de>
2015-01-28 10:45:28 +0100

    update

--- src/apps/chem/TDA_guess.h
+++ src/apps/chem/TDA_guess.h
@@ -38,6 +38,8 @@ private:
 };
 class guess{
 private :
+	// The world
+	World &world;
 	// size of the simulation box
 	const double L;
 	/// Molecular orbitals, the MRA mos or the MRA mos projected on the STO basis
@@ -46,13 +48,16 @@ private :
 	vecfuncT mos_;
 	/// Maximum order of polynomials for excitation operators
 	size_t order_;
+	const coord_3d zero_position_;
 public :
 	/// Constructor
 	guess(World & world,const double box_size, const std::string mo_mode,const vecfuncT &active_mo, const vecfuncT & ao_basis ) :
+		world(world),
 		L(box_size),
 		mo_mode_(mo_mode),
-		projected_(false)
-	{
+		projected_(false),
+		zero_position_(initv(0.0,0.0,0.0))
+{
 		if(world.rank()==0) std::cout << "Making guess excitation operators, guess_mode is " << mo_mode_ << std::endl;
 		if(mo_mode_=="projected" or mo_mode_=="projected_mo" or mo_mode_=="projected_mos" or mo_mode_=="ao"){
 			if(world.rank()==0) std::cout << "Molecular orbitals are getting projected to AO-Basis ..." << std::endl;
@@ -62,7 +67,7 @@ public :
 		order_ =6;
 		make_polynomial_exops_key(world);
 		if(world.rank()==0) std::cout << "\n\n" << std::endl;
-	}
+}
 
 	/// The key and the exponents for custom guess exops
 	/// assignment goes as follows : if(input_line == key[i]) exponents for exop are in exop_exopnents[i]
@@ -90,11 +95,11 @@ public :
 			for(size_t y=0;y<order_;y++){
 				for(size_t z=0;z<order_;z++){
 					if(x+y+z <order_){
-					double exx = (double) x; double exy = (double) y ; double exz = (double) z;
-					std::string key_tmp =  "x" + stringify(x) + "y" + stringify(y) + "z" + stringify(z);
-					std::vector<double> ex_tmp; ex_tmp.push_back(exx); ex_tmp.push_back(exy); ex_tmp.push_back(exz);
-					exponents.push_back(ex_tmp);
-					key.push_back(key_tmp);
+						double exx = (double) x; double exy = (double) y ; double exz = (double) z;
+						std::string key_tmp =  "x" + stringify(x) + "y" + stringify(y) + "z" + stringify(z);
+						std::vector<double> ex_tmp; ex_tmp.push_back(exx); ex_tmp.push_back(exy); ex_tmp.push_back(exz);
+						exponents.push_back(ex_tmp);
+						key.push_back(key_tmp);
 					}
 				}
 			}
@@ -254,6 +259,396 @@ public :
 		double xnorm2 = x_self_overlap.sum();
 		scale(world,guess_x,1.0/sqrt(xnorm2));
 		return guess_x;
+	}
+
+	/// Make a symmetric guess
+	std::vector<vecfuncT> make_symmetry_guess(World & world, const std::string point_group)const{
+		if(world.rank()==0) std::cout << "Making symmetric guess excitation vectors with point group: " << point_group << std::endl;
+		coord_3d position; position[0]=0.0; position[1]=0.0; position[2]=0.0;
+		std::vector<vecfuncT> guess_vectors;
+		if(point_group=="c2v"){
+			guess_vectors = make_c2v_guess(position);
+		}else if(point_group=="d2h"){
+			guess_vectors = make_d2h_guess(position);
+		}else{
+			MADNESS_EXCEPTION("ERROR in guess calculation: Unknown point group",1);
+		}
+
+		MADNESS_EXCEPTION("ERROR in guess calculation: Should not end up here ... unknown point group ?",1);
+	}
+
+	std::vector<vecfuncT> make_c2v_guess(const coord_3d &position, const bool big=false)const{
+		std::vector<vecfuncT> guess_vectors;
+		{
+			// A1 (odd and even)
+			real_function_3d a1_odd_exop = real_factory_3d(world);
+			real_function_3d a1_eve_exop = real_factory_3d(world);
+			std::vector<std::vector<double> > a1_odd_exponents;
+			std::vector<std::vector<double> > a1_eve_exponents;
+			a1_odd_exponents.push_back(initv(0,0,1));
+			a1_odd_exponents.push_back(initv(2,0,1));
+			a1_odd_exponents.push_back(initv(0,2,1));
+			a1_odd_exponents.push_back(initv(0,0,3));
+			a1_odd_exponents.push_back(initv(0,0,1));
+
+			a1_eve_exponents.push_back(initv(2,0,0));
+			a1_eve_exponents.push_back(initv(0,2,0));
+			a1_eve_exponents.push_back(initv(0,0,2));
+			a1_eve_exponents.push_back(initv(4,0,0));
+			a1_eve_exponents.push_back(initv(0,4,0));
+			a1_eve_exponents.push_back(initv(0,0,4));
+			a1_eve_exponents.push_back(initv(2,2,0));
+			a1_eve_exponents.push_back(initv(2,0,2));
+			a1_eve_exponents.push_back(initv(0,2,2));
+
+			for(size_t i=0;i<a1_odd_exponents.size();i++){
+				a1_odd_exop += make_polynomial(world,a1_odd_exponents[i],position,L);
+			}
+			for(size_t i=0;i<a1_eve_exponents.size();i++){
+				a1_eve_exop += make_polynomial(world,a1_eve_exponents[i],position,L);
+			}
+			vecfuncT a1_odd_guess = mul(world,a1_odd_exop,mos_);
+			vecfuncT a1_eve_guess = mul(world,a1_eve_exop,mos_);
+			if(big){
+				guess_vectors.push_back(a1_odd_guess);
+				guess_vectors.push_back(a1_eve_guess);
+			}else{
+				vecfuncT small_guess = add(world,a1_odd_guess,a1_eve_guess);
+				guess_vectors.push_back(small_guess);
+			}
+		}
+		{
+			// A2 (with and without z component)
+			real_function_3d a2_1_exop = real_factory_3d(world);
+			real_function_3d a2_2_exop = real_factory_3d(world);
+			std::vector<std::vector<double> > a2_1_exponents;
+			std::vector<std::vector<double> > a2_2_exponents;
+			a2_1_exponents.push_back(initv(1,1,0));
+			a2_1_exponents.push_back(initv(3,1,0));
+			a2_1_exponents.push_back(initv(1,3,0));
+
+			a2_2_exponents.push_back(initv(1,1,1));
+
+			for(size_t i=0;i<a2_1_exponents.size();i++){
+				a2_1_exop += make_polynomial(world,a2_1_exponents[i],position,L);
+			}
+			for(size_t i=0;i<a2_2_exponents.size();i++){
+				a2_2_exop += make_polynomial(world,a2_2_exponents[i],position,L);
+			}
+			vecfuncT a2_1_guess = mul(world,a2_1_exop,mos_);
+			vecfuncT a2_2_guess = mul(world,a2_1_exop,mos_);
+			if(big){
+				guess_vectors.push_back(a2_1_guess);
+				guess_vectors.push_back(a2_2_guess);
+			}else{
+				guess_vectors.push_back(add(world,a2_1_guess,a2_2_guess));
+			}
+		}
+		{
+			// B1 (with and without y component)
+			real_function_3d b1_1_exop = real_factory_3d(world);
+			real_function_3d b1_2_exop = real_factory_3d(world);
+			std::vector<std::vector<double> > b1_1_exponents;
+			std::vector<std::vector<double> > b1_2_exponents;
+			b1_1_exponents.push_back(initv(1,0,0));
+			b1_1_exponents.push_back(initv(1,0,1));
+			b1_1_exponents.push_back(initv(3,0,0));
+			b1_2_exponents.push_back(initv(1,2,0));
+			b1_1_exponents.push_back(initv(1,0,2));
+			b1_1_exponents.push_back(initv(3,0,1));
+			b1_2_exponents.push_back(initv(1,2,1));
+			b1_1_exponents.push_back(initv(1,0,3));
+
+
+			for(size_t i=0;i<b1_1_exponents.size();i++){
+				b1_1_exop += make_polynomial(world,b1_1_exponents[i],position,L);
+			}
+			for(size_t i=0;i<b1_2_exponents.size();i++){
+				b1_1_exop += make_polynomial(world,b1_2_exponents[i],position,L);
+			}
+			vecfuncT b1_1_guess = mul(world,b1_1_exop,mos_);
+			vecfuncT b1_2_guess = mul(world,b1_1_exop,mos_);
+			if(big){
+				guess_vectors.push_back(b1_1_guess);
+				guess_vectors.push_back(b1_2_guess);
+			}else{
+				guess_vectors.push_back(add(world,b1_1_guess,b1_2_guess));
+			}
+		}
+		{
+			// B2 (with and without x component)
+			real_function_3d b2_1_exop = real_factory_3d(world);
+			real_function_3d b2_2_exop = real_factory_3d(world);
+			std::vector<std::vector<double> > b2_1_exponents;
+			std::vector<std::vector<double> > b2_2_exponents;
+			b2_1_exponents.push_back(initv(0,1,0));
+			b2_1_exponents.push_back(initv(0,1,1));
+			b2_1_exponents.push_back(initv(0,1,2));
+			b2_2_exponents.push_back(initv(2,1,0));
+			b2_1_exponents.push_back(initv(0,3,0));
+			b2_2_exponents.push_back(initv(2,1,1));
+			b2_1_exponents.push_back(initv(0,3,1));
+			b2_1_exponents.push_back(initv(0,1,3));
+
+
+			for(size_t i=0;i<b2_1_exponents.size();i++){
+				b2_1_exop += make_polynomial(world,b2_1_exponents[i],position,L);
+			}
+			for(size_t i=0;i<b2_2_exponents.size();i++){
+				b2_2_exop += make_polynomial(world,b2_2_exponents[i],position,L);
+			}
+			vecfuncT b2_1_guess = mul(world,b2_1_exop,mos_);
+			vecfuncT b2_2_guess = mul(world,b2_1_exop,mos_);
+			if(big){
+				guess_vectors.push_back(b2_1_guess);
+				guess_vectors.push_back(b2_2_guess);
+			}else{
+				guess_vectors.push_back(add(world,b2_1_guess,b2_2_guess));
+			}
+		}
+		// check
+		if(big and not guess_vectors.size()==8) MADNESS_EXEPTION("ERROR in c2v guess, big guess demanded but not 8 guess vectors created ... ???",1);
+		if(not big and not guess_vectors.size()==4) MADNESS_EXEPTION("ERROR in c2v guess, normal guess demanded but not 4 guess vectors created ... ???",1);
+		return guess_vectors;
+
+	}
+	std::vector<vecfuncT> make_d2h_guess(const coord_3d &position, const bool big=false)const{
+		std::vector<vecfuncT> guess_vectors;
+		{
+			// Ag
+			real_function_3d ag_exop = real_factory_3d(world);
+			std::vector<std::vector<double> > ag_exponents;
+			ag_exponents.push_back(initv(2,0,0));
+			ag_exponents.push_back(initv(0,2,0));
+			ag_exponents.push_back(initv(0,0,2));
+			ag_exponents.push_back(initv(4,0,0));
+			ag_exponents.push_back(initv(0,4,0));
+			ag_exponents.push_back(initv(0,0,4));
+			ag_exponents.push_back(initv(2,2,0));
+			ag_exponents.push_back(initv(0,2,2));
+			ag_exponents.push_back(initv(2,0,2));
+
+			for(size_t i=0;i<ag_exponents.size();i++){
+				ag_exop += make_polynomial(world,ag_exponents[i],position,L);
+			}
+			vecfuncT ag_guess = mul(world,ag_exop,mos_);
+			guess_vectors.push_back(ag_guess);
+
+		}
+		{
+			// B1g
+			real_function_3d b1g_exop = real_factory_3d(world);
+			std::vector<std::vector<double> > b1g_exponents;
+			b1g_exponents.push_back(initv(1,1,0));
+			b1g_exponents.push_back(initv(3,1,0));
+			b1g_exponents.push_back(initv(1,3,0));
+			b1g_exponents.push_back(initv(1,1,2));
+
+			for(size_t i=0;i<b1g_exponents.size();i++){
+				b1g_exop += make_polynomial(world,b1g_exponents[i],position,L);
+			}
+			vecfuncT b1g_guess = mul(world,b1g_exop,mos_);
+			guess_vectors.push_back(b1g_guess);
+		}
+		{
+			// b2g
+			real_function_3d b2g_exop = real_factory_3d(world);
+			std::vector<std::vector<double> > b2g_exponents;
+			b2g_exponents.push_back(initv(1,0,1));
+			b2g_exponents.push_back(initv(3,0,1));
+			b2g_exponents.push_back(initv(1,2,1));
+			b2g_exponents.push_back(initv(1,0,3));
+
+			for(size_t i=0;i<b2g_exponents.size();i++){
+				b2g_exop += make_polynomial(world,b2g_exponents[i],position,L);
+			}
+			vecfuncT b2g_guess = mul(world,b2g_exop,mos_);
+			guess_vectors.push_back(b2g_guess);
+		}
+		{
+			// b3g
+			real_function_3d b3g_exop = real_factory_3d(world);
+			std::vector<std::vector<double> > b3g_exponents;
+			b3g_exponents.push_back(initv(0,1,1));
+			b3g_exponents.push_back(initv(2,1,1));
+			b3g_exponents.push_back(initv(0,3,1));
+			b3g_exponents.push_back(initv(0,1,3));
+
+
+			for(size_t i=0;i<b3g_exponents.size();i++){
+				b3g_exop += make_polynomial(world,b3g_exponents[i],position,L);
+			}
+			vecfuncT b3g_guess = mul(world,b3g_exop,mos_);
+			guess_vectors.push_back(b3g_guess);
+		}
+		{
+			// au
+			real_function_3d au_exop = real_factory_3d(world);
+			std::vector<std::vector<double> > au_exponents;
+			au_exponents.push_back(initv(1,1,1));
+
+			for(size_t i=0;i<au_exponents.size();i++){
+				au_exop += make_polynomial(world,au_exponents[i],position,L);
+			}
+			vecfuncT au_guess = mul(world,au_exop,mos_);
+			guess_vectors.push_back(au_guess);
+		}
+		{
+			// b1u
+			real_function_3d b1u_exop = real_factory_3d(world);
+			std::vector<std::vector<double> > b1u_exponents;
+			b1u_exponents.push_back(initv(0,0,1));
+			b1u_exponents.push_back(initv(0,0,3));
+			b1u_exponents.push_back(initv(0,2,1));
+			b1u_exponents.push_back(initv(2,0,1));
+
+			for(size_t i=0;i<b1u_exponents.size();i++){
+				b1u_exop += make_polynomial(world,b1u_exponents[i],position,L);
+			}
+			vecfuncT b1u_guess = mul(world,b1u_exop,mos_);
+			guess_vectors.push_back(b1u_guess);
+		}
+		{
+			// b2u
+			real_function_3d b2u_exop = real_factory_3d(world);
+			std::vector<std::vector<double> > b2u_exponents;
+			b2u_exponents.push_back(initv(0,1,2));
+			b2u_exponents.push_back(initv(2,1,0));
+			b2u_exponents.push_back(initv(0,3,0));
+			b2u_exponents.push_back(initv(0,1,0));
+
+			for(size_t i=0;i<b2u_exponents.size();i++){
+				b2u_exop += make_polynomial(world,b2u_exponents[i],position,L);
+			}
+			vecfuncT b2u_guess = mul(world,b2u_exop,mos_);
+			guess_vectors.push_back(b2u_guess);
+		}
+		{
+			// b3u
+			real_function_3d b3u_exop = real_factory_3d(world);
+			std::vector<std::vector<double> > b3u_exponents;
+			b3u_exponents.push_back(initv(1,0,0));
+			b3u_exponents.push_back(initv(1,0,2));
+			b3u_exponents.push_back(initv(1,2,0));
+			b3u_exponents.push_back(initv(3,0,0));
+
+			for(size_t i=0;i<b3u_exponents.size();i++){
+				b3u_exop += make_polynomial(world,b3u_exponents[i],position,L);
+			}
+			vecfuncT b3u_guess = mul(world,b3u_exop,mos_);
+			guess_vectors.push_back(b3u_guess);
+		}
+		//check
+		if(not guess_vectors.size()==8) MADNESS_EXCEPTION("ERROR in creating d2h guess, not 8 guess vectors created",1);
+		return guess_vectors;
+	}
+	std::vector<vecfuncT> make_c2h_guess(const coord_3d &position, const bool big=false)const{
+		std::vector<vecfuncT> guess_vectors;
+		{
+			//Ag
+			std::vector<std::vector<double> > exponents1;
+			std::vector<std::vector<double> > exponents2;
+			real_function_3d exop1 = real_factory_3d(world);
+			real_function_3d exop2 = real_factory_3d(world);
+			exponents1.push_back(initv(2.0,0.0,0.0));
+			exponents1.push_back(initv(0.0,2.0,0.0));
+			exponents1.push_back(initv(0.0,0.0,2.0));
+			exponents1.push_back(initv(4.0,0.0,0.0));
+			exponents1.push_back(initv(0.0,4.0,0.0));
+			exponents1.push_back(initv(0.0,0.0,4.0));
+			exponents1.push_back(initv(2.0,2.0,0.0));
+			exponents1.push_back(initv(0.0,2.0,2.0));
+			exponents1.push_back(initv(2.0,0.0,2.0));
+			exponents2.push_back(initv(1.0,1.0,0.0));
+			exponents2.push_back(initv(3.0,1.0,0.0));
+			exponents2.push_back(initv(1.0,3.0,0.0));
+			exponents2.push_back(initv(1.0,1.0,2.0));
+
+			for(size_t i=0;i<exponents1.size();i++){
+				exop1 += make_polynomial(world,exponents1[i],position,L);
+			}
+			for(size_t i=0;i<exponents2.size();i++){
+				exop2 += make_polynomial(world,exponents2[i],position,L);
+			}
+			vecfuncT guess1 = mul(world,exop1,mos_);
+			vecfuncT guess2 = mul(world,exop2,mos_);
+			if(big){
+				guess_vectors.push_back(guess1);
+				guess_vectors.push_back(guess2);
+			}else{
+				guess_vectors.push_back(add(world,guess1,guess2));
+			}
+		}
+		{
+			//Au
+			std::vector<std::vector<double> > exponents1;
+			std::vector<std::vector<double> > exponents2;
+			real_function_3d exop1 = real_factory_3d(world);
+			real_function_3d exop2 = real_factory_3d(world);
+			exponents1.push_back(initv(0.0,0.0,0.0));
+			exponents1.push_back(initv(0.0,0.0,0.0));
+			exponents1.push_back(initv(0.0,0.0,0.0));
+			exponents1.push_back(initv(0.0,0.0,0.0));
+			exponents2.push_back(initv(0.0,0.0,0.0));
+			exponents2.push_back(initv(0.0,0.0,0.0));
+			exponents2.push_back(initv(0.0,0.0,0.0));
+			exponents2.push_back(initv(0.0,0.0,0.0));
+
+			for(size_t i=0;i<exponents1.size();i++){
+				exop1 += make_polynomial(world,exponents1[i],position,L);
+			}
+			for(size_t i=0;i<exponents2.size();i++){
+				exop2 += make_polynomial(world,exponents2[i],position,L);
+			}
+			vecfuncT guess1 = mul(world,exop1,mos_);
+			vecfuncT guess2 = mul(world,exop2,mos_);
+			if(big){
+				guess_vectors.push_back(guess1);
+				guess_vectors.push_back(guess2);
+			}else{
+				guess_vectors.push_back(add(world,guess1,guess2));
+			}
+		}
+		{
+			//B1
+			std::vector<std::vector<double> > exponents1;
+			std::vector<std::vector<double> > exponents2;
+			real_function_3d exop1 = real_factory_3d(world);
+			real_function_3d exop2 = real_factory_3d(world);
+			exponents1.push_back(initv(0.0,0.0,0.0));
+			exponents1.push_back(initv(0.0,0.0,0.0));
+			exponents1.push_back(initv(0.0,0.0,0.0));
+			exponents1.push_back(initv(0.0,0.0,0.0));
+			exponents2.push_back(initv(0.0,0.0,0.0));
+			exponents2.push_back(initv(0.0,0.0,0.0));
+			exponents2.push_back(initv(0.0,0.0,0.0));
+			exponents2.push_back(initv(0.0,0.0,0.0));
+
+			for(size_t i=0;i<exponents1.size();i++){
+				exop1 += make_polynomial(world,exponents1[i],position,L);
+			}
+			for(size_t i=0;i<exponents2.size();i++){
+				exop2 += make_polynomial(world,exponents2[i],position,L);
+			}
+			vecfuncT guess1 = mul(world,exop1,mos_);
+			vecfuncT guess2 = mul(world,exop2,mos_);
+			if(big){
+				guess_vectors.push_back(guess1);
+				guess_vectors.push_back(guess2);
+			}else{
+				guess_vectors.push_back(add(world,guess1,guess2));
+			}
+		}
+		//check
+		if(big and not guess_vectors.size()==8) MADNESS_EXEPTION("ERROR in c2h guess, big guess demanded but not 8 guess vectors created ... ???",1);
+		if(not big and not guess_vectors.size()==4) MADNESS_EXEPTION("ERROR in c2h guess, normal guess demanded but not 4 guess vectors created ... ???",1);
+		return guess_vectors;
+	}
+	/// Needed by symmetry guess function
+	std::vector<double> initv(const double &x,const double &y, const double &z)const{
+		std::vector<double> tmp;
+		tmp.push_back(x); tmp.push_back(y); tmp.push_back(z);
+		return tmp;
 	}
 
 	/// Project the solution vectors back to the polynomial basis
