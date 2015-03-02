m-a-d-n-e-s-s/madness

commit 2babd734dcf0eed4d7b67818c48e58c6e295e697

fix potential race issues with unique_tag
SAFE_MPI_GLOBAL_MUTEX is ***ONLY*** useful for serializing MPI calls
when MPI uses MPI_THREAD_SERIALIZED.  It preprocesses to ***NOTHING***
when MPI_THREAD_MULTIPLE is used.  That means that it will not protect
unique_tag's bounded increment all of the time.

To fix this, we allow the obolus(SafeMPI::charon) to be defined in
safempi.cc all of the time and use it explicitly in unique_tag.  When
used to protect MPI calls and ONLY to protect MPI calls, we continue to
use the SAFE_MPI_GLOBAL_MUTEX macro.

Signed-off-by: Jeff Hammond <jeff.science@gmail.com>

src/madness/world/safempi.h
@@ -472,7 +472,11 @@ namespace SafeMPI {
             /// So that send and receiver agree on the tag all processes
             /// need to call this routine in the same sequence.
             int unique_tag() {
-                SAFE_MPI_GLOBAL_MUTEX;
+                /* Cannot use MPI mutex for anything else!
+		 * It will preprocess to nothing for MPI_THREAD_MULTIPLE!
+		 *	SAFE_MPI_GLOBAL_MUTEX;
+		 */
+		madness::ScopedMutex<madness::SCALABLE_MUTEX_TYPE> obolus(SafeMPI::charon)
                 int result = utag++;
                 if (utag >= 4095) utag = 1024;
                 return result;
@@ -484,7 +488,11 @@ namespace SafeMPI {
             ///
             /// Tags in [1000,1023] are statically assigned.
             int unique_reserved_tag() {
-                SAFE_MPI_GLOBAL_MUTEX;
+                /* Cannot use MPI mutex for anything else!
+		 * It will preprocess to nothing for MPI_THREAD_MULTIPLE!
+		 *	SAFE_MPI_GLOBAL_MUTEX;
+		 */
+		madness::ScopedMutex<madness::SCALABLE_MUTEX_TYPE> obolus(SafeMPI::charon)
                 int result = urtag++;
                 if (result >= 1000) MADNESS_EXCEPTION( "too many reserved tags in use" , result );
                 return result;

src/madness/world/safempi.cc
@@ -35,9 +35,7 @@
 
 namespace SafeMPI {
 
-#ifdef MADNESS_SERIALIZES_MPI
     madness::SCALABLE_MUTEX_TYPE charon;
-#endif
 
     void Intracomm::binary_tree_info(int root, int& parent, int& child0, int& child1) {
         const int np = Get_size();

commit 804efdcc541eb3d622fcf06c71edfc5e120fe873

SafeMPI::charon is no always declared extern, not just when MADNESS_SERIALIZES_MPI has been defined. Fixes a bug introduced in 2babd734dcf0eed4d7b67818c48e58c6e295e697.

src/madness/world/safempi.h
@@ -81,8 +81,8 @@
 
 namespace SafeMPI {
 
-#ifdef MADNESS_SERIALIZES_MPI
     extern madness::SCALABLE_MUTEX_TYPE charon;      // Inside safempi.cc
+#ifdef MADNESS_SERIALIZES_MPI
 #define SAFE_MPI_GLOBAL_MUTEX madness::ScopedMutex<madness::SCALABLE_MUTEX_TYPE> obolus(SafeMPI::charon);
 #else
 #define SAFE_MPI_GLOBAL_MUTEX