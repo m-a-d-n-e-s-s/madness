m-a-d-n-e-s-s/madness

commit 87bce503063ae32c8ec29e19ac0220df13131f51

Madness now runs task while waiting on the main thread. The number of threads in the thread pool is now correct when using TBB.

src/madness/world/worldthread.h
@@ -1026,11 +1026,33 @@ namespace madness {
 
         /// Returns true if one was run
         static bool run_task() {
-#ifdef MADNESS_TASK_PROFILING
-            return instance()->run_tasks(false, static_cast<ThreadPoolThread*>(ThreadBase::this_thread()));
+#ifdef HAVE_INTEL_TBB
+            const std::size_t num_pending_tasks = tbb_parent_task->ref_count();
+
+            // Construct a the parent task
+            tbb::task& waiter = *new( tbb::task::allocate_root() ) tbb::empty_task;
+            waiter.set_ref_count(2);
+
+            // Create a dummy task that we send throught the queue.
+            tbb::task& dummy = *new( waiter.allocate_child() ) tbb::empty_task;
+            tbb::task::enqueue(dummy);
+
+            // Run tasks while we wait for the dummy task.
+            waiter.wait_for_all();
+
+            // destroy the waiter
+            tbb::task::destroy(waiter);
+            return num_pending_tasks != tbb_parent_task->ref_count();
 #else
-            return instance()->run_tasks(false, NULL);
+
+#ifdef MADNESS_TASK_PROFILING
+            ThreadBase* const thread = static_cast<ThreadPoolThread*>(ThreadBase::this_thread());
+#else
+            ThreadBase* const thread = nullptr;
 #endif // MADNESS_TASK_PROFILING
+
+            return instance()->run_tasks(false, thread);
+#endif // HAVE_INTEL_TBB
         }
 
         /// Returns number of threads in the pool
@@ -1058,12 +1080,7 @@ namespace madness {
             MutexWaiter waiter;
             while (!probe()) {
 
-#if HAVE_INTEL_TBB
-                // TODO: Enable busy waiting with TBB.
-                const bool working = false;
-#else
                 const bool working = (dowork ? ThreadPool::run_task() : false);
-#endif // HAVE_INTEL_TBB
                 const double current_time = cpu_time();
 
                 if (working) {

src/madness/world/worldthread.cc
@@ -351,14 +351,12 @@ namespace madness {
             nthreads = 1;
 
         if (SafeMPI::COMM_WORLD.Get_size() > 1) {
-            // There are nthreads+2 because the main and communicator thread
-            // are now a part of tbb.
-            tbb_scheduler = new tbb::task_scheduler_init(nthreads+2);
+            // There are nthreads+1 because the communication thread
+            // is run as a tbb task.
+            tbb_scheduler = new tbb::task_scheduler_init(nthreads+1);
         }
         else {
-            // There are nthreads+1 because the main
-            // is now part of tbb.
-            tbb_scheduler = new tbb::task_scheduler_init(nthreads+1);
+            tbb_scheduler = new tbb::task_scheduler_init(nthreads);
         }
         tbb_parent_task = new(tbb::task::allocate_root()) tbb::empty_task;
         tbb_parent_task->increment_ref_count();

commit daeb0d36f7b8aeeec7ca6558e32d71613185bdf9

Fixed an error introduced in commit 87bce503063ae32c8ec29e19ac0220df13131f51.

src/madness/world/worldthread.h
@@ -909,7 +909,7 @@ namespace madness {
 #endif
         }
 
-        bool run_tasks(bool wait, ThreadPoolThread* this_thread) {
+        bool run_tasks(bool wait, ThreadPoolThread* const this_thread) {
 #if HAVE_INTEL_TBB
 //            if (!wait && tbb_task_list->empty()) return false;
 //            tbb::task* t = &tbb_task_list->pop_front();
@@ -1046,9 +1046,9 @@ namespace madness {
 #else
 
 #ifdef MADNESS_TASK_PROFILING
-            ThreadBase* const thread = static_cast<ThreadPoolThread*>(ThreadBase::this_thread());
+            ThreadPoolThread* const thread = static_cast<ThreadPoolThread*>(ThreadBase::this_thread());
 #else
-            ThreadBase* const thread = nullptr;
+            ThreadPoolThread* const thread = nullptr;
 #endif // MADNESS_TASK_PROFILING
 
             return instance()->run_tasks(false, thread);