c72c1d891cc9b188d22c7e9c7482a8430b1f2391
Justus Calvin <justusc@vt.edu>
2015-08-23 10:04:01 -0400

    The copy_target_properties macro now correctly handles property values with more than one value.

--- cmake/modules/CopyTargetProperties.cmake
+++ cmake/modules/CopyTargetProperties.cmake
@@ -3,7 +3,7 @@ macro(copy_target_properties _intarget _outtarget _props)
   foreach(_prop ${_props})
     get_target_property(_value ${_intarget} ${_prop})
     if(_value)
-        set_target_properties(${_outtarget} PROPERTIES ${_prop} ${_value})
+      set_target_properties(${_outtarget} PROPERTIES ${_prop} "${_value}")
     endif()
   endforeach()
 
