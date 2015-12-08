5250c3cc0711683e31df1f087b5b8ea55de4ba6c
Matt Reuter <matthew.reuter@stonybrook.edu>
2015-12-08 16:27:06 -0500

    Fix indentation in the SIAM example.

--- src/examples/siam_example.cc
+++ src/examples/siam_example.cc
@@ -41,65 +41,65 @@ public:
 };
 
 double charge_function(const coord_3d &r) {
-    const double coeff = pow(xi/constants::pi, 1.5);
-    return coeff*exp(-xi * (r[0]*r[0] + r[1]*r[1] + r[2]*r[2]));
+  const double coeff = pow(xi/constants::pi, 1.5);
+  return coeff*exp(-xi * (r[0]*r[0] + r[1]*r[1] + r[2]*r[2]));
 }
 
 double exact_function(const coord_3d &x) {
-    const double r = sqrt(x[0]*x[0] + x[1]*x[1] + x[2]*x[2]);
-    if (r > R) return 1.0 / (eps_ext * r);
-    else return erf(sqrt(xi)*r) / (eps_int*r) + (1./eps_ext - 1./eps_int)/R;
+  const double r = sqrt(x[0]*x[0] + x[1]*x[1] + x[2]*x[2]);
+  if (r > R) return 1.0 / (eps_ext * r);
+  else return erf(sqrt(xi)*r) / (eps_int*r) + (1./eps_ext - 1./eps_int)/R;
 }
 
 int main(int argc, char **argv) {
-    initialize(argc, argv);
-    World world(SafeMPI::COMM_WORLD);
-    startup(world, argc, argv);
+  initialize(argc, argv);
+  World world(SafeMPI::COMM_WORLD);
+  startup(world, argc, argv);
 
-    // Function defaults
-    FunctionDefaults<3>::set_k(10);
-    FunctionDefaults<3>::set_thresh(1e-8);
-    FunctionDefaults<3>::set_cubic_cell(-5, 5);
-    FunctionDefaults<3>::set_initial_level(3);
-    FunctionDefaults<3>::set_truncate_on_project(true);
+  // Function defaults
+  FunctionDefaults<3>::set_k(10);
+  FunctionDefaults<3>::set_thresh(1e-8);
+  FunctionDefaults<3>::set_cubic_cell(-5, 5);
+  FunctionDefaults<3>::set_initial_level(3);
+  FunctionDefaults<3>::set_truncate_on_project(true);
 
-    // Integral and derivative operators
-    real_convolution_3d op = CoulombOperator(world, 1e-4, FunctionDefaults<3>::get_thresh());
-    real_derivative_3d Dx = free_space_derivative<double,3>(world, 0);
-    real_derivative_3d Dy = free_space_derivative<double,3>(world, 1);
-    real_derivative_3d Dz = free_space_derivative<double,3>(world, 2);
+  // Integral and derivative operators
+  real_convolution_3d op = CoulombOperator(world, 1e-4, FunctionDefaults<3>::get_thresh());
+  real_derivative_3d Dx = free_space_derivative<double,3>(world, 0);
+  real_derivative_3d Dy = free_space_derivative<double,3>(world, 1);
+  real_derivative_3d Dz = free_space_derivative<double,3>(world, 2);
 
-    // Functors for dielectric related quantities
-    real_functor_3d epsx_functor(new DSphere(eps_int, eps_ext, 0));
-    real_functor_3d epsy_functor(new DSphere(eps_int, eps_ext, 1));
-    real_functor_3d epsz_functor(new DSphere(eps_int, eps_ext, 2));
+  // Functors for dielectric related quantities
+  real_functor_3d epsx_functor(new DSphere(eps_int, eps_ext, 0));
+  real_functor_3d epsy_functor(new DSphere(eps_int, eps_ext, 1));
+  real_functor_3d epsz_functor(new DSphere(eps_int, eps_ext, 2));
 
-    // Make the actual functions
-    real_function_3d exact = real_factory_3d(world).f(exact_function);
-    real_function_3d charge = real_factory_3d(world).f(charge_function);
-    real_function_3d eps_x = real_factory_3d(world).functor(epsx_functor);
-    real_function_3d eps_y = real_factory_3d(world).functor(epsy_functor);
-    real_function_3d eps_z = real_factory_3d(world).functor(epsz_functor);
+  // Make the actual functions
+  real_function_3d exact = real_factory_3d(world).f(exact_function);
+  real_function_3d charge = real_factory_3d(world).f(charge_function);
+  real_function_3d eps_x = real_factory_3d(world).functor(epsx_functor);
+  real_function_3d eps_y = real_factory_3d(world).functor(epsy_functor);
+  real_function_3d eps_z = real_factory_3d(world).functor(epsz_functor);
 
-    const double rfourpi = 1.0/(4.0*constants::pi);
+  const double rfourpi = 1.0/(4.0*constants::pi);
 
-    real_function_3d u = op(charge).truncate();
-    NonlinearSolver solver;
-    for (int iter=0; iter<20; iter++) {
-      real_function_3d surf_charge = rfourpi*(eps_x*Dx(u) + eps_y*Dy(u) + eps_z*Dz(u));
-      real_function_3d r = (u - op(charge + surf_charge)).truncate();
-      real_function_3d unew = solver.update(u, r);
-      double change = (unew-u).norm2();
-      u = unew;
+  real_function_3d u = op(charge).truncate();
+  NonlinearSolver solver;
+  for (int iter=0; iter<20; iter++) {
+    real_function_3d surf_charge = rfourpi*(eps_x*Dx(u) + eps_y*Dy(u) + eps_z*Dz(u));
+    real_function_3d r = (u - op(charge + surf_charge)).truncate();
+    real_function_3d unew = solver.update(u, r);
+    double change = (unew-u).norm2();
+    u = unew;
 
-      print("iter", iter, "change", change, "surf charge", surf_charge.trace());
-      
-      if (change < 10.*FunctionDefaults<3>::get_thresh()) break;
-    }
+    print("iter", iter, "change", change, "surf charge", surf_charge.trace());
     
-    coord_3d lo{-5., 0., 0.}, hi{0.5, 0., 0.}; // Range for line plotting
-    plot_line("testpot.dat", 301, lo, hi, u, exact);
-    
-    finalize();
-    return 0;
+    if (change < 10.*FunctionDefaults<3>::get_thresh()) break;
+  }
+  
+  coord_3d lo{-5., 0., 0.}, hi{0.5, 0., 0.}; // Range for line plotting
+  plot_line("testpot.dat", 301, lo, hi, u, exact);
+  
+  finalize();
+  return 0;
 }
