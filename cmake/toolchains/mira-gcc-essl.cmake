b9a6e7a96ff80dfb64d5e51f72d714dbbd4e13f3
Nichols A. Romero <naromero@gmail.com>
2016-03-31 21:03:49 +0000

    If all build options are turned on, pthread library does not need to be listed explictly. As you turn off build options, compile fails because it is missing pthread library. XL files do in fact have the pthread library as a dependency

--- cmake/toolchains/mira-gcc-essl.cmake
+++ cmake/toolchains/mira-gcc-essl.cmake
@@ -43,7 +43,7 @@ set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g -Wall" CACHE STRING "Inital C++ relea
 # Set library
 
 set(XL_LIBRARIES ${XLSMP_DIR}/bglib64/libxlsmp.a)
-set(XLF_LIBRARIES ${XLF_DIR}/bglib64/libxlf90_r.a;${XLF_DIR}/bglib64/libxlfmath.a;${XLF_DIR}/bglib64/libxlopt.a;${XLF_DIR}/bglib64/libxl.a;-ldl;-lm)
+set(XLF_LIBRARIES ${XLF_DIR}/bglib64/libxlf90_r.a;${XLF_DIR}/bglib64/libxlfmath.a;${XLF_DIR}/bglib64/libxlopt.a;${XLF_DIR}/bglib64/libxl.a;-ldl;-lm;-lpthread)
 set(BLAS_LIBRARIES ${ESSL_DIR}/lib64/libesslbg.a;${BLAS_LIBRARY};${XLF_LIBRARIES};${XL_LIBRARIES})
 set(LAPACK_LIBRARIES ${LAPACK_DIR}/lib/liblapack.a;${BLAS_LIBRARIES})
 set(FORTRAN_INTEGER_SIZE "4" CACHE STRING "Set Fortran integer size in bytes")
