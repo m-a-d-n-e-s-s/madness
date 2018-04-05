e9dae0a8ca0cd7d238ad1dafde45c9157036f1bd
Bryan Sundahl <bryan.sundahl@stonybrook.edu>
2018-04-05 09:19:12 -0400

    Updated do_inner_localX to copy the input, distributed matrices into local contiguous copies and use DGEMM to do the multiplication.

--- src/madness/mra/funcimpl.h
+++ src/madness/mra/funcimpl.h
@@ -5000,46 +5000,45 @@ namespace madness {
         }
 
 
-        template <typename R>
-        static void do_inner_localX(const typename mapT::iterator lstart,
-                                    const typename mapT::iterator lend,
-                                    typename FunctionImpl<R,NDIM>::mapT* rmap_ptr,
-                                    const bool sym,
-                                    Tensor< TENSOR_RESULT_TYPE(T,R) >* result_ptr,
-                                    Mutex* mutex) {
-            Tensor< TENSOR_RESULT_TYPE(T,R) >& result = *result_ptr;
-            Tensor< TENSOR_RESULT_TYPE(T,R) > r(result.dim(0),result.dim(1));
-            for (typename mapT::iterator lit=lstart; lit!=lend; ++lit) {
-                const keyT& key = lit->first;
-                typename FunctionImpl<R,NDIM>::mapT::iterator rit=rmap_ptr->find(key);
-                if (rit != rmap_ptr->end()) {
-                    const mapvecT& leftv = lit->second;
-                    const typename FunctionImpl<R,NDIM>::mapvecT& rightv =rit->second;
-                    const int nleft = leftv.size();
-                    const int nright= rightv.size();
+       template <typename R>
+       static void do_inner_localX(const typename mapT::iterator lstart,
+                                   const typename mapT::iterator lend,
+                                   typename FunctionImpl<R,NDIM>::mapT* rmap_ptr,
+                                   const bool sym,
+                                   Tensor< TENSOR_RESULT_TYPE(T,R) >* result_ptr,
+                                   Mutex* mutex) {
+           Tensor< TENSOR_RESULT_TYPE(T,R) >& result = *result_ptr;
+           //Tensor< TENSOR_RESULT_TYPE(T,R) > r(result.dim(0),result.dim(1));
+           for (typename mapT::iterator lit=lstart; lit!=lend; ++lit) {
+               const keyT& key = lit->first;
+               typename FunctionImpl<R,NDIM>::mapT::iterator rit=rmap_ptr->find(key);
+               if (rit != rmap_ptr->end()) {
+                   const mapvecT& leftv = lit->second;
+                   const typename FunctionImpl<R,NDIM>::mapvecT& rightv =rit->second;
+                   const int nleft = leftv.size();
+                   const int nright= rightv.size();
 
-                    for (int iv=0; iv<nleft; iv++) {
-                        const int i = leftv[iv].first;
-                        const GenTensor<T>* iptr = leftv[iv].second;
+                   unsigned int size = leftv[0].second->size();
+                   Tensor<T> Left(nleft, size);
+                   Tensor<R> Right(nright, size);
+                   Tensor< TENSOR_RESULT_TYPE(T,R)> r(nleft, nright);
+                   for(unsigned int iv = 0; iv < nleft; ++iv) Left(iv,_) = *(leftv[iv].second);
+                   for(unsigned int jv = 0; jv < nright; ++jv) Right(jv,_) = *(rightv[jv].second);
+                   // call mxmT from mxm.h in tensor
+                   mxmT(nleft, nright, size, r.ptr(), Left.ptr(), Right.ptr());
+                   mutex->lock();
+                   for(unsigned int iv = 0; iv < nleft; ++iv) {
+                       const int i = leftv[iv].first;
+                       for(unsigned int jv = 0; jv < nright; ++jv) {
+                         const int j = rightv[jv].first;
+                         if (!sym || (sym && i<=j)) result(i,j) += r(iv,jv);
+                       }
+                   }
+                   mutex->unlock();
+               }
+           }
+       }
 
-                        for (int jv=0; jv<nright; jv++) {
-                            const int j = rightv[jv].first;
-                            const GenTensor<R>* jptr = rightv[jv].second;
-
-                            if (!sym || (sym && i<=j))
-                                r(i,j) += iptr->trace_conj(*jptr);
-                        }
-                    }
-                }
-            }
-            mutex->lock();
-            result += r;
-            mutex->unlock();
-        }
-
-        static double conj(double x) {
-            return x;
-        }
 
         static double conj(float x) {
             return x;
