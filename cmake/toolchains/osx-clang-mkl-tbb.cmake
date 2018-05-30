a01cc020689459637678160c640786fd0eb8ae0a
Eduard Valeyev <eduard@valeyev.net>
2018-05-30 11:56:40 -0400

    updated toolchains to provide "new" LAPACK variables

--- cmake/toolchains/osx-clang-mkl-tbb.cmake
+++ cmake/toolchains/osx-clang-mkl-tbb.cmake
@@ -48,5 +48,5 @@ endif()
 set(LAPACK_LIBRARIES "-L${MKL_ROOT_DIR}/lib" "-lmkl_intel_lp64" "-lmkl_core" 
     "-lmkl_sequential" "-lm" CACHE STRING "LAPACK linker flags")
 set(LAPACK_INCLUDE_DIRS ${MKL_ROOT_DIR}/include CACHE STRING "LAPACK include directories")
-set(LAPACK_COMPILE_DEFINITIONS MADNESS_LINALG_USE_LAPACKE=1 HAVE_INTEL_MKL=1 CACHE STRING "LAPACK preprocessor definitions")
+set(LAPACK_COMPILE_DEFINITIONS HAVE_INTEL_MKL=1 CACHE STRING "LAPACK preprocessor definitions")
 set(INTEGER4 TRUE CACHE BOOL "Set Fortran integer size to 4 bytes")
