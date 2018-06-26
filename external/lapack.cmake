9cfad4f99313729856c1ac2f45dcf5fdbc97fec8
Eduard Valeyev <eduard@valeyev.net>
2018-06-26 13:52:10 -0400

    made processing of LAPACK_LIBRARIES more robust

--- external/lapack.cmake
+++ external/lapack.cmake
@@ -46,8 +46,16 @@ endif()
 
 cmake_push_check_state()
 
-set(CMAKE_REQUIRED_LIBRARIES ${LAPACK_LIBRARIES} ${CMAKE_REQUIRED_LIBRARIES} 
-    ${CMAKE_THREAD_LIBS_INIT})
+# process LAPACK_LIBRARIES for CMAKE_REQUIRED_LIBRARIES (this is likely only to work with Makefile generator):
+# 1. get rid of the surrounding quotes
+string(REGEX REPLACE "\"" "" PROCESSED_LAPACK_LIBRARIES "${LAPACK_LIBRARIES}")
+# 2. convert a space-separated string of libs into a list
+string(REGEX REPLACE " " ";" PROCESSED_LAPACK_LIBRARIES "${PROCESSED_LAPACK_LIBRARIES}")
+# 3. restore (and protect!) the space in "-framework X"
+string(REGEX REPLACE "-framework;(.*)" "-framework\\\\ \\1" PROCESSED_LAPACK_LIBRARIES "${PROCESSED_LAPACK_LIBRARIES}")
+#message(STATUS "PROCESSED_LAPACK_LIBRARIES=${PROCESSED_LAPACK_LIBRARIES}")
+set(CMAKE_REQUIRED_LIBRARIES ${PROCESSED_LAPACK_LIBRARIES} ${CMAKE_REQUIRED_LIBRARIES}
+        ${CMAKE_THREAD_LIBS_INIT})
 
 # Verify that we can link against BLAS
 check_c_fortran_function_exists(sgemm BLAS_WORKS)
@@ -55,7 +63,7 @@ check_c_fortran_function_exists(sgemm BLAS_WORKS)
 if(BLAS_WORKS)
   message(STATUS "A library with BLAS API found.")
 else()
-  message(FATAL_ERROR "Uable to link against BLAS function. Specify the BLAS library in LAPACK_LIBRARIES.")
+  message(FATAL_ERROR "Unable to link against BLAS function. Specify the BLAS library in LAPACK_LIBRARIES.")
 endif()
 
 # Verify that we can link against LAPACK
@@ -64,7 +72,7 @@ check_c_fortran_function_exists(cheev LAPACK_WORKS)
 if(LAPACK_WORKS)
   message(STATUS "A library with LAPACK API found.")
 else()
-  message(FATAL_ERROR "Uable to link against LAPACK function. Specify the LAPACK library in LAPACK_LIBRARIES.")
+  message(FATAL_ERROR "Unable to link against LAPACK function. Specify the LAPACK library in LAPACK_LIBRARIES.")
 endif()
 
 set(LAPACK_FOUND TRUE)
