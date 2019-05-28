b9dc4c5cf31077f3a0469414a9ee7687676014f1
Eduard Valeyev <eduard@valeyev.net>
2019-05-28 13:52:48 -0400

    - gperftools and libunbwind search can be hinted via environment - always search for libunwind

--- external/libunwind.cmake
+++ external/libunwind.cmake
@@ -1,10 +1,6 @@
-if(ENABLE_TASK_PROFILER)
-
-  find_package(Libunwind)
+find_package(Libunwind)
     
-  # Set the output variables
-  if(LIBUNWIND_FOUND)
-    set(MADNESS_HAS_LIBUNWIND 1)
-  endif()
-      
+# Set the output variables
+if(LIBUNWIND_FOUND)
+  set(MADNESS_HAS_LIBUNWIND 1)
 endif()
