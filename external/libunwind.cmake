4de9d425ad91e52102114b13e66985f9a0005958
Eduard Valeyev <eduard@valeyev.net>
2019-05-28 15:28:48 -0400

    partially reverted b9dc4c5cf31077f3a0469414a9ee7687676014f1

--- external/libunwind.cmake
+++ external/libunwind.cmake
@@ -1,6 +1,10 @@
-find_package(Libunwind)
+if(ENABLE_TASK_PROFILER)
+
+  find_package(Libunwind)
     
-# Set the output variables
-if(LIBUNWIND_FOUND)
-  set(MADNESS_HAS_LIBUNWIND 1)
+  # Set the output variables
+  if(LIBUNWIND_FOUND)
+    set(MADNESS_HAS_LIBUNWIND 1)
+  endif()
+      
 endif()
