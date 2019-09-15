d5b4a2aa5d44db9f0a1d60a3755c1dcfa6a533e4
Jeff Hammond <jeff.science@gmail.com>
2019-09-14 18:43:48 -0700

    try to install MKL via APT
    Signed-off-by: Jeff Hammond <jeff.science@gmail.com>

--- ci/dep-linux.sh
+++ ci/dep-linux.sh
@@ -113,3 +113,12 @@ else
     find ${HOME}/mpich -name mpicxx
 fi
 
+# Do not exit on error because MKL is optional
+set +e
+
+# Install MKL
+wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
+sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
+sudo sh -c 'echo deb https://apt.repos.intel.com/mkl all main > /etc/apt/sources.list.d/intel-mkl.list'
+sudo apt-get update
+sudo apt-get install intel-mkl-2019.4-070
