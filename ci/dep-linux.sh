8d89325fca59c9242984900b4769f53c6fb56237
Eduard Valeyev <eduard@valeyev.net>
2019-09-16 08:42:28 -0400

    more libxc download tweaks

--- ci/dep-linux.sh
+++ ci/dep-linux.sh
@@ -79,7 +79,7 @@ fi
 # Install libxc
 if [ ! -f "${HOME}/libxc/lib/libxc.a" ]; then
     export LIBXC_VERSION=4.3.4
-    wget -O libxc-${LIBXC_VERSION} "https://gitlab.com/libxc/libxc/-/archive/${LIBXC_VERSION}/libxc-${LIBXC_VERSION}.tar.gz"
+    wget -O libxc-${LIBXC_VERSION}.tar.gz "https://gitlab.com/libxc/libxc/-/archive/${LIBXC_VERSION}/libxc-${LIBXC_VERSION}.tar.gz"
     tar -xzf libxc-${LIBXC_VERSION}.tar.gz
     ls -l
     cd libxc-${LIBXC_VERSION}
