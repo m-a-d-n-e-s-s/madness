025d1cd07d1428050370fe61a62a27c942e583d1
Eduard Valeyev <eduard@valeyev.net>
2020-01-17 19:08:38 -0500

    cleanup

--- external/pthread.cmake
+++ external/pthread.cmake
@@ -3,8 +3,3 @@ if (NOT TARGET Threads::Threads)
   set(CMAKE_THREAD_PREFER_PTHREAD TRUE)
   find_package(Threads REQUIRED)
 endif()
-
-# Check that pthreads was found
-if(NOT TARGET Threads::Threads)
-  message(FATAL_ERROR "MADNESS requires pthreads support.")
-endif()
