8fad0c5a2e0816a21ffd206cd3e2e00b83919f48
Murat Keceli <keceli@gmail.com>
2020-02-17 03:59:59 +0000

    Install Cereal for CI

--- ci/dep-linux.sh
+++ ci/dep-linux.sh
@@ -123,3 +123,13 @@ sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
 sudo sh -c 'echo deb https://apt.repos.intel.com/mkl all main > /etc/apt/sources.list.d/intel-mkl.list'
 sudo apt-get update
 sudo apt-get install intel-mkl-2019.4-070
+
+# Install Cereal
+cd
+git clone https://github.com/USCiLab/cereal
+cd cereal
+mkdir build
+cd build
+cmake -DSKIP_PORTABILITY_TEST=ON ..
+make
+make install
