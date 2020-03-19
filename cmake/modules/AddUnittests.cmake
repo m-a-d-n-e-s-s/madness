e07915b2b43c312b165eec0f34b14d227099196c
Eduard Valeyev <eduard@valeyev.net>
2020-03-19 15:38:21 -0400

    prototyped changes to test builds that do not require no-PIE flags ... mac only right now, but test_ar works!

--- cmake/modules/AddUnittests.cmake
+++ cmake/modules/AddUnittests.cmake
@@ -3,7 +3,7 @@ macro(add_unittests _component _sources _libs)
   # Add targets and for world_unittests
   add_custom_target(${_component}_unittests)
   add_dependencies(unittests ${_component}_unittests)
-    
+
   # Add a test that builds the unit tests
   add_test(build_${_component}_unittests
       "${CMAKE_COMMAND}" --build ${PROJECT_BINARY_DIR} --target ${_component}_unittests)
@@ -15,10 +15,18 @@ macro(add_unittests _component _sources _libs)
     get_filename_component(_test "${_test_source}" NAME_WE)
     
     # Create test executables
-    add_executable(${_test} EXCLUDE_FROM_ALL ${_source_list})
-    target_link_libraries(${_test} ${_libs} "${DISABLEPIE_LINKER_FLAG}")
-    
-    # Add the test and set dependencies
+    add_library(${_test}-lib EXCLUDE_FROM_ALL ${_source_list})
+    foreach(_lib ${_libs})
+      set(libtarget ${_lib})
+      if (_lib MATCHES "^MAD" AND BUILD_SHARED_LIBS AND TARGET ${_lib}-static)
+        set(libtarget -Wl,-all_load ${_lib}-static)
+      endif(_lib MATCHES "^MAD")
+      target_link_libraries(${_test}-lib PRIVATE ${libtarget})
+    endforeach(_lib ${_libs})
+    add_executable(${_test} EXCLUDE_FROM_ALL ${PROJECT_SOURCE_DIR}/src/madness/misc/exec_stub.cc)
+    target_link_libraries(${_test} ${_test}-lib)
+
+        # Add the test and set dependencies
     add_test(NAME ${_component}-${_test} COMMAND ${_test})
     add_dependencies(${_component}_unittests ${_test})
     set_tests_properties(${_component}-${_test} 
