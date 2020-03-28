6f2619864c8aeed1b8d598354bb04bf77673df68
Eduard Valeyev <eduard@valeyev.net>
2020-03-28 14:20:20 -0400

    [travis] try discovering Intel TBB v7

--- ci/dep-linux.sh
+++ ci/dep-linux.sh
@@ -147,6 +147,6 @@ sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
 sudo sh -c 'echo deb https://apt.repos.intel.com/mkl all main > /etc/apt/sources.list.d/intel-mkl.list'
 sudo sh -c 'echo deb https://apt.repos.intel.com/tbb all main > /etc/apt/sources.list.d/intel-tbb.list'
 sudo apt-get update
-sudo apt-get install intel-mkl
-sudo apt-get install intel-tbb
+sudo apt-get install intel-mkl-2020.1-102
+sudo apt-get install intel-tbb-2020.2-102
 
