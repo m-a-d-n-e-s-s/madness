a994ee59017a97a3a7cb397021890321c470ecc4
Robert J. Harrison <rjharrison@gmail.com>
2020-08-21 11:00:34 -0400

    nearly there!

--- src/madness/world/test_pdca.cc
+++ src/madness/world/test_pdca.cc
@@ -25,34 +25,42 @@ void test_multi_work(madness::World& universe, madness::World& subworld, madness
     long key = 1+(universe.rank()%2);
     int value;
     {
+        madness::WorldContainer<int,double> data(subworld);
         madness::archive::ContainerRecordInputArchive ar(subworld,container,key);
         madness::archive::ParallelInputArchive<madness::archive::ContainerRecordInputArchive> par(subworld, ar);
-        par & value;
+        par & value & data;
         std::cout << subworld.id() << " : subworld read process " << subworld.rank() << " read " << value << std::endl;
     }
     {
+        madness::WorldContainer<int,double> subdata(subworld);
+        subdata.replace(subworld.rank(), subworld.rank()*100.0);
+        subworld.gop.fence();
+        
         long newkey = key*1000;
         madness::archive::ContainerRecordOutputArchive ar(subworld,container,newkey);
         madness::archive::ParallelOutputArchive<madness::archive::ContainerRecordOutputArchive> par(subworld, ar);
-        par & value*1000;
+        par & value*1000 & subdata;
     }
 }
 
 void test_multi(madness::World& universe) {
 
+    // For testing passing of containers from universe to subworld
+    madness::WorldContainer<int,double> data(universe);
+    data.replace(universe.rank(), universe.rank()*100.0);
+    universe.gop.fence();
+
     // From universe put data into container
     madness::WorldContainer<long,std::vector<unsigned char>> container(universe);
-    if (universe.rank() == 0) {
-        {
-            madness::archive::ContainerRecordOutputArchive ar(universe,container,1);
-            madness::archive::ParallelOutputArchive<madness::archive::ContainerRecordOutputArchive> par(universe, ar);
-            par & 1;
-        }
-        {
-            madness::archive::ContainerRecordOutputArchive ar(universe,container,2);
-            madness::archive::ParallelOutputArchive<madness::archive::ContainerRecordOutputArchive> par(universe, ar);
-            par & 2;
-        }
+    {
+        madness::archive::ContainerRecordOutputArchive ar(universe,container,1);
+        madness::archive::ParallelOutputArchive<madness::archive::ContainerRecordOutputArchive> par(universe, ar);
+        par & 1 & data;
+    }
+    {
+        madness::archive::ContainerRecordOutputArchive ar(universe,container,2);
+        madness::archive::ParallelOutputArchive<madness::archive::ContainerRecordOutputArchive> par(universe, ar);
+        par & 2 & data;
     }
     universe.gop.fence(); // Rank 0 must finish writing before workers start
 
@@ -67,22 +75,22 @@ void test_multi(madness::World& universe) {
 
     // In universe read back results from subworld computations
     {
+        madness::WorldContainer<int,double> subdata(universe);
         madness::archive::ContainerRecordInputArchive ar(universe,container,1000);
         madness::archive::ParallelInputArchive<madness::archive::ContainerRecordInputArchive> par(universe, ar);
         int value;
-        par & value;
+        par & value & subdata;
         madness::print("first result", value);
     }
     {
+        madness::WorldContainer<int,double> subdata(universe);
         madness::archive::ContainerRecordInputArchive ar(universe,container,2000);
         madness::archive::ParallelInputArchive<madness::archive::ContainerRecordInputArchive> par(universe, ar);
         int value;
-        par & value;
+        par & value & subdata;
         madness::print("second result", value);
     }
     universe.gop.fence();
-
-        
 }
 
 int main(int argc, char** argv) {
