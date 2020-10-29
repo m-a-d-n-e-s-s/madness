cf9d638d8a3779abdfb68db3615689f492b0b7d0
ahurta92 <ahurta92@gmail.com>
2020-10-29 13:02:10 -0400

    move properties definitions to cc file

--- src/apps/adrian/property_functions.h
+++ src/apps/adrian/property_functions.h
@@ -6,7 +6,6 @@
 #include <string>
 #include <vector>
 
-#include "../../madness/mra/funcplot.h"
 #include "adrian/property_operators.h"
 
 typedef Tensor<double> TensorT;
@@ -16,47 +15,13 @@ typedef FunctionFactory<double, 3> FactoryT;
 typedef Vector<double, 3> CoordinateT;
 typedef std::vector<real_function_3d> VectorFunction3DT;
 
-namespace madness {
 // returns tensor alpha_ij=inner(rho_b,op_c)
 Tensor<double> ComputeSecondOrderPropertyTensor(World &world,
                                                 const VectorFunction3DT &rho_b,
-                                                const Property &op_c) {
-  return matrix_inner(world, rho_b, op_c.operator_vector, true);
-}
+                                                const Property &op_c);
 
 void PrintSecondOrderAnalysis(World &world, const Tensor<double> alpha_tensor,
                               const Tensor<double> omega,
-                              const ResponseParameters Rparams) {
-  Tensor<double> V, epolar;
-  syev(alpha_tensor, V, epolar);
-  double Dpolar_average = 0.0;
-  double Dpolar_iso = 0.0;
-  for (unsigned int i = 0; i < 3; ++i)
-    Dpolar_average = Dpolar_average + epolar[i];
-  Dpolar_average = Dpolar_average / 3.0;
-  Dpolar_iso =
-      sqrt(.5) * sqrt(std::pow(alpha_tensor(0, 0) - alpha_tensor(1, 1), 2) +
-                      std::pow(alpha_tensor(1, 1) - alpha_tensor(2, 2), 2) +
-                      std::pow(alpha_tensor(2, 2) - alpha_tensor(0, 0), 2));
-
-  int num_states = Rparams.states;
-
-  if (world.rank() == 0) {
-    print("\nTotal Dynamic Polarizability Tensor");
-    printf("\nFrequency  = %.6f a.u.\n\n", omega(0, 0));
-    // printf("\nWavelength = %.6f a.u.\n\n", Rparams.omega * ???);
-    print(alpha_tensor);
-    printf("\tEigenvalues = ");
-    printf("\t %.6f \t %.6f \t %.6f \n", epolar[0], epolar[1], epolar[2]);
-    printf("\tIsotropic   = \t %.6f \n", Dpolar_average);
-    printf("\tAnisotropic = \t %.6f \n", Dpolar_iso);
-    printf("\n");
-
-    for (long i = 0; i < num_states; i++) {
-      print(epolar[i]);
-    }
-  }
-}
-}  // namespace madness
+                              const ResponseParameters Rparams);
 
 #endif  // SRC_APPS_ADRIAN_PROPERTY_FUNCTIONS_H_
--- src/apps/adrian/property_operators.h
+++ src/apps/adrian/property_operators.h
@@ -11,14 +11,15 @@
 #include <vector>
 
 #include "../../madness/mra/funcplot.h"
-namespace madness {
-// Type definitions
-typedef Tensor<double> tensorT;
-typedef Function<double, 3> functionT;
-typedef std::shared_ptr<FunctionFunctorInterface<double, 3>> functorT;
-typedef FunctionFactory<double, 3> factoryT;
-typedef Vector<double, 3> coordT;
 
+// Type definitions
+
+typedef Tensor<double> TensorT;
+typedef Function<double, 3> FunctionT;
+typedef std::shared_ptr<FunctionFunctorInterface<double, 3>> FunctorT;
+typedef FunctionFactory<double, 3> FactoryT;
+typedef Vector<double, 3> CoordinateT;
+typedef std::vector<real_function_3d> VectorFunction3DT;
 //
 class MolecularDerivativeFunctor : public FunctionFunctorInterface<double, 3> {
   typedef Vector<double, 3> coordT;
@@ -29,17 +30,9 @@ class MolecularDerivativeFunctor : public FunctionFunctorInterface<double, 3> {
   const int axis;
 
  public:
-  MolecularDerivativeFunctor(const Molecule &molecule, int atom, int axis)
-      : molecule(molecule), atom(atom), axis(axis) {}
-
-  double operator()(const coordT &x) const {
-    return molecule.nuclear_attraction_potential_derivative(atom, axis, x[0],
-                                                            x[1], x[2]);
-  }
-
-  std::vector<coordT> special_points() const {
-    return std::vector<coordT>(1, molecule.get_atom(atom).get_coords());
-  }
+  MolecularDerivativeFunctor(const Molecule &molecule, int atom, int axis);
+  double operator()(const coordT &x) const;
+  std::vector<coordT> special_points() const;
 };  // namespace madness
 // A proerty class...creates a object with operator vector and property name
 // Used to compute proerties or compute rhs vectors
@@ -50,53 +43,18 @@ class Property {
   std::vector<real_function_3d> operator_vector;
 
   // default constructor
-  Property() : num_operators(0), property(""), operator_vector() {}
-  Property(World &world, std::string property_type)
-      : num_operators(3), operator_vector(num_operators) {
-    property = property_type;
-    MADNESS_ASSERT(property.compare("dipole") == 0);
-    for (int i = 0; i < 3; i++) {
-      std::vector<int> f(3, 0);
-      f[i] = 1;
-      operator_vector.at(i) = real_factory_3d(world).functor(
-          real_functor_3d(new BS_MomentFunctor(f)));
-    }
-  }
+  Property();
+  Property(World &world, std::string property_type);
 
-  Property(World &world, std::string property_type, Molecule molecule)
-      : num_operators(molecule.natom() * 3), operator_vector(num_operators) {
-    property = property_type;
-    MADNESS_ASSERT(property.compare("nuclear") == 0);
-
-    vecfuncT dv(molecule.natom() * 3);  // default constructor for vector?
-
-    for (size_t atom = 0; atom < molecule.natom(); ++atom) {
-      for (int axis = 0; axis < 3; ++axis) {
-        // question here....MolecularDerivativeFunctor takes derivative with
-        // respect to axis atom and axis
-        functorT func(new MolecularDerivativeFunctor(molecule, atom, axis));
-        // here we save
-        operator_vector.at(atom * 3 + axis) =
-            functionT(factoryT(world)
-                          .functor(func)
-                          .nofence()
-                          .truncate_on_project()
-                          .truncate_mode(0));
-
-        print("norm of vector ", operator_vector[atom * 3 + axis].norm2());
-        // need to project
-        //        operator_vector[atom * 3 + axis] = mul_sparse(
-        //           world, dv[atom * 3 + axis], Gparams.orbitals,
-        //           Rparams.small);
-
-        // project rhs vectors for state
-
-        // core projector contribution
-      }
-    }
-  }
+  Property(World &world, std::string property_type, Molecule molecule);
 };
+// returns tensor alpha_ij=inner(rho_b,op_c)
+Tensor<double> ComputeSecondOrderPropertyTensor(World &world,
+                                                const VectorFunction3DT &rho_b,
+                                                const Property &op_c);
 
-}  // namespace madness
+void PrintSecondOrderAnalysis(World &world, const Tensor<double> alpha_tensor,
+                              const Tensor<double> omega,
+                              const ResponseParameters Rparams);
 
 #endif  // SRC_APPS_ADRIAN_PROPERTY_OPERATORS_H_
