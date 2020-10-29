7ed9576648bd0fd04902d50f87e81f2421ba7b9f
ahurta92 <ahurta92@gmail.com>
2020-10-29 13:03:10 -0400

    Read input outside of TDHF...Rparams will see up multiple objects

--- src/apps/adrian/densityTest.cc
+++ src/apps/adrian/densityTest.cc
@@ -1,17 +1,10 @@
 
-//
-// Created by adrian on 4/19/20.
-//
-/*
- *    Written by: bsundahl
- *    Date: A long time ago...
- *
- */
 
 #include <stdlib.h>
 
 #include "TDDFT.h"  // All response functions/objects enter through this
 #include "adrian/density.h"
+#include "adrian/global_functions.h"
 #include "adrian/property_functions.h"
 
 #if defined(HAVE_SYS_TYPES_H) && defined(HAVE_SYS_STAT_H) && \
@@ -24,7 +17,6 @@ static inline int file_exists(const char* inpname) {
   int rc = stat(inpname, &buffer);
   return (rc == 0);
 }
-
 #endif
 
 int main(int argc, char** argv) {
@@ -35,22 +27,52 @@ int main(int argc, char** argv) {
 
   // This makes a default input file name of 'input'
   const char* input = "input";
+
   for (int i = 1; i < argc; i++) {
     if (argv[i][0] != '-') {
       input = argv[i];
       break;
     }
   }
+
   if (!file_exists(input)) throw "input file not found";
+  // first step is to read the input for Rparams and Gparams
+  std::shared_ptr<std::istream> shared_input =
+      std::make_shared<std::ifstream>(input);
+  ResponseParameters Rparams;
+  GroundParameters Gparams;
+
+  if (world.rank() == 0) {
+    if (shared_input->fail())
+      MADNESS_EXCEPTION("Response failed to open input stream", 0);
+    // Welcome user (future ASCII art of Robert goes here)
+    print("\n   Preparing to solve the TDHF equations.\n");
+    // Read input files
+    Rparams.read(*shared_input);
+    // Print out what was read in
+  }
+  Gparams.read(world, Rparams.archive);
+  if (world.rank() == 0) {
+    Gparams.print_params();
+    print_molecule(world, Gparams);
+  }
+  // if a proerty calculation set the number of states
+  if (Rparams.property) {
+    Rparams.SetNumberOfStates(Gparams.molecule);
+  }
+
+  // print params
+  if (world.rank() == 0) {
+    Rparams.print_params();
+  }
+  // Broadcast to all other nodes
 
   // Create the TDHF object
 
-  FirstOrderDensity densityTest(world, input);
+  FirstOrderDensity densityTest(world, Rparams, Gparams);
   //
   densityTest.PlotResponseDensity(world);
   densityTest.PrintDensityInformation();
-
-  ResponseParameters Rparams = densityTest.GetResponseParameters();
 
   if (Rparams.property) {  //
 
