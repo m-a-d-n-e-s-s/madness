045fc1a632c48c36233aca5a0e6413b4e4c239e5
ahurta92 <ahurta92@gmail.com>
2020-10-29 13:01:35 -0400

    Create global functions used in any file

---
+++ src/apps/adrian/global_functions.h
@@ -0,0 +1,12 @@
+#ifndef SRC_APPS_ADRIAN_GLOBAL_FUNCTIONS_H_
+#define SRC_APPS_ADRIAN_GLOBAL_FUNCTIONS_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "ResponseParameters.h"
+#include "adrian/density.h"
+
+void print_molecule(World &world, GroundParameters Gparams);
+#endif  // SRC_APPS_ADRIAN_GLOBAL_FUNCTIONS_H_
