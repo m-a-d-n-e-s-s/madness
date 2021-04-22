4380c7d517032a6b47e2aebacc31c5d56405e21b
fbischoff <florian.bischoff@hu-berlin.de>
2021-04-22 09:59:54 +0200

    fixed CalculationParameter check

--- src/apps/chem/CalculationParameters.h
+++ src/apps/chem/CalculationParameters.h
@@ -323,7 +323,7 @@ struct CalculationParameters : public QCCalculationParametersBase {
         }
 
         //NWChem interface doesn't support geometry optimization
-        if (get<bool>("gopt") && nwfile() != "") error("NWchem initialization only supports single point energy calculations.");
+        if (get<bool>("gopt") && nwfile() != "none") error("NWchem initialization only supports single point energy calculations.");
 
         //NWChem only supports Boys localization (or canonical)
         if (nwfile() != "none") {
