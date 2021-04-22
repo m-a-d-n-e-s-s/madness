0b282e29b11e5d62bf2e3cf08019dbf350fb72ed
fbischoff <florian.bischoff@hu-berlin.de>
2021-04-22 22:37:06 +0200

    fixed reading error in mp2

--- src/apps/chem/mp2.h
+++ src/apps/chem/mp2.h
@@ -269,10 +269,7 @@ namespace madness {
 
     public:
     	/// default ctor; initialize energies with a large number
-    	ElectronPair()
-    		: i(-1), j(-1), e_singlet(uninitialized()), e_triplet(uninitialized()),
-    			ij_gQf_ij(uninitialized()), ji_gQf_ij(uninitialized()), iteration(0), converged(false) {
-    	}
+    	ElectronPair() : ElectronPair(-1,-1) {}
 
     	/// ctor; initialize energies with a large number
     	ElectronPair(const int i, const int j)
@@ -324,11 +321,11 @@ namespace madness {
             	if (world.rank()==0) printf("loading matrix elements %s",name.c_str());
                 archive::ParallelInputArchive ar(world, name.c_str(), 1);
                 ar & *this;
-            	if (world.rank()==0) printf(" %s\n",(converged)?" converged":" not converged");
-            	function.set_thresh(FunctionDefaults<6>::get_thresh());
-                constant_term.set_thresh(FunctionDefaults<6>::get_thresh());
+                if (world.rank()==0) printf(" %s\n",(converged)?" converged":" not converged");
+                if (function.is_initialized()) function.set_thresh(FunctionDefaults<6>::get_thresh());
+                if (constant_term.is_initialized()) constant_term.set_thresh(FunctionDefaults<6>::get_thresh());
             } else {
-		    	if (world.rank()==0) print("could not find pair ",i,j," on disk");
+                if (world.rank()==0) print("could not find pair ",i,j," on disk");
             }
             return exists;
         }
