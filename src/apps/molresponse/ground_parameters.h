3ba261f2aa522b5eeef53128fdff94ecca51b971
ahurta92 <ahurta92@gmail.com>
2021-05-12 18:51:26 -0400

    first attempt ground_params...going to go back

--- src/apps/molresponse/ground_parameters.h
+++ src/apps/molresponse/ground_parameters.h
@@ -6,9 +6,11 @@
 #ifndef MADNESS_APPS_GROUNDPARAMS_H_INCLUDED
 #define MADNESS_APPS_GROUNDPARAMS_H_INCLUDED
 
+#include <chem/QCCalculationParametersBase.h>
+
+#include "../chem/NWChem.h"  // For nwchem interface
 #include "../chem/SCFOperators.h"
 #include "../chem/molecule.h"
-#include "../chem/NWChem.h"  // For nwchem interface
 #include "Plot_VTK.h"
 #include "basic_operators.h"
 #include "chem/potentialmanager.h"
@@ -17,19 +19,102 @@
 
 namespace madness {
 
+#if 1
+
+struct GroundParameters : public QCCalculationParametersBase {
+  GroundParameters(const GroundParameters& other) = default;
+
+  GroundParameters() {
+    initialize<std::string>("inFile", "restart", "ground state restart file");
+    initialize<bool>("spinrestricted", false, "turn on plotting of final orbitals. Output format is .vts");
+    initialize<unsigned int>("num_orbitals", false, "number of ground state orbitals");
+    initialize<Tensor<double>>("energies", {}, "Energy of ground state orbitals");
+    initialize<Tensor<double>>("occ", {}, "Occupancy of ground state orbitals");
+    initialize<double>("L", {}, "Box size of ground state");
+    initialize<int>("k", {}, "Order of polynomial used in ground state");
+    initialize<Molecule>("molecule", {}, "The molecule used in ground state calc");
+    initialize<vector_real_function_3d>("orbitals", {}, "ground state orbitals");
+    initialize<std::string>("xc", "hf", "xc funcitonal used in ground state");
+  }
+
+ public:
+  using QCCalculationParametersBase::read;
+
+  bool spinrestricted() const { return get<bool>("spinrestricted"); }
+  unsigned int num_orbitals() const { return get<unsigned int>("num_orbitals"); }
+  Tensor<double> energies() const { return get<Tensor<double>>("energies"); }
+  Tensor<double> occ() const { return get<Tensor<double>>("occ"); }
+  double L() const { return get<double>("L"); }
+  int k() const { return get<int>("k"); }
+  Molecule molecule() const { return get<Molecule>("molecule"); }
+  vector_real_function_3d orbitals() const { return get<vector_real_function_3d>("orbitals"); }
+  std::string xc() const { return get<std::string>("xc"); }
+
+  void read(World& world, const std::string& filename) {
+    // Save the filename
+    inFile = filename;
+
+    unsigned int dummyversion;
+    double dummy1;
+    std::vector<int> dummy2;
+
+    archive::ParallelInputArchive input(world, filename.c_str());
+    input& dummyversion;
+    input& dummy1;          // double
+    input& spinrestricted;  // bool
+    input& num_orbitals;    // int
+    input& energies;        // Tensor<double>    orbital energies
+    input& occ;             // Tensor<double>    orbital occupations
+    input& dummy2;          // std::vector<int>  sets of orbitals(?)
+    input& L;               // double            box size
+    input& k;               // int               wavelet order
+    input& molecule;        // Molecule
+    input& xc;              // std:string        xc functional
+
+    // Check that order is positive and less than 30
+    if (k < 1 or k > 30) {
+      if (world.rank() == 0)
+        print(
+            "\n   ***PLEASE NOTE***\n   Invalid wavelet order read from "
+            "archive, setting to 8.\n   This seems to happen when the default "
+            "wavelet order is used in moldft.");
+      k = 8;
+    }
+
+    // Set this so we can read in whats
+    // written in the archive
+    FunctionDefaults<3>::set_k(k);
+
+    // Possible to call this function multiple times now
+    // Do this to ensure everything works.
+    orbitals.clear();
+
+    // Read in ground state orbitals
+    for (unsigned int i = 0; i < num_orbitals; i++) {
+      real_function_3d reader;
+      input& reader;
+      orbitals.push_back(reader);
+    }
+
+    // Clean up
+    truncate(world, orbitals);
+  }
+};
+
+#else
 struct GroundParameters {
   // Ground state parameters that are read in from archive
-  std::string inFile;   ///< Name of input archive to read in ground state
-  bool spinrestricted;  ///< Indicates if ground state calc. was open or closed
-                        ///< shell
-  unsigned int num_orbitals;  ///< Number of orbitals in ground state
-  Tensor<double> energies;    ///< Energy of ground state orbitals
-  Tensor<double> occ;         ///< Occupancy of ground state orbitals
-  double L;  ///< Box size of ground state - response calcluation is in same box
-  int k;     ///< Order of polynomial used in ground state
-  Molecule molecule;  ///< The molecule used in ground state calculation
+  std::string inFile;                      ///< Name of input archive to read in ground state
+  bool spinrestricted;                     ///< Indicates if ground state calc. was open or closed
+                                           ///< shell
+  unsigned int num_orbitals;               ///< Number of orbitals in ground state
+  Tensor<double> energies;                 ///< Energy of ground state orbitals
+  Tensor<double> occ;                      ///< Occupancy of ground state orbitals
+  double L;                                ///< Box size of ground state - response calcluation is in same box
+  int k;                                   ///< Order of polynomial used in ground state
+  Molecule molecule;                       ///< The molecule used in ground state calculation
   std::vector<real_function_3d> orbitals;  ///< The ground state orbitals
-  std::string xc;  ///< Name of xc functional used in ground state
+  std::string xc;                          ///< Name of xc functional used in ground state
 
   // Default constructor
   GroundParameters() {}
