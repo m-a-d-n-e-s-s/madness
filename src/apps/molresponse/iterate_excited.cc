d84d1d079b32250c8de96eb6a0f32c469160b7c8
ahurta92 <ahurta92@gmail.com>
2021-05-19 09:06:32 -0400

    new params

--- src/apps/molresponse/iterate_excited.cc
+++ src/apps/molresponse/iterate_excited.cc
@@ -8,9 +8,9 @@
 #include <string>
 #include <utility>
 
+#include "../chem/NWChem.h"  // For nwchem interface
 #include "../chem/SCFOperators.h"
 #include "../chem/molecule.h"
-#include "../chem/NWChem.h"  // For nwchem interface
 #include "Plot_VTK.h"
 #include "TDDFT.h"
 #include "chem/potentialmanager.h"
@@ -32,25 +32,25 @@ void TDDFT::solve_excited_states(World& world) {
   molresponse::start_timer(world);
 
   // Plotting input orbitals
-  if (Rparams.plot_initial) {
+  if (r_params.plot_initial()) {
     if (world.rank() == 0) print("\n   Plotting ground state densities.\n");
-    if (Rparams.plot_L > 0.0)
+    if (r_params.plot_L() > 0.0)
       do_vtk_plots(world,
-                   Rparams.plot_pts,
-                   Rparams.plot_L,
+                   r_params.plot_pts(),
+                   r_params.plot_L(),
                    0,
-                   Gparams.num_orbitals,
-                   Gparams.molecule,
-                   square(world, Gparams.orbitals),
+                   r_params.num_orbitals(),
+                   molecule,
+                   square(world, ground_orbitals),
                    "ground");
     else
       do_vtk_plots(world,
-                   Rparams.plot_pts,
-                   Gparams.L / 2.0,
+                   r_params.plot_pts(),
+                   r_params.L() / 2.0,
                    0,
-                   Gparams.num_orbitals,
-                   Gparams.molecule,
-                   square(world, Gparams.orbitals),
+                   r_params.num_orbitals(),
+                   molecule,
+                   square(world, ground_orbitals),
                    "ground");
   }
 
@@ -62,75 +62,67 @@ void TDDFT::solve_excited_states(World& world) {
   // Here print the relevant parameters
 
   // Ready to iterate!
-  for (unsigned int proto = 0; proto < Rparams.protocol_data.size(); proto++) {
+  for (unsigned int proto = 0; proto < r_params.protocol().size(); proto++) {
     // Set defaults inside here
-    set_protocol<3>(world, Rparams.protocol_data[proto]);
+    set_protocol<3>(world, r_params.protocol()[proto]);
 
     // Do something to ensure all functions have same k value
-    check_k(world, Rparams.protocol_data[proto], FunctionDefaults<3>::get_k());
+    check_k(world, r_params.protocol()[proto], FunctionDefaults<3>::get_k());
 
     // Create the active subspace (select which ground state orbitals to
     // calculate excitations from)
-    // if(Rparams.e_window) select_active_subspace(world);
+    // if(r_params.e_window) select_active_subspace(world);
 
     if (proto == 0) {
-      if (Rparams.restart) {
+      if (r_params.restart()) {
         if (world.rank() == 0) {
-          print("   Restarting from file:", Rparams.restart_file);
+          print("   Restarting from file:", r_params.restart_file());
         }
-        load(world, Rparams.restart_file);
-        check_k(
-            world, Rparams.protocol_data[proto], FunctionDefaults<3>::get_k());
+        load(world, r_params.restart_file());
+        check_k(world, r_params.protocol()[proto], FunctionDefaults<3>::get_k());
       } else {
         // Create trial functions by...
         // (Always creating (at least) twice the amount requested for
         // initial diagonalization)
         if (world.rank() == 0) print("\n   Creating trial functions.\n");
-        if (Rparams.random) {
+        if (r_params.random()) {
           // Random guess
-          Chi.X = create_random_guess(world,
-                                           2 * Rparams.states,
-                                           Gparams.num_orbitals,
-                                           Gparams.orbitals,
-                                           Gparams.molecule);
-        } else if (Rparams.nwchem != "") {
+          Chi.X =
+              create_random_guess(world, 2 * r_params.n_states(), r_params.num_orbitals(), ground_orbitals, molecule);
+        } else if (r_params.nwchem() != "") {
           // Virtual orbitals from NWChem
-          Chi.X = create_nwchem_guess(world, 2 * Rparams.states);
-        } else if (Rparams.guess_xyz) {
+          Chi.X = create_nwchem_guess(world, 2 * r_params.n_states());
+        } else if (r_params.guess_xyz()) {
           // Use a symmetry adapted operator on ground state functions
-          Chi.X = create_trial_functions2(
-              world, Gparams.orbitals, Rparams.print_level);
+          Chi.X = create_trial_functions2(world, ground_orbitals, r_params.print_level());
         } else {
-          Chi.X = create_trial_functions(
-              world, 2 * Rparams.states, Gparams.orbitals, Rparams.print_level);
+          Chi.X = create_trial_functions(world, 2 * r_params.n_states(), ground_orbitals, r_params.print_level());
         }
 
         // Load balance
         // Only balancing on x-components. Smart?
         if (world.size() > 1) {
           // Start a timer
-          if (Rparams.print_level >= 1) molresponse::start_timer(world);
+          if (r_params.print_level() >= 1) molresponse::start_timer(world);
           if (world.rank() == 0) print("");  // Makes it more legible
 
           LoadBalanceDeux<3> lb(world);
-          for (size_t j = 0; j < Rparams.states; j++) {
-            for (size_t k = 0; k < Gparams.num_orbitals; k++) {
+          for (size_t j = 0; j < r_params.n_states(); j++) {
+            for (size_t k = 0; k < r_params.num_orbitals(); k++) {
               lb.add_tree(Chi.X[j][k], lbcost<double, 3>(1.0, 8.0), true);
             }
           }
-          for (size_t j = 0; j < Gparams.num_orbitals; j++) {
-            lb.add_tree(Gparams.orbitals[j], lbcost<double, 3>(1.0, 8.0), true);
+          for (size_t j = 0; j < r_params.num_orbitals(); j++) {
+            lb.add_tree(ground_orbitals[j], lbcost<double, 3>(1.0, 8.0), true);
           }
           FunctionDefaults<3>::redistribute(world, lb.load_balance(2));
 
-          if (Rparams.print_level >= 1)
-            molresponse::end_timer(world, "Load balancing:");
+          if (r_params.print_level() >= 1) molresponse::end_timer(world, "Load balancing:");
         }
 
         // Project out groundstate from guesses
-        QProjector<double, 3> projector(world, Gparams.orbitals);
-        for (unsigned int i = 0; i < Chi.X.size(); i++)
-          Chi.X[i] = projector(Chi.X[i]);
+        QProjector<double, 3> projector(world, ground_orbitals);
+        for (unsigned int i = 0; i < Chi.X.size(); i++) Chi.X[i] = projector(Chi.X[i]);
 
         // Ensure orthogonal guesses
         for (size_t i = 0; i < 2; i++) {
@@ -154,15 +146,14 @@ void TDDFT::solve_excited_states(World& world) {
         // Sort
         sort(world, omega, Chi.X);
         // Basic output
-        if (Rparams.print_level >= 1 and world.rank() == 0) {
+        if (r_params.print_level() >= 1 and world.rank() == 0) {
           print("\n   Final initial guess excitation energies:");
           print(omega);
         }
-        //Chi = X_space(world, Rparams.states, Gparams.num_orbitals);
+        // Chi = X_space(world, r_params.n_states(), r_params.num_orbitals);
         // Select lowest energy functions from guess
-        Chi.X= select_functions(
-            world, Chi.X, omega, Rparams.states, Rparams.print_level);
-		Chi.Y=response_space(world,Rparams.states,Gparams.num_orbitals);
+        Chi.X = select_functions(world, Chi.X, omega, r_params.n_states(), r_params.print_level());
+        Chi.Y = response_space(world, r_params.n_states(), r_params.num_orbitals());
         // Initial guess for y are zero functions
       }
     }
@@ -172,35 +163,34 @@ void TDDFT::solve_excited_states(World& world) {
   }
 
   // Plot the response function if desired
-  if (Rparams.plot) {
+  if (r_params.plot()) {
     // Need to get densities first
-    std::vector<real_function_3d> densities =
-        transition_density(world, Gparams.orbitals, Chi.X, Chi.Y);
+    std::vector<real_function_3d> densities = transition_density(world, ground_orbitals, Chi.X, Chi.Y);
 
     // For the instance where we don't plot all the orbitals
     std::vector<real_function_3d> plot_densities;
-    for (size_t i : Rparams.plot_data) {
+    for (size_t i : r_params.plot_data()) {
       plot_densities.push_back(densities[i]);
     }
 
     // Now plot
     if (world.rank() == 0) print("\n   Plotting response state densities.\n");
-    if (Rparams.plot_L > 0.0)
+    if (r_params.plot_L() > 0.0)
       do_vtk_plots(world,
-                   Rparams.plot_pts,
-                   Rparams.plot_L,
+                   r_params.plot_pts(),
+                   r_params.plot_L(),
                    0,
-                   Rparams.plot_data.size(),
-                   Gparams.molecule,
+                   r_params.plot_data().size(),
+                   molecule,
                    plot_densities,
                    "response-state");
     else
       do_vtk_plots(world,
-                   Rparams.plot_pts,
-                   Gparams.L,
+                   r_params.plot_pts(),
+                   r_params.L(),
                    0,
-                   Rparams.plot_data.size(),
-                   Gparams.molecule,
+                   r_params.plot_data().size(),
+                   molecule,
                    plot_densities,
                    "response-state");
   }
