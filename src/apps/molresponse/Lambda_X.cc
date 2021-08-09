eeaea61c64dde99a141a55abb27f9fd9a0911a2a
ahurta92 <ahurta92@gmail.com>
2021-08-09 08:58:40 -0400

    truncate to ensure convergence

--- src/apps/molresponse/Lambda_X.cc
+++ src/apps/molresponse/Lambda_X.cc
@@ -36,17 +36,32 @@ X_space TDDFT::Compute_Lambda_X(World& world,
                                 std::string calc_type) {
   // compute
   bool compute_Y = calc_type.compare("full") == 0;
-  X_space Lambda_X = X_space(world, Chi.num_states(), Chi.num_orbitals());
 
+  X_space Lambda_X = X_space(world, Chi.num_states(), Chi.num_orbitals());
   X_space F0X = compute_F0X(world, Chi, xc, compute_Y);
+
+  if (r_params.print_level() >= 3) {
+    print("<X|F0|X>");
+    print(inner(Chi, F0X));
+  }
+  // put it all together
+
   F0X.truncate();
 
   X_space E0X = Chi.copy();
+  E0X.truncate();
   E0X.X = E0X.X * hamiltonian;
 
   if (compute_Y) {
     E0X.Y = E0X.Y * hamiltonian;
   }
+  E0X.truncate();
+  if (r_params.print_level() >= 3) {
+    print("<X|E0|X>");
+    print(inner(Chi, E0X));
+  }
+
+  // put it all together
   X_space gamma;
 
   // compute
@@ -57,8 +72,17 @@ X_space TDDFT::Compute_Lambda_X(World& world,
   } else {
     gamma = compute_gamma_tda(world, Chi, xc);
   }
+  if (r_params.print_level() >= 3) {
+    print("<X|Gamma|X>");
+    print(inner(Chi, gamma));
+  }
 
   Lambda_X = (F0X - E0X) + gamma;
+
+  if (r_params.print_level() >= 3) {
+    print("<X|Lambda|X>");
+    print(inner(Chi, Lambda_X));
+  }
 
   return Lambda_X;
 }
