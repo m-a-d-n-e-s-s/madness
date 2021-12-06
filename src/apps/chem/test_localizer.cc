f26a4ab47a36f4a806c23c204a02ee87bfdd530e
fbischoff <florian.bischoff@hu-berlin.de>
2021-12-06 22:04:21 +0100

    some cleanup

--- src/apps/chem/test_localizer.cc
+++ src/apps/chem/test_localizer.cc
@@ -88,87 +88,31 @@ bool is_centered_normal(const Vector<double,3>& v1, const Vector<double,3>& v2, 
     return (inplane<thresh);
 }
 
-template<typename T, std::size_t NDIM>
-MolecularOrbitals<T, NDIM>
-compute_initial_orbitals(World& world, const AtomicBasisSet& aobasis, const Molecule& molecule,
-                         std::shared_ptr<NuclearCorrelationFactor>& ncf) {
-    std::vector<real_function_3d> aos = SCF::project_ao_basis_only(world, aobasis, molecule);
-    MolecularOrbitals<double, 3> ao;
-    ao.set_mos(aos * ncf->inverse());
-    functionT rho =
-            factoryT(world).functor(
-                    functorT(
-                            new MolecularGuessDensityFunctor(molecule,
-                                                             aobasis))).truncate_on_project();
-    Tensor<T> fock = compute_fock_matrix(world, ncf, ao,rho);
-    Tensor<T> overlap = matrix_inner(world, ao.get_mos() * ncf->square(), ao.get_mos());
-    Tensor<T> U;
-    Tensor<typename Tensor<T>::scalar_type> evals;
-
-    sygvp(world, fock, overlap, 1, U, evals);
-    Tensor<T> UT = transpose(U);
-
-    MolecularOrbitals<T, NDIM> mos;
-    const long nmo = molecule.total_nuclear_charge() / 2;
-    std::vector<Function<T, NDIM>> v = transform(world, ao.get_mos(), U(_, Slice(0, nmo - 1)));
-
-    mos.update_mos_and_eps(v, evals(Slice(0, nmo - 1)));
-    mos.set_all_orbitals_occupied();
-    mos.recompute_localize_sets();
-    return mos;
-}
-
-template<typename T, std::size_t NDIM>
-std::tuple<Localizer<T,NDIM>, MolecularOrbitals<T,NDIM>, std::shared_ptr<NuclearCorrelationFactor>>
-prepare_calculation(World& world, Molecule& molecule) {
-
-    molecule.orient();
-    molecule.print();
-    std::shared_ptr<PotentialManager> pm(new PotentialManager(molecule, ""));
-    pm->make_nuclear_potential(world);
-//        auto ncf = create_nuclear_correlation_factor(world, molecule, pm, std::make_pair("slater", 2.0));
-    auto ncf = create_nuclear_correlation_factor(world, molecule, pm, std::make_pair("none", 1.0));
-    ncf->initialize(FunctionDefaults<NDIM>::get_thresh() * 0.1);
-
-    AtomicBasisSet aobasis;
-//    aobasis.read_file("sto-3g");
-    aobasis.read_file("6-31G");
-    std::vector<int> at_to_bf, at_nbf;
-    aobasis.atoms_to_bfn(molecule, at_to_bf, at_nbf);
-
-    std::vector<real_function_3d> aos = SCF::project_ao_basis_only(world, aobasis, molecule);
-    Localizer<double, 3> localizer(world, aobasis, molecule, aos);
-    if (ncf->type()!=madness::NuclearCorrelationFactor::None) localizer.set_metric(ncf->function());
-    localizer.print_info();
-
-    auto mos = compute_initial_orbitals<double, 3>(world, aobasis, molecule, ncf);
-    mos.pretty_print("initial mos");
-
-    return std::tuple(localizer,mos,ncf);
-}
 
 /// test localized orbitals: should be pointing towards the edges of a tetrahedron
-bool test_ne_boys(World& world) {
+bool test_ne_boys(World& world, const Nemo& nemo) {
     test_output tout("testing ne boys localization");
-    Molecule ne_mol;
-    ne_mol.read_structure_from_library("ne");
-    auto [localizer,mos,ncf]=prepare_calculation<double,3>(world,ne_mol);
+
+    MolecularOrbitals<double,3> mos;
+    mos.update_mos_and_eps(nemo.get_calc()->amo,nemo.get_calc()->aeps);
+    mos.set_all_orbitals_occupied();
+    mos.recompute_localize_sets();
+    mos.pretty_print("initial orbitals after a few SCF iterations");
+
+    Localizer<double,3> localizer(world,nemo.get_calc()->aobasis,nemo.molecule(),nemo.get_calc()->ao);
+
     localizer.set_method("boys");
     localizer.set_enforce_core_valence_separation(true);
     localizer.print_info();
-    Tensor<double> fock1 = compute_fock_matrix(world, ncf, mos);
-    Tensor<double> overlap = matrix_inner(world,ncf->square()*mos.get_mos(),mos.get_mos());
+    Tensor<double> fock1 =nemo.compute_fock_matrix(mos.get_mos(), mos.get_occ());
+    Tensor<double> overlap = matrix_inner(world,nemo.get_ncf_ptr()->square()*mos.get_mos(),mos.get_mos());
 
-    auto mo1=localizer.localize(mos, fock1, overlap, true);
-    Tensor<double> fock2 = compute_fock_matrix(world, ncf, mo1);
-    mo1.pretty_print("cv-localized MOs");
+    auto lmo=localizer.localize(mos, fock1, overlap, true);
+    Tensor<double> fock2 =nemo.compute_fock_matrix(lmo.get_mos(), lmo.get_occ());
+    lmo.pretty_print("cv-localized MOs");
     print(fock2);
-//    Tensor<double> fock2 = compute_fock_matrix(world, ncf, mo1);
-//    print("final fock matrix");
-//    print(fock2);
-    std::vector<Vector<double,3>> center=mo1.compute_center(ncf->square());
-    ne_mol.print();
-    for (int i=0; i<mo1.get_mos().size(); ++i) {
+    std::vector<Vector<double,3>> center=lmo.compute_center(nemo.get_ncf_ptr()->square());
+    for (int i=0; i<lmo.get_mos().size(); ++i) {
         print("center of mo",i,center[i][0],center[i][1],center[i][2]);
     }
     print("distances ne tetrahedron",dist(center[1],center[2]),
@@ -187,13 +131,13 @@ bool test_ne_boys(World& world) {
 
     bool success=true;
     // distances depend on the ao basis..
-    double refdistance=localizer.get_aobasis().get_name()=="STO-3G" ? 0.7158 :  0.51877 ;
-    success=success and (fabs(dist(center[1],center[2])-refdistance)<0.001);
-    success=success and (fabs(dist(center[1],center[3])-refdistance)<0.001);
-    success=success and (fabs(dist(center[1],center[4])-refdistance)<0.001);
-    success=success and (fabs(dist(center[2],center[3])-refdistance)<0.001);
-    success=success and (fabs(dist(center[2],center[4])-refdistance)<0.001);
-    success=success and (fabs(dist(center[3],center[4])-refdistance)<0.001);
+//    double refdistance=localizer.get_aobasis().get_name()=="STO-3G" ? 0.7158 :  0.51877 ;
+//    success=success and (fabs(dist(center[1],center[2])-refdistance)<0.001);
+//    success=success and (fabs(dist(center[1],center[3])-refdistance)<0.001);
+//    success=success and (fabs(dist(center[1],center[4])-refdistance)<0.001);
+//    success=success and (fabs(dist(center[2],center[3])-refdistance)<0.001);
+//    success=success and (fabs(dist(center[2],center[4])-refdistance)<0.001);
+//    success=success and (fabs(dist(center[3],center[4])-refdistance)<0.001);
 
     success=success and (fabs(bend(center[1],center[0],center[2])-109.4712)<0.001);
     success=success and (fabs(bend(center[1],center[0],center[3])-109.4712)<0.001);
@@ -224,8 +168,6 @@ bool test_ethylene(World& world, const Nemo& nemo) {
     bananabonds[{0,1}]=0; // C1--C2
 
     Molecule ethylene_mol=nemo.molecule();
-//    ethylene_mol.read_structure_from_library("ethylene");
-//    auto [localizer,mos,ncf]=prepare_calculation<double,3>(world,ethylene_mol);
     MolecularOrbitals<double,3> mos;
     mos.update_mos_and_eps(nemo.get_calc()->amo,nemo.get_calc()->aeps);
     mos.set_all_orbitals_occupied();
@@ -258,7 +200,7 @@ bool test_ethylene(World& world, const Nemo& nemo) {
             nemo.get_calc()->amo=lmo.get_mos();
 
             Tensor<double> fock2 = nemo.compute_fock_matrix(lmo.get_mos(), lmo.get_occ());
-            print("final fock matrix");
+            print("final fock matrix for",method);
             print(fock2);
             if (enforce_cv) {
                 bool success1=Localizer<double,3>::check_core_valence_separation(fock2, lmo.get_localize_sets());
@@ -318,63 +260,6 @@ bool test_ethylene(World& world, const Nemo& nemo) {
 
 }
 
-template<typename T, std::size_t NDIM>
-int test_localization(World& world, Localizer<T, NDIM>& localizer, std::shared_ptr<NuclearCorrelationFactor>& ncf,
-                      const MolecularOrbitals<T, NDIM>& mo, double sum_orbital_energy, const bool verbose) {
-    test_output tout("testing "+localizer.get_method(), verbose);
-    int success = 0;
-    localizer.set_enforce_core_valence_separation(false);
-    MolecularOrbitals<T, NDIM> lmo = localizer.localize(mo, true);
-    Tensor<T> fock = compute_fock_matrix(world, ncf, lmo);
-    print(localizer.get_method(), "localized fock matrix");
-    print(fock);
-
-    double trace = 0.0;
-    for (int i = 0; i < fock.dim(0); ++i) trace += fock(i, i);
-    print("sum over diagonal fock matrix elements", trace);
-    success+=(std::fabs(trace-sum_orbital_energy)/trace<FunctionDefaults<3>::get_thresh());
-
-    tout.end(success);
-    return success;
-}
-
-template<typename T, std::size_t NDIM>
-int test_core_valence_separation(World& world, Localizer<T, NDIM>& localizer,
-                                 std::shared_ptr<NuclearCorrelationFactor>& ncf,
-                                 const MolecularOrbitals<T, NDIM>& mo1,
-                                 const double sum_orbital_energy,
-                                 const bool verbose) {
-    bool success = 0;
-    std::string method=localizer.get_method();
-    test_output tout("testing core-valence separation "+method);
-    if (verbose) tout.set_cout_to_terminal();
-    localizer.set_enforce_core_valence_separation(true);
-    Tensor<T> fock1 = compute_fock_matrix(world, ncf, mo1);
-    Tensor<T> overlap = matrix_inner(world,ncf->square()*mo1.get_mos(),mo1.get_mos());
-    print(method, "diagonal fock matrix to start with");
-    print(fock1);
-    MolecularOrbitals<T, NDIM> lmo = localizer.localize(mo1, fock1, overlap, true);
-//    Tensor<T> UT = localizer.compute_core_valence_separation_transformation_matrix(world, mo, fock1, overlap);
-//    std::vector<Function<T, NDIM>> result = transform(world, mo.get_mos(), UT);
-//    truncate(world, result);
-//    MolecularOrbitals<T, NDIM> lmo;
-//    lmo.set_mos(result);
-
-    Tensor<T> fock = compute_fock_matrix(world, ncf, lmo);
-    print(method, "localized fock matrix");
-    print(fock);
-    bool success1=Localizer<T,NDIM>::check_core_valence_separation(fock,lmo.get_localize_sets());
-    print("success cv-separation",success1);
-    success +=success1;
-
-    double trace= fock_trace(fock);
-    print("sum over diagonal fock matrix elements", trace);
-    success+=(std::fabs(trace-sum_orbital_energy)/trace<FunctionDefaults<3>::get_thresh());
-
-    tout.end(success==0);
-    return (success==0);
-}
-
 int main(int argc, char **argv) {
     int result = 0;
     {
@@ -406,29 +291,12 @@ int main(int argc, char **argv) {
         Nemo nemo(world,parser);
         nemo.value();
 
-
-//        test_ne_boys(world);
         test_ethylene(world,nemo);
 
-        /*
-         * test orbital invariance of the orbital energy sum
-         */
-//
-//        double sum_orbital_energies=mos.get_eps().sum();
-//        print("canonical sum over orbital energies",sum_orbital_energies);
-//
-//        localizer.set_method("boys");
-//        test_localization(world, localizer, ncf, mos, sum_orbital_energies, verbose);
-//        test_core_valence_separation(world, localizer, ncf, mos, sum_orbital_energies, verbose);
-//
-//        localizer.set_method("pm");
-//        test_localization(world, localizer, ncf, mos, sum_orbital_energies, verbose);
-//        test_core_valence_separation(world, localizer, ncf, mos, sum_orbital_energies, verbose);
-//
-//        localizer.set_method("new");
-//        test_localization(world, localizer, ncf, mos, sum_orbital_energies, verbose);
-//        test_core_valence_separation(world, localizer, ncf, mos, sum_orbital_energies, verbose);
-
+        parser.set_keyval("structure","ne");
+        Nemo nemo1(world,parser);
+        nemo1.value();
+        test_ne_boys(world,nemo1);
         print("result", result);
     }
     madness::finalize();
