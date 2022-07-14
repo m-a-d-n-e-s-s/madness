01a1e29c679672ccc46388f2efdb557a10be31e5
fbischoff <florian.bischoff@hu-berlin.de>
2022-07-14 12:25:00 +0200

    working on the documentation

--- doc/conf.py
+++ doc/conf.py
@@ -1,5 +1,6 @@
 # Configuration file for the Sphinx documentation builder.
 #
+#
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
@@ -14,6 +15,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+# import myst_parser
 
 # -- Project information -----------------------------------------------------
 
@@ -27,7 +29,8 @@ author = 'Robert Harrison'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser']
+extensions = ["myst_parser"]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
