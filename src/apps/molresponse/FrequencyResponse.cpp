ac3023a9022a779c384f586d02bf0702f6d1a0ae
Adrian Hurtado <adrian.hurtado@stonybrook.edu>
2022-07-20 17:34:41 -0400

    basically remove step restriction

--- src/apps/molresponse/FrequencyResponse.cpp
+++ src/apps/molresponse/FrequencyResponse.cpp
@@ -77,7 +77,7 @@ void FrequencyResponse::iterate(World &world) {
     vector_real_function_3d rho_omega = make_density(world, Chi);
     converged = false;// Converged flag
 
-    auto max_rotation = 25;// 1000 * conv_den;
+    auto max_rotation = 1000;// 1000 * conv_den;
 
     for (iter = 0; iter <= r_params.maxiter(); ++iter) {
 
