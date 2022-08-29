454a392fd310f84e4161e0edc92d09375e777da5
Adrian Hurtado <adrian.hurtado@stonybrook.edu>
2022-08-29 17:22:40 -0400

    fix the number of arguments for mad-excited

--- src/apps/molresponse/testing/mad-excited.cpp
+++ src/apps/molresponse/testing/mad-excited.cpp
@@ -33,7 +33,7 @@ auto main(int argc, char *argv[]) -> int {
 
     std::cout.precision(6);
 
-    if (argc != 5) {
+    if (argc != 4) {
 
         std::cout << "Wrong number of inputs" << std::endl;
         return 1;
@@ -41,8 +41,7 @@ auto main(int argc, char *argv[]) -> int {
 
     const std::string molecule_name{argv[1]};
     const std::string xc{argv[2]};
-    const std::string op{argv[3]};
-    const std::string is_high_prec{argv[4]};
+    const std::string is_high_prec{argv[3]};
 
 
     bool high_prec;
@@ -59,29 +58,32 @@ auto main(int argc, char *argv[]) -> int {
     try {
 
         auto m_schema = moldftSchema(molecule_name, xc, schema);
-        moldft(world, m_schema, true, true, high_prec);
+        moldft(world, m_schema, false, true, high_prec);
         auto excited_schema = excitedSchema(schema, m_schema);
         excited_schema.print();
 
         try {
-            bool success = runExcited(world, excited_schema, false, high_prec);
 
-        } catch (const SafeMPI::Exception &e) {
-            print(e);
-        } catch (const madness::MadnessException &e) {
-            std::cout << e << std::endl;
-        } catch (const madness::TensorException &e) { print(e); } catch (const char *s) {
-            print(s);
-        } catch (const std::string &s) { print(s); } catch (const std::exception &e) {
-            print(e.what());
-        } catch (...) { error("caught unhandled exception"); }
+            moldft(world, m_schema, false, false, high_prec);
+            runExcited(world, excited_schema, true, high_prec);
+        } catch (MadnessException &madnessException) {
+            print(madnessException);
+            moldft(world, m_schema, true, false, high_prec);
+            runExcited(world, excited_schema, true, high_prec);
+        }
 
-    } catch (const std::filesystem::filesystem_error &ex) { std::cerr << ex.what() << "\n"; }
+    } catch (const SafeMPI::Exception &e) { print(e); } catch (const madness::MadnessException &e) {
+        std::cout << e << std::endl;
+    } catch (const madness::TensorException &e) { print(e); } catch (const char *s) {
+        print(s);
+    } catch (const std::string &s) { print(s); } catch (const std::exception &e) {
+        print(e.what());
+    } catch (const std::filesystem::filesystem_error &ex) {
+        std::cerr << ex.what() << "\n";
+    } catch (...) { error("caught unhandled exception"); }
 
     if (world.rank() == 0) { print("Finished Excited-State Calculation"); }
-
     return result;
-
     // print_meminfo(world.rank(), "startup");
     // std::cout.precision(6);
     // print_stats(world);
