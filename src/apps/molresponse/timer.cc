966f71b47cb9ba4ae03200cba2cdd4895ec85ecf
Adrian Hurtado <adrian.hurtado@stonybrook.edu>
2022-09-13 17:22:25 -0400

    remove deprecated files

--- src/apps/molresponse/timer.cc
+++ src/apps/molresponse/timer.cc
@@ -5,37 +5,36 @@
 
 #include <vector>
 
-#include "TDDFT.h"
 #include "madness/mra/mra.h"
 // Needed for timers
 namespace molresponse {
-    double pop(std::vector<double>& v) {
+    double pop(std::vector<double> &v) {
         double x = v.back();
         v.pop_back();
         return x;
     }
     // Pulled from SCF.cc, starts a timer
-    void start_timer(World& world) {
+    void start_timer(madness::World &world) {
         world.gop.fence();
-        ttt.push_back(wall_time());
-        sss.push_back(cpu_time());
+        ttt.push_back(madness::wall_time());
+        sss.push_back(madness::cpu_time());
     }
     // Stops a timer
-    void end_timer(World& world, const char* msg) {
+    void end_timer(madness::World &world, const char *msg) {
         world.gop.fence();
-        MADNESS_CHECK(ttt.size() > 0);
+        MADNESS_CHECK(!ttt.empty());
 
-        double wall = wall_time() - pop(ttt);
-        double cpu = cpu_time() - pop(sss);
+        double wall = madness::wall_time() - pop(ttt);
+        double cpu = madness::cpu_time() - pop(sss);
 
 
         if (world.rank() == 0) printf("   timer: %20.20s %8.2fs %8.2fs\n", msg, cpu, wall);
     }
-    void end_timer(World& world, const char* msg, const std::string& key,
-                   std::map<std::string, std::pair<double, double>>& time) {
+    void end_timer(madness::World &world, const char *msg, const std::string &key,
+                   std::map<std::string, std::pair<double, double>> &time) {
         world.gop.fence();
-        double wall = wall_time() - pop(ttt);
-        double cpu = cpu_time() - pop(sss);
+        double wall = madness::wall_time() - pop(ttt);
+        double cpu = madness::cpu_time() - pop(sss);
 
         std::pair<double, double> timings;
         // first is wall
