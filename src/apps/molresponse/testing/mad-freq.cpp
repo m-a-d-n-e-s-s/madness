e0f4749a2a653264e0296025f73e913f079e0cd0
Adrian Hurtado <adrian.hurtado@stonybrook.edu>
2022-12-01 18:07:07 -0500

    clang-tidy

--- src/apps/molresponse/testing/mad-freq.cpp
+++ src/apps/molresponse/testing/mad-freq.cpp
@@ -54,7 +54,6 @@ auto main(int argc, char *argv[]) -> int {
             auto schema = runSchema(world, xc);
             auto m_schema = moldftSchema(world, molecule_name, xc, schema);
             auto f_schema = frequencySchema(world, schema, m_schema, op);
-            world.gop.fence();
             if (std::filesystem::exists(m_schema.calc_info_json_path) &&
                 std::filesystem::exists(m_schema.moldft_restart)) {
                 runFrequencyTests(world, f_schema, precision);
