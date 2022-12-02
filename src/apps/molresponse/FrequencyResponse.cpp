02546cf2b2c7c197c3a4ca1cd599c85e65320a75
Adrian Hurtado <adrian.hurtado@stonybrook.edu>
2022-12-02 12:13:26 -0500

    new step restriction

--- src/apps/molresponse/FrequencyResponse.cpp
+++ src/apps/molresponse/FrequencyResponse.cpp
@@ -23,7 +23,7 @@ void FrequencyResponse::iterate(World &world) {
     // the Final protocol should be equal to dconv at the minimum
     const double conv_den = std::max(100 * FunctionDefaults<3>::get_thresh(), r_params.dconv());
     const double relative_max_target =
-            std::max(2500 * FunctionDefaults<3>::get_thresh(), 5 * r_params.dconv());
+            std::max(1500 * FunctionDefaults<3>::get_thresh(), 5 * r_params.dconv());
     // m residuals for x and y
     Tensor<double> bsh_residualsX((int(m)));
     Tensor<double> bsh_residualsY((int(m)));
