c331f235d6df6226208ebc1482b0aba5b5e36cf8
Adrian Hurtado <adrian.hurtado@stonybrook.edu>
2022-12-06 08:31:13 -0500

    just a little tighter

--- src/apps/molresponse/FrequencyResponse.cpp
+++ src/apps/molresponse/FrequencyResponse.cpp
@@ -23,7 +23,7 @@ void FrequencyResponse::iterate(World &world) {
     // the Final protocol should be equal to dconv at the minimum
     const double dconv =
             std::max(FunctionDefaults<3>::get_thresh() * 100, r_params.dconv());//.01 .0001 .1e-5
-    const double relative_max_target = 200 * FunctionDefaults<3>::get_thresh();
+    const double relative_max_target = 150 * FunctionDefaults<3>::get_thresh();
     // m residuals for x and y
     Tensor<double> bsh_residualsX((int(m)));
     Tensor<double> bsh_residualsY((int(m)));
