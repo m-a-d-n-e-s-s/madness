7610e5b743b735d3bc7472ad3d219def1a76b867
Adrian Hurtado <adrian.hurtado@stonybrook.edu>
2022-12-16 17:11:12 -0500

    reduce max sub and max iters

--- src/apps/molresponse/testing/runners.hpp
+++ src/apps/molresponse/testing/runners.hpp
@@ -512,8 +512,8 @@ void set_frequency_response_parameters(World &world, ResponseParameters &r_param
             r_params.set_user_defined_value<double>("dconv", 1e-7);
         }
         //r_params.set_user_defined_value("archive", std::string("../restartdata"));
-        r_params.set_user_defined_value("maxiter", size_t(20));
-        r_params.set_user_defined_value("maxsub", size_t(10));
+        r_params.set_user_defined_value("maxiter", size_t(10));
+        r_params.set_user_defined_value("maxsub", size_t(5));
         r_params.set_user_defined_value("kain", true);
         r_params.set_user_defined_value("omega", frequency);
         r_params.set_user_defined_value("first_order", true);
