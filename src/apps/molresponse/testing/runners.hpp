23367b0a6f5c492e588ba1757e66b4e9d10a4a4d
Adrian Hurtado <adrian.hurtado@stonybrook.edu>
2023-01-06 13:19:07 -0500

    match moldft and response convergence settings

--- src/apps/molresponse/testing/runners.hpp
+++ src/apps/molresponse/testing/runners.hpp
@@ -372,7 +372,7 @@ void runMOLDFT(World &world, const moldftSchema &moldftSchema, bool try_run, boo
             param1.set_user_defined_value<vector<double>>("protocol", {1e-4, 1e-6, 1e-7});
             param1.set_user_defined_value<double>("dconv", 1e-4);
         } else if (precision == "high") {
-            param1.set_user_defined_value<vector<double>>("protocol", {1e-4, 1e-6, 1e-8});
+            param1.set_user_defined_value<vector<double>>("protocol", {1e-6, 1e-7, 1e-8});
             param1.set_user_defined_value<double>("dconv", 1e-6);
         } else {
             param1.set_user_defined_value<vector<double>>("protocol", {1e-8});
