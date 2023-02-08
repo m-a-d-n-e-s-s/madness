92b9d43d64021bc226dc817403d10d5960f9d628
Adrian Hurtado <adrian.hurtado@stonybrook.edu>
2023-02-08 17:37:27 -0500

    revert to old form of J operator

--- src/apps/molresponse/ResponseBase.cpp
+++ src/apps/molresponse/ResponseBase.cpp
@@ -479,34 +479,17 @@ auto ResponseBase::compute_gamma_full(World &world, const gamma_orbitals &densit
     functionT rho_x_b;
     functionT rho_y_b;
     auto mul_tol = FunctionDefaults<3>::get_thresh();
-    // note that x can refer to x or y
-    /*
     auto rho_b = make_density(world, chi_alpha);
     int b = 0;
     for (const auto &rho_b_i: rho_b) {
         auto temp_J = apply(*shared_coulomb_operator, rho_b_i);
         J.X[b++] = mul(world, temp_J, phi0);
     }
-     */
-    auto compute_j = [&, &phi0 = phi0](const auto &dx) {
-        auto rho_x_b = dot(world, dx, phi0);
-        rho_x_b.truncate();
-        // apply the coulomb operator to rho_b
-        rho_x_b = apply(*shared_coulomb_operator, rho_x_b);
-        return mul_sparse(world, rho_x_b, phi0, mul_tol, true);
-    };
-
-    // compute j_x = op(rho_x)*phi0
-
-    std::transform(chi_alpha.X.begin(), chi_alpha.X.end(), j_x.begin(), compute_j);
-    if (world.rank() == 0) { print("compute jX"); }
-    std::transform(chi_alpha.Y.begin(), chi_alpha.Y.end(), j_y.begin(), compute_j);
-
-    J.X = j_x + j_y;
-    if (world.rank() == 0) { print("add jx+jy"); }
+    world.gop.fence();
     J.Y = J.X.copy();
     if (world.rank() == 0) { print("copy JX into JY"); }
     world.gop.fence();
+
     if (r_params.print_level() >= 1) {
         molresponse::end_timer(world, "J[omega]", "J[omega]", iter_timing);
     }
@@ -525,7 +508,9 @@ auto ResponseBase::compute_gamma_full(World &world, const gamma_orbitals &densit
         }
     }
     if (r_params.print_level() >= 1) { molresponse::start_timer(world); }
+
     auto K = response_exchange_multiworld(phi0, chi_alpha, true);
+
     if (r_params.print_level() >= 1) {
         molresponse::end_timer(world, "K[omega]", "K[omega]", iter_timing);
     }
