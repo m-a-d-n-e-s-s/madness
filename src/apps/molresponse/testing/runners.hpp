2392d89e8b714d77bdbe27a971c511e4728b0357
Adrian Hurtado <adrian.hurtado@stonybrook.edu>
2023-03-05 20:48:02 -0500

    switch back to 2 protocols

--- src/apps/molresponse/testing/runners.hpp
+++ src/apps/molresponse/testing/runners.hpp
@@ -367,10 +367,10 @@ void runMOLDFT(World &world, const moldftSchema &moldftSchema, bool try_run, boo
         param1.set_user_defined_value<double>("l", 200);
 
         if (precision == "low") {
-            param1.set_user_defined_value<vector<double>>("protocol", {1e-4, 1e-6, 1e-7});
+            param1.set_user_defined_value<vector<double>>("protocol", {1e-4, 1e-6});
             param1.set_user_defined_value<double>("dconv", 1e-4);
         } else if (precision == "high") {
-            param1.set_user_defined_value<vector<double>>("protocol", {1e-6, 1e-7, 1e-8});
+            param1.set_user_defined_value<vector<double>>("protocol", {1e-4, 1e-6, 1e-8});
             param1.set_user_defined_value<double>("dconv", 1e-6);
         } else {
             param1.set_user_defined_value<vector<double>>("protocol", {1e-8});
@@ -508,17 +508,17 @@ void set_frequency_response_parameters(World &world, ResponseParameters &r_param
                                        const double &frequency, const std::string &precision) {
     if (world.rank() == 0) {
         if (precision == "high") {
-            r_params.set_user_defined_value<vector<double>>("protocol", {1e-6, 1e-7, 1e-8});
+            r_params.set_user_defined_value<vector<double>>("protocol", {1e-4, 1e-6, 1e-8});
             r_params.set_user_defined_value<double>("dconv", 1e-5);
         } else if (precision == "low") {
-            r_params.set_user_defined_value<vector<double>>("protocol", {1e-4, 1e-6, 1e-7});
+            r_params.set_user_defined_value<vector<double>>("protocol", {1e-4, 1e-6});
             r_params.set_user_defined_value<double>("dconv", 1e-4);
         } else {
             r_params.set_user_defined_value<vector<double>>("protocol", {1e-9});
             r_params.set_user_defined_value<double>("dconv", 1e-6);
         }
         //r_params.set_user_defined_value("archive", std::string("../restartdata"));
-        r_params.set_user_defined_value("maxiter", size_t(20));
+        r_params.set_user_defined_value("maxiter", size_t(25));
         r_params.set_user_defined_value("maxsub", size_t(5));
         r_params.set_user_defined_value("kain", true);
         r_params.set_user_defined_value("omega", frequency);
