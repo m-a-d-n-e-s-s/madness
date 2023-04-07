bbfb0eb05c460e0dfb0c3f8c519c0d023d90bdfc
Adrian Hurtado <adrian.hurtado@stonybrook.edu>
2023-04-07 09:14:26 -0400

    lower the high protocol

--- src/apps/molresponse/testing/runners.hpp
+++ src/apps/molresponse/testing/runners.hpp
@@ -508,7 +508,7 @@ void set_frequency_response_parameters(World &world, ResponseParameters &r_param
                                        const double &frequency, const std::string &precision) {
     if (world.rank() == 0) {
         if (precision == "high") {
-            r_params.set_user_defined_value<vector<double>>("protocol", {1e-4, 1e-6, 1e-8});
+            r_params.set_user_defined_value<vector<double>>("protocol", {1e-4, 1e-6, 1e-7});
             r_params.set_user_defined_value<double>("dconv", 1e-5);
         } else if (precision == "low") {
             r_params.set_user_defined_value<vector<double>>("protocol", {1e-4, 1e-6});
