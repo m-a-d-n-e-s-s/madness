1adba01caeee956eea7c2dfada443687b4f92434
fbischoff <florian.bischoff@hu-berlin.de>
2023-04-12 15:25:54 +0200

    change sprintf to snprintf

--- src/madness/misc/gprofexit.cc
+++ src/madness/misc/gprofexit.cc
@@ -39,18 +39,19 @@ namespace madness {
 
     /// Invoke with id and nproc as rank and size in MPI::COMM_WORLD
     void gprofexit(int id, int nproc) {
-        char buf[64];
+        std::size_t bufsize=64;
+        char buf[bufsize];
         if (id == 0) {
             for (int p=nproc-1; p>0; --p) {
                 while (access("gmon.out",F_OK)) usleep(1000);
-                sprintf(buf,"gmon.out.%d",id+1);
+                snprintf(buf,bufsize,"gmon.out.%d",id+1);
                 if (rename("gmon.out",buf))
                     fprintf(stderr,"gprofexit: failed renaming gmon.out to %s", buf);
             }
         }
         else if ((id+1) < nproc) {
             // Wait for process id+1 to commence writing
-            sprintf(buf,"gmon.out.%d",id+1);
+            snprintf(buf,bufsize,"gmon.out.%d",id+1);
             while (access(buf,F_OK)) usleep(10000);
         }
     }
