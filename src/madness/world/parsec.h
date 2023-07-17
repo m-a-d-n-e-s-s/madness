bbc9f5ad8cbd0726d9efde9f0b886bfac526848e
Eduard Valeyev <eduard@valeyev.net>
2023-07-17 08:11:32 -0400

    by calling ParsecRuntime::initialize_with_existing_context(ctx) before madness::initialize can use existing parsec context

--- src/madness/world/parsec.h
+++ src/madness/world/parsec.h
@@ -13,6 +13,7 @@
 #include <parsec/scheduling.h>
 
 #include <iostream>
+#include <optional>
 
 namespace madness{
     class PoolTaskInterface;
@@ -20,6 +21,7 @@ namespace madness{
     class ParsecRuntime {
     private:
         static parsec_context_t *ctx;
+        static std::optional<bool> made_new_ctx;
         static parsec_taskpool_t *tp;
         static parsec_execution_stream_t *madness_comm_thread_es;
 #ifdef PARSEC_PROF_TRACE
@@ -31,6 +33,7 @@ namespace madness{
         ~ParsecRuntime();
 
         static parsec_context_t* context();
+        static void initialize_with_existing_context(parsec_context_t* ctx);
         static parsec_execution_stream_t *execution_stream();
         static void schedule(PoolTaskInterface* task);
         static int test();
