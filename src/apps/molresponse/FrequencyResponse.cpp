cfda62a51430d7e49025bd43e5ec55f5ca02a0bc
Adrian Hurtado <adrian.hurtado@stonybrook.edu>
2023-10-24 19:14:48 -0400

    compute_y bug

--- src/apps/molresponse/FrequencyResponse.cpp
+++ src/apps/molresponse/FrequencyResponse.cpp
@@ -285,7 +285,7 @@ auto FrequencyResponse::update_response(World &world, X_space &chi, XCOperator<d
     }
     inner_to_json(world, "x_update", response_context.inner(new_chi, new_chi), iter_function_data);
 
-    bool compute_Y = r_params.calc_type() == "full";
+    bool compute_y = r_params.calc_type() == "full";
     x_space_step_restriction(world, chi, new_chi, compute_y, max_rotation);
     if (r_params.print_level() >= 1) { molresponse::end_timer(world, "update response", "update", iter_timing); }
 
