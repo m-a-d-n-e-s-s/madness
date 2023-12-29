a8894255cc71fdd8127947376e2dabd9e8b24787
Eduard Valeyev <eduard@valeyev.net>
2023-12-28 19:03:02 -0500

    fixup test_runtime.cpp

--- doc/tutorial/test_runtime.cpp
+++ doc/tutorial/test_runtime.cpp
@@ -1,4 +1,5 @@
 #include <madness/world/worldgop.h>
+#include <cassert>
 
 int task (int i) { return i + 1; }
 
@@ -9,18 +10,14 @@ int main(int argc, char* argv[]) {
   const auto me = world.rank();
   const auto nproc = world.size();
 
-  Future<int> f;
   if (me == 0) {
-    f = world.taskq.add(&task, 0);
-    for (auto t = 1; t != 1000; ++t) {
+    for (auto t = 0; t != 1000; ++t) {
       const auto proc = t % nproc;
-      f = world.taskq.add(
-          proc, &task, f);
+      auto f = world.taskq.add(proc, &task, t);
+      assert(f.get() == t+1);
     }
   }
   world.gop.fence();
-  if (f.get() != 1000)
-    abort();
 
   finalize();
   return 0;
