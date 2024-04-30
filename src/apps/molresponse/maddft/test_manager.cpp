1795110f5dfc2411b5cb3fbb9720a684cfef04fc
Adrian Hurtado <adrian.hurtado@stonybrook.edu>
2024-04-30 12:54:13 -0400

    Now works with mol json and mixed inputs

--- src/apps/molresponse/maddft/test_manager.cpp
+++ src/apps/molresponse/maddft/test_manager.cpp
@@ -82,3 +82,46 @@ TEST_CASE("MOLDFT ONLY", "[Parameters]") {
   // check if parameters are the same
   check_params_equal(params_json, param_input);
 }
+
+TEST_CASE("Mixed Input") {
+  World& world = World::get_default();
+
+  auto check_calc_params = [&](auto& params) {
+    REQUIRE(params.get_run_moldft() == true);
+    REQUIRE(params.get_run_response() == true);
+    REQUIRE(params.get_molresponse_params().quadratic() == true);
+    REQUIRE(params.get_molresponse_params().freq_range() ==
+            std::vector<double>{0.0, 0.056, 0.1});
+  };
+
+  vector<path> input_files = {"input_no_mol", "input_no_mol.json"};
+  vector<path> mol_files = {"h2.mol", "h2.json"};
+
+  vector<ParameterManager> param_combinations;
+
+  for (auto& input : input_files) {
+    for (auto& mol : mol_files) {
+      auto params = ParameterManager(world, {input, mol});
+      check_calc_params(params);
+      param_combinations.push_back(params);
+    }
+  }
+  int i = 0;
+  // check if parameters are the same
+  for (auto& params : param_combinations) {
+    for (auto& params2 : param_combinations) {
+      bool check_equal = params == params2;
+      if (!check_equal) {
+        auto diff =
+            json::diff(params.get_input_json(), params2.get_input_json());
+        i++;
+        if (world.rank() == 0) {
+
+          print("diff case: ", i);
+          print(diff.dump(2));
+        }
+      }
+      REQUIRE(check_equal);
+    }
+  }
+}
