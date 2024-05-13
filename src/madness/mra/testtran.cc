fa28a9f1f01de5601171e76b2459eadf2399721f
Robert J. Harrison <rjharrison@gmail.com>
2024-05-13 14:29:46 -0400

    new benchmark code

--- src/madness/mra/testtran.cc
+++ src/madness/mra/testtran.cc
@@ -7,13 +7,70 @@ static const double L = 32.0;   // box size
 static const long k = 8;        // wavelet order
 static const double thresh = 1e-4; // precision
 
-double psi(const Vector<double,3>& r) {
-  return exp(-sqrt(r[0]*r[0]+r[1]*r[1]+r[2]*r[2]+1e-6));
+static const size_t nfunc = 4; // number of functions
+
+// A class that behaves like a function to compute a Gaussian of given origin and exponent
+class Gaussian : public FunctionFunctorInterface<double,3> {
+public:
+    const coord_3d center;
+    const double exponent;
+    const double coefficient;
+    std::vector<coord_3d> specialpt;
+
+    Gaussian(const coord_3d& center, double exponent, double coefficient)
+        : center(center), exponent(exponent), coefficient(coefficient), specialpt(1)
+    {
+        specialpt[0][0] = center[0];
+        specialpt[0][1] = center[1];
+        specialpt[0][2] = center[2];
+    }
+
+    // MADNESS will call this interface
+    double operator()(const coord_3d& x) const {
+        double sum = 0.0;
+        for (int i=0; i<3; i++) {
+            double xx = center[i]-x[i];
+            sum += xx*xx;
+        };
+        return coefficient*exp(-exponent*sum);
+    }
+
+    // By default, adaptive projection into the spectral element basis
+    // starts uniformly distributed at the initial level.  However, if
+    // a function is "spiky" it may be necessary to project at a finer
+    // level but doing this uniformly is expensive.  This method
+    // enables us to tell MADNESS about points/areas needing deep
+    // refinement (the default is no special points).
+    std::vector<coord_3d> special_points() const {
+        return specialpt;
+    }
+};
+
+// Makes a new square-normalized Gaussian functor with random origin and exponent
+real_functor_3d random_gaussian() {
+    const double expntmin=1e-1;
+    const double expntmax=1e4;
+    const real_tensor& cell = FunctionDefaults<3>::get_cell();
+    coord_3d origin;
+    for (int i=0; i<3; i++) {
+        origin[i] = RandomValue<double>()*(cell(i,1)-cell(i,0)) + cell(i,0);
+    }
+    double lo = log(expntmin);
+    double hi = log(expntmax);
+    double expnt = exp(RandomValue<double>()*(hi-lo) + lo);
+    double coeff = pow(2.0*expnt/constants::pi,0.75);
+    return real_functor_3d(new Gaussian(origin,expnt,coeff));
 }
 
-double V(const Vector<double,3>& r) {
-  return -1.0/sqrt(r[0]*r[0]+r[1]*r[1]+r[2]*r[2]+1e-6);
+// Makes a vector of new square-normalized Gaussian functions with random origin and exponent
+std::vector<real_function_3d> random_gaussians(size_t n, World& world) {
+    std::vector<real_function_3d> result(n);
+    for (size_t i=0; i<n; i++) {
+        result[i] = FunctionFactory<double,3>(world).functor(random_gaussian());
+    }
+    return result;
 }
+
 
 int main(int argc, char**argv) {
   initialize(argc,argv);
@@ -28,41 +85,44 @@ int main(int argc, char**argv) {
   FunctionDefaults<3>::set_truncate_mode(1);
   FunctionDefaults<3>::set_cubic_cell(-L/2,L/2);
 
-  Function<double,3> u = FunctionFactory<double,3>(world).f(psi);
-  Function<double,3> v = FunctionFactory<double,3>(world).f(V);
+  default_random_generator.setstate(99); // Ensure all processes have the same state
 
-  u.truncate();
-  v.truncate();
-  u.compress();
+  // Create a vector of random Gaussian functions
+  std::vector<real_function_3d> v = random_gaussians(nfunc, world);
 
-  //print("u");
-  //u.print_tree();
-  // v.print_tree();
+  truncate(world, v);
 
-  std::vector<Function<double,3>> w(2);
-  w[0] = u;
-  w[1] = v;
+  // Generate a random tensor of coefficients
+  Tensor<double> c(nfunc,nfunc);
+  for (size_t i=0; i<nfunc; i++) {
+      for (size_t j=0; j<nfunc; j++) {
+          c(i,j) = RandomValue<double>()-0.5;
+      }
+  }
 
-  Tensor<double> c(2,2);
-  c(0,0) = 2.0;
-  c(1,1) = 3.0;
-  c(0,1) = 5.0;
-  c(1,0) = 7.0;
+  print("c");
+  print(c);
 
-  auto newu = c(0,0)*w[0] + c(0,1)*w[1];
-  auto newv = c(1,0)*w[0] + c(1,1)*w[1];
+  auto rold = transform(world, v, c); // old transform uses gaxpy and only zero-based sparsity
 
-  auto r = transform(world, w, c, 0.0, true);
-  r[0].verify_tree();
-  r[1].verify_tree();
+  // manual loop no sparsity or asynchronous execution
+  // std::vector<real_function_3d> rold = zero_functions_compressed<double,3>(world, v.size());
+  // for (size_t i=0; i<nfunc; i++) {
+  //     for (size_t j=0; j<nfunc; j++) {
+  //         rold[i] += v[j] * c(j,i);
+  //     }
+  // }
+  
+  auto r = transform(world, v, c, 0.0, true); // new transform uses mxm and thresholding
+  
+  for (const auto& f : r) {
+      f.verify_tree();
+  }
 
-  //print("new");
-  //r[0].print_tree();
-  //print("old");
-  //newu.print_tree();
-
-  print("newu", (newu-r[0]).norm2());
-  print("newv", (newv-r[1]).norm2());
+  // compute the norm of the errors for each component
+  for (size_t i=0; i<nfunc; i++) {
+      print("error", i, (rold[i]-r[i]).norm2());
+  }
 
   finalize();
 
