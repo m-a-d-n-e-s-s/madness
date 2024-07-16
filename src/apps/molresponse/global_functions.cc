bc3855cad27605a432288f9792a8218790a6915f
ahurta92 <ahurta92@gmail.com>
2024-07-16 11:32:58 -0400

    ground_exchange k[0] needed new exchange as well

--- src/apps/molresponse/global_functions.cc
+++ src/apps/molresponse/global_functions.cc
@@ -6,13 +6,13 @@
 #include "madness/chem/SCFOperators.h"
 #include "response_parameters.h"
 
-
-static auto set_poisson(World &world, const double lo, const double econv = FunctionDefaults<3>::get_thresh()) {
+static auto set_poisson(World &world, const double lo, const double econv = FunctionDefaults<3>::get_thresh())
+{
     return std::shared_ptr<real_convolution_3d>(CoulombOperatorPtr(world, lo, econv));
 }
 
-
-auto initialize_calc_params(World &world, const std::string &input_file) -> CalcParams {
+auto initialize_calc_params(World &world, const std::string &input_file) -> CalcParams
+{
     ResponseParameters r_params{};
     commandlineparser parser;
     parser.set_keyval("input", input_file);
@@ -25,8 +25,9 @@ auto initialize_calc_params(World &world, const std::string &input_file) -> Calc
     return {ground_calculation, molecule, r_params};
 }
 // TODO some operator definitions that I will need to move to a separate file
-auto T(World &world, response_space &f) -> response_space {
-    response_space T;// Fock = (T + V) * orbitals
+auto T(World &world, response_space &f) -> response_space
+{
+    response_space T; // Fock = (T + V) * orbitals
     real_derivative_3d Dx(world, 0);
     real_derivative_3d Dy(world, 1);
     real_derivative_3d Dz(world, 2);
@@ -55,7 +56,8 @@ auto T(World &world, response_space &f) -> response_space {
  * @param f
  * @return
  */
-auto ground_exchange(const vecfuncT &phi0, const X_space &x, const bool compute_y) -> X_space {
+auto ground_exchange(const vecfuncT &phi0, const X_space &x, const bool compute_y) -> X_space
+{
     World &world = phi0[0].world();
     molresponse::start_timer(world);
     auto num_states = x.num_states();
@@ -64,11 +66,14 @@ auto ground_exchange(const vecfuncT &phi0, const X_space &x, const bool compute_
     long n{};
     response_matrix xx;
     // place all x and y functions into a single response vector
-    if (compute_y) {
+    if (compute_y)
+    {
         n = 2;
         xx = to_response_matrix(x);
         // place all x
-    } else {
+    }
+    else
+    {
         n = 1;
         xx = x.x.x;
         // if not compute y we are only working with the x functions
@@ -79,27 +84,32 @@ auto ground_exchange(const vecfuncT &phi0, const X_space &x, const bool compute_
     long b_index = 0;
     long p_index = 0;
     long p = 0;
-    for (long b = 0; b < num_states; b++) {
+    for (long b = 0; b < num_states; b++)
+    {
         b_index = b * num_orbitals * num_orbitals * n;
         p = 0;
         // for each function in a response vector copy num orbital times
-        std::for_each(xx[b].begin(), xx[b].end(), [&](const auto &xb_p) {
+        std::for_each(xx[b].begin(), xx[b].end(), [&](const auto &xb_p)
+                      {
             p_index = p * num_orbitals;
             for (long j = 0; j < num_orbitals; j++) { x_vector.at(b_index + p_index + j) = copy(xb_p, false); }
-            p++;
-        });
+            p++; });
     }
     vecfuncT phi1(n_exchange);
     vecfuncT phi2(n_exchange);
     int orb_i = 0;
     // copy ground-state orbitals into a single long vector
-    std::for_each(phi1.begin(), phi1.end(), [&](auto &phi_i) { phi_i = copy(phi0[orb_i++ % num_orbitals]); });
+    std::for_each(phi1.begin(), phi1.end(), [&](auto &phi_i)
+                  { phi_i = copy(phi0[orb_i++ % num_orbitals]); });
     world.gop.fence();
     phi2 = madness::copy(world, phi1);
     world.gop.fence();
     molresponse::end_timer(world, "ground exchange copy");
     auto K = molresponseExchange(world, phi1, phi2, x_vector, n, num_states, num_orbitals);
-    if (world.rank() == 0) { print("made it out of molresponseExchange"); }
+    if (world.rank() == 0)
+    {
+        print("made it out of molresponseExchange");
+    }
     return K;
 }
 // compute full response exchange |i><i|J|p>
@@ -114,7 +124,8 @@ auto ground_exchange(const vecfuncT &phi0, const X_space &x, const bool compute_
  * @param f
  * @return
  */
-auto response_exchange(const vecfuncT &phi0, const X_space &x, const bool compute_y) -> X_space {
+auto response_exchange(const vecfuncT &phi0, const X_space &x, const bool compute_y) -> X_space
+{
     World &world = phi0[0].world();
     molresponse::start_timer(world);
     auto num_states = x.num_states();
@@ -130,53 +141,68 @@ auto response_exchange(const vecfuncT &phi0, const X_space &x, const bool comput
     long p_index = 0;
     long p = 0;
     // 111 222 333 111 222 333 111 222 333 111 222 333
-    for (long b = 0; b < num_states; b++) {
+    for (long b = 0; b < num_states; b++)
+    {
         b_index = n * b * num_orbitals * num_orbitals;
         p = 0;
-        std::for_each(phi0.begin(), phi0.end(), [&](const auto &phi_p) {
+        std::for_each(phi0.begin(), phi0.end(), [&](const auto &phi_p)
+                      {
             p_index = num_orbitals * p;
             for (long j = 0; j < num_orbitals; j++) { phi_right.at(b_index + p_index + j) = copy(phi_p, false); }
-            p++;
-        });
-        if (compute_y) {
+            p++; });
+        if (compute_y)
+        {
             p = 0;
-            std::for_each(phi0.begin(), phi0.end(), [&](const auto &phi_p) {
+            std::for_each(phi0.begin(), phi0.end(), [&](const auto &phi_p)
+                          {
                 p_index = num_orbitals * p;
                 for (long j = 0; j < num_orbitals; j++) { phi_right.at(num_orbitals * num_orbitals + b_index + p_index + j) = copy(phi_p, false); }
-                p++;
-            });
+                p++; });
         }
     }
     world.gop.fence();
 
     vecfuncT x_vector(n_exchange);
     vecfuncT x_vector_conjugate(n_exchange);
-    for (long b = 0; b < num_states; b++) {// for each state copy the vector n times
+    for (long b = 0; b < num_states; b++)
+    { // for each state copy the vector n times
         b_index = b * num_orbitals * num_orbitals * n;
-        for (long j = 0; j < num_orbitals; j++) {
+        for (long j = 0; j < num_orbitals; j++)
+        {
             p_index = j * num_orbitals;
-            std::transform(x.x[b].begin(), x.x[b].end(), x_vector.begin() + b_index + p_index, [&](const auto &xbi) { return copy(xbi, false); });
+            std::transform(x.x[b].begin(), x.x[b].end(), x_vector.begin() + b_index + p_index, [&](const auto &xbi)
+                           { return copy(xbi, false); });
 
-            std::transform(x.y[b].begin(), x.y[b].end(), x_vector_conjugate.begin() + b_index + p_index, [&](const auto &xbi) { return copy(xbi, false); });
+            std::transform(x.y[b].begin(), x.y[b].end(), x_vector_conjugate.begin() + b_index + p_index, [&](const auto &xbi)
+                           { return copy(xbi, false); });
         }
-        if (compute_y) {
+        if (compute_y)
+        {
             long y_shift = num_orbitals * num_orbitals;
-            for (long j = 0; j < num_orbitals; j++) {
+            for (long j = 0; j < num_orbitals; j++)
+            {
                 p_index = j * num_orbitals;
-                std::transform(x.y[b].begin(), x.y[b].end(), x_vector.begin() + b_index + p_index + y_shift, [&](const auto &ybi) { return copy(ybi, false); });
+                std::transform(x.y[b].begin(), x.y[b].end(), x_vector.begin() + b_index + p_index + y_shift, [&](const auto &ybi)
+                               { return copy(ybi, false); });
                 std::transform(x.x[b].begin(), x.x[b].end(), x_vector_conjugate.begin() + b_index + p_index + y_shift,
-                               [&](const auto &ybi) { return copy(ybi, false); });
+                               [&](const auto &ybi)
+                               { return copy(ybi, false); });
             }
         }
     }
     world.gop.fence();
 
     vecfuncT phi_left(n_exchange);
-    for (long b = 0; b < num_states; b++) {
+    for (long b = 0; b < num_states; b++)
+    {
         b_index = b * num_orbitals * num_orbitals * n;
-        for (long p = 0; p < n * num_orbitals; p++) {
+        for (long p = 0; p < n * num_orbitals; p++)
+        {
             p_index = p * num_orbitals;
-            for (int i = 0; i < num_orbitals; i++) { phi_left[b_index + p_index + i] = copy(phi0[i], false); }
+            for (int i = 0; i < num_orbitals; i++)
+            {
+                phi_left[b_index + p_index + i] = copy(phi0[i], false);
+            }
         }
     }
     world.gop.fence();
@@ -199,7 +225,8 @@ auto response_exchange(const vecfuncT &phi0, const X_space &x, const bool comput
     return K;
 }
 // compute exchange |i><i|J|p>
-auto newK(const vecfuncT &ket, const vecfuncT &bra, const vecfuncT &vf) -> vecfuncT {
+auto newK(const vecfuncT &ket, const vecfuncT &bra, const vecfuncT &vf) -> vecfuncT
+{
     World &world = ket[0].world();
     const double lo = 1.e-10;
 
@@ -211,80 +238,125 @@ auto newK(const vecfuncT &ket, const vecfuncT &bra, const vecfuncT &vf) -> vecfu
 }
 // sum_i |i><i|J|p> for each p
 auto molresponseExchange(World &world, const vecfuncT &ket_i, const vecfuncT &bra_i, const vecfuncT &fp, const int &n, const int &num_states,
-                         const int &num_orbitals) -> X_space {
+                         const int &num_orbitals) -> X_space
+{
     molresponse::start_timer(world);
     reconstruct(world, ket_i, false);
     reconstruct(world, bra_i, false);
     reconstruct(world, fp, false);
-    if (world.rank() == 0) { print("exchange reconstruct tree"); }
+    if (world.rank() == 0)
+    {
+        print("exchange reconstruct tree");
+    }
     world.gop.fence();
     /*
     norm_tree(world, ket_i, false);
     norm_tree(world, bra_i, false);
     norm_tree(world, fp, false);
      */
-    if (world.rank() == 0) { print("exchange norm tree"); }
+    if (world.rank() == 0)
+    {
+        print("exchange norm tree");
+    }
     const double lo = 1.0e-10;
     auto tol = FunctionDefaults<3>::get_thresh();
     auto poisson = set_poisson(world, lo);
     world.gop.fence();
-    if (world.rank() == 0) { print("create poisson v23"); }
+    if (world.rank() == 0)
+    {
+        print("create poisson v23");
+    }
     auto v23 = mul(world, bra_i, fp, true);
-    //mul(world, bra_i, fp, true);
-    if (world.rank() == 0) { print("multiply v23"); }
+    // mul(world, bra_i, fp, true);
+    if (world.rank() == 0)
+    {
+        print("multiply v23");
+    }
     truncate(world, v23, tol, true);
     // truncate
-    if (world.rank() == 0) { print("truncate v23"); }
+    if (world.rank() == 0)
+    {
+        print("truncate v23");
+    }
     v23 = apply(world, *poisson, v23);
-    if (world.rank() == 0) { print("apply v23"); }
+    if (world.rank() == 0)
+    {
+        print("apply v23");
+    }
     truncate(world, v23, tol, true);
-    if (world.rank() == 0) { print("truncate after apply v23"); }
+    if (world.rank() == 0)
+    {
+        print("truncate after apply v23");
+    }
     auto v123 = mul(world, ket_i, v23, true);
-    if (world.rank() == 0) { print("multiply  apply v123"); }
+    if (world.rank() == 0)
+    {
+        print("multiply  apply v123");
+    }
     const long n_exchange{num_states * n * num_orbitals};
     auto exchange_vector = vecfuncT(n_exchange);
     long b = 0;
     long b_shift = 0;
     compress(world, v123);
-    for (auto &kij: exchange_vector) {
+    for (auto &kij : exchange_vector)
+    {
         b_shift = b * num_orbitals;
         auto phi_phiX_i = vecfuncT(num_orbitals);
         // this right here is a sketch of how sums with iterators could work
         kij = FunctionFactory<double, 3>(world).compressed();
-        std::for_each(v123.begin() + b_shift, v123.begin() + b_shift + num_orbitals, [&](const auto &v123_i) { kij.gaxpy(1.0, v123_i, 1.0, false); });
+        std::for_each(v123.begin() + b_shift, v123.begin() + b_shift + num_orbitals, [&](const auto &v123_i)
+                      { kij.gaxpy(1.0, v123_i, 1.0, false); });
         b++;
     }
     world.gop.fence();
-    if (world.rank() == 0) print("exchange sum");
+    if (world.rank() == 0)
+        print("exchange sum");
     truncate(world, exchange_vector, tol, true);
     molresponse::end_timer(world, "exchange apply");
 
     molresponse::start_timer(world);
     auto exchange_matrix = create_response_matrix(num_states, n * num_orbitals);
     b = 0;
-    for (auto &xi: exchange_matrix) {
-        for (auto &xij: xi) { xij = copy(exchange_vector[b++]); }
+    for (auto &xi : exchange_matrix)
+    {
+        for (auto &xij : xi)
+        {
+            xij = copy(exchange_vector[b++]);
+        }
     }
     X_space K0 = X_space::zero_functions(world, num_states, num_orbitals);
-    if (n == 2) {
+    if (n == 2)
+    {
         K0 = to_X_space(exchange_matrix);
-    } else {
+    }
+    else
+    {
         K0.x = exchange_matrix;
     }
     world.gop.fence();
     molresponse::end_timer(world, "ground exchange reorganize");
     return K0;
 }
-auto make_k(const vecfuncT &ket, const vecfuncT &bra, const ResponseParameters &r_params) {
+auto make_k(const vecfuncT &ket, const vecfuncT &bra, const ResponseParameters &r_params)
+{
     const double lo = 1.e-10;
     auto &world = ket[0].world();
     Exchange<double, 3> k{world, lo};
     k.set_bra_and_ket(bra, ket);
-    if (r_params.hfexalg() == "multiworld") {
+    if (r_params.hfexalg() == "multiworld")
+    {
         k.set_algorithm(madness::Exchange<double, 3>::multiworld_efficient);
-    } else if (r_params.hfexalg() == "smallmem") {
+    }
+    else if (r_params.hfexalg() == "multiworld_row")
+    {
+        k.set_algorithm(madness::Exchange<double, 3>::multiworld_efficient_row);
+    }
+    else if (r_params.hfexalg() == "smallmem")
+    {
         k.set_algorithm(madness::Exchange<double, 3>::small_memory);
-    } else if (r_params.hfexalg() == "largemem") {
+    }
+    else if (r_params.hfexalg() == "largemem")
+    {
         k.set_algorithm(madness::Exchange<double, 3>::large_memory);
     };
     return k;
@@ -300,7 +372,8 @@ auto make_k(const vecfuncT &ket, const vecfuncT &bra, const ResponseParameters &
  * @param f
  * @return
  */
-auto response_exchange(const vecfuncT &phi0, const X_space &chi, const bool &compute_y, const ResponseParameters &r_params) -> X_space {
+auto response_exchange(const vecfuncT &phi0, const X_space &chi, const bool &compute_y, const ResponseParameters &r_params) -> X_space
+{
     World &world = phi0[0].world();
     molresponse::start_timer(world);
     auto num_states = chi.num_states();
@@ -308,9 +381,10 @@ auto response_exchange(const vecfuncT &phi0, const X_space &chi, const bool &com
     auto K = X_space::zero_functions(world, num_states, num_orbitals);
     vector_real_function_3d k1x, k1y, k2x, k2y;
 
-
-    if (compute_y) {
-        for (const auto &b: chi.active) {
+    if (compute_y)
+    {
+        for (const auto &b : chi.active)
+        {
             auto x = chi.x[b];
             auto y = chi.y[b];
             auto K1X = make_k(x, phi0, r_params);
@@ -326,8 +400,11 @@ auto response_exchange(const vecfuncT &phi0, const X_space &chi, const bool &com
             K.y[b] = gaxpy_oop(1.0, k2x, 1.0, k2y, false);
             world.gop.fence();
         }
-    } else {
-        for (const auto &b: chi.active) {
+    }
+    else
+    {
+        for (const auto &b : chi.active)
+        {
             auto x = chi.x[b];
             auto y = chi.x[b];
             auto K1X = make_k(x, phi0, r_params);
@@ -352,7 +429,8 @@ auto response_exchange(const vecfuncT &phi0, const X_space &chi, const bool &com
  * @param f
  * @return
  */
-auto ground_exchange(const vecfuncT &phi0, const X_space &chi, const bool &compute_y, const ResponseParameters &r_params) -> X_space {
+auto ground_exchange(const vecfuncT &phi0, const X_space &chi, const bool &compute_y, const ResponseParameters &r_params) -> X_space
+{
     World &world = phi0[0].world();
     molresponse::start_timer(world);
 
@@ -364,16 +442,22 @@ auto ground_exchange(const vecfuncT &phi0, const X_space &chi, const bool &compu
     // the question is copying pointers mpi safe
     world.gop.fence();
     auto k0 = make_k(phi0, phi0, r_params);
-    if (compute_y) {
-        for (const auto &i: chi.active) {
+    if (compute_y)
+    {
+        for (const auto &i : chi.active)
+        {
             K0.x[i] = k0(chi.x[i]);
             K0.y[i] = k0(chi.y[i]);
         }
-    } else {
-        for (const auto &i: chi.active) { K0.x[i] = k0(chi.x[i]); }
+    }
+    else
+    {
+        for (const auto &i : chi.active)
+        {
+            K0.x[i] = k0(chi.x[i]);
+        }
         K0.y = K0.x.copy();
     }
     K0.truncate();
     return K0;
-}
-;
+};
