51c70885f368fcbdaac3db462b5e8d28eed4dbf7
Eduard Valeyev <eduard@valeyev.net>
2025-03-03 12:03:11 -0500

    is_madness_thread only used for Pthreads backend ops

--- src/madness/world/thread.cc
+++ src/madness/world/thread.cc
@@ -468,11 +468,10 @@ namespace madness {
     }
 
     void thread_purge() {
-      MADNESS_ASSERT(is_madness_thread());
-
 #if defined(HAVE_PARSEC)
       __parsec_schedule_flush_private(ThreadPool::instance()->parsec_runtime->execution_stream());
 #elif !(defined(HAVE_INTEL_TBB))
+      MADNESS_ASSERT(is_madness_thread());
       ThreadPool::instance()->flush_prebuf();
 #endif
     }
