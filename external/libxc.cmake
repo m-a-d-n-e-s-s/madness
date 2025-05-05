b11ef797c9abde0aee0c98a8f5b531591b01e624
Eduard Valeyev <eduard@valeyev.net>
2025-05-05 19:03:56 -0400

    seek/use CMake-built libxc (available since at least libxc 4) to help with subprojectability

--- external/libxc.cmake
+++ external/libxc.cmake
@@ -1,9 +1,9 @@
 if(ENABLE_LIBXC)
 
-  find_package(Libxc)
+  find_package(Libxc CONFIG)
     
   # Set the output variables
-  if(LIBXC_FOUND)
+  if(TARGET Libxc::xc)
     set(MADNESS_HAS_LIBXC 1)
   endif()
       
