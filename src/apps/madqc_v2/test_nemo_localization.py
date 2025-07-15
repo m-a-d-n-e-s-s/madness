c367c498c1667932440620fcc2a9cb6007448c27
fbischoff <florian.bischoff@hu-berlin.de>
2025-07-15 22:55:08 +0200

    adding/moving tests to madqc

--- src/apps/madqc_v2/test_nemo_localization.py
+++ src/apps/madqc_v2/test_nemo_localization.py
@@ -9,22 +9,24 @@ from madjsoncompare import madjsoncompare
 
 
 def localizer_run(localizer):
-    prefix='madtest'+localizer
+#    prefix='madtest'+localizer
 
-    # prefix='mad_@BINARY@_@TESTCASE@'
-    outputfile=prefix+'.calc_info.json'
+    prefix='mad_@BINARY@_@TESTCASE@'
+    outputfile=prefix+localizer+'.calc_info.json'
     referencefile="@SRCDIR@/"+prefix+".calc_info.ref.json"
 
 
-    cmd='rm '+outputfile+'; ./@BINARY@ --geometry=h2o --dft="maxiter=10; econv=3.e-5; k=8; localize='+localizer+'; dconv=1.e-3; prefix='+prefix+'"'
+    cmd='rm -r ' + outputfile + ' ' + prefix+localizer+'; ./@BINARY@ --geometry=h2o --wf=nemo --dft="maxiter=10; econv=1.e-5; k=8; localize='+localizer+'; dconv=1.e-3; prefix='+prefix+localizer+'"'
     print("executing \n ",cmd)
 #    output=subprocess.run(cmd,shell=True,capture_output=True, text=True).stdout
     p=subprocess.run(cmd,shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE , universal_newlines=True)
 
     print("finished with run1")
+    print(p.stdout)
+
 
     cmp=madjsoncompare(outputfile,referencefile)
-    cmp.compare(["return_energy"],1.e-4)
+    cmp.compare(["tasks",0,"scf_total_energy"],1.e-4)
     return cmp.success
 
 if __name__ == "__main__":
