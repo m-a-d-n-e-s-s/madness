21ecefe1f44499b3ba20ed80a3564e6bfe519ff3
fbischoff <florian.bischoff@hu-berlin.de>
2025-09-15 15:02:20 +0200

    fix compilation

--- src/madness/mra/test_vectormacrotask.cc
+++ src/madness/mra/test_vectormacrotask.cc
@@ -479,7 +479,7 @@ int test_task1(World& universe, const std::vector<real_function_3d>& v3) {
 
 int test_priority(World& universe, const std::vector<real_function_3d>& v3) {
     if (universe.rank()==0) print("\nstarting MicroTaskPriority\n");
-    auto taskq = std::shared_ptr<MacroTaskQ>(new MacroTaskQ(universe, universe.size()));
+    auto taskq = std::shared_ptr<MacroTaskQ>(new MacroTaskQ(universe, universe.size(), MacroTaskInfo::default_storage_policy));
     taskq->set_printlevel(3);
     MicroTaskPriority t1;
     real_function_3d ref_t1 = t1(v3[0], 2.0, v3);
