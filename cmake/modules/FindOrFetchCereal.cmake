b2e524b90b82cc63efe71f8484138d9dfa066df9
fbischoff <florian.bischoff@hu-berlin.de>
2025-09-18 18:17:48 +0200

    bump up cereal version to 1.3.2 to comply with cmake minimal version

--- cmake/modules/FindOrFetchCereal.cmake
+++ cmake/modules/FindOrFetchCereal.cmake
@@ -38,7 +38,7 @@ if (NOT TARGET cereal)
             FetchContent_Declare(
                     cereal
                     GIT_REPOSITORY https://github.com/USCiLab/cereal.git
-                    GIT_TAG v1.3.0)
+                    GIT_TAG v1.3.2)
 
             # configure cereal
             set(JUST_INSTALL_CEREAL ON CACHE BOOL "")
