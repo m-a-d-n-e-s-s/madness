d9533208ed65096e0f8d6368e6ca1d3948f24ae7
ahurta92 <ahurta92@gmail.com>
2025-10-27 11:34:55 -0400

    formatting

--- src/apps/molresponse_v2/ResponseSolver.cpp
+++ src/apps/molresponse_v2/ResponseSolver.cpp
@@ -8,13 +8,14 @@
 #include "functypedefs.h"
 #include "projector.h"
 
+const double restricted_density_factor = 2.0;
+
 // Compute density function
 real_function_3d compute_density(World &world,
                                  const StaticRestrictedResponse &rvec,
                                  const vector_real_function_3d &phi0) {
-  auto &x = rvec.x_alpha;
-  auto xphi = mul(world, x, phi0, true);
-  return 2.0 * sum(world, xphi, true);
+  auto xphi = mul(world, rvec.x_alpha, phi0, true);
+  return restricted_density_factor * sum(world, xphi, true);
 }
 
 real_function_3d compute_density(World &world,
@@ -22,77 +23,79 @@ real_function_3d compute_density(World &world,
                                  const vector_real_function_3d &phi0) {
   auto phi_phi = phi0;
   phi_phi.insert(phi_phi.end(), phi0.begin(), phi0.end());
-  auto &x = rvec.flat;
 
-  auto xphi = mul(world, x, phi_phi, true);
-  return 2.0 * sum(world, xphi, true);
+  auto xphi = mul(world, rvec.flat, phi_phi, true);
+  return restricted_density_factor * sum(world, xphi, true);
 }
 
 real_function_3d compute_density(World &world,
                                  const StaticUnrestrictedResponse &rvec,
                                  const vector_real_function_3d &phi0) {
-  auto phi_phi = phi0;
-  phi_phi.insert(phi_phi.end(), phi0.begin(), phi0.end());
-  auto &x = rvec.flat;
-
-  auto xphi = mul(world, x, phi_phi, true);
-  return 2.0 * sum(world, xphi, true);
+  throw std::runtime_error(
+      "compute_density for StaticUnrestrictedResponse not implemented");
+  // auto phi_phi = phi0;
+  // phi_phi.insert(phi_phi.end(), phi0.begin(), phi0.end());
+  // auto xphi = mul(world, rvec.flat, phi_phi, true);
+  // return 2.0 * sum(world, xphi, true);
 }
 
 real_function_3d compute_density(World &world,
-                                 const DynamicUnrestrictedResponse &rvec,
+                                 const DynamicUnrestrictedResponse &rvec, 
                                  const vector_real_function_3d &phi0) {
-  auto phi_phi = phi0;
-  phi_phi.insert(phi_phi.end(), phi0.begin(), phi0.end());
-  auto &x = rvec.flat;
-
-  auto xphi = mul(world, x, phi_phi, true);
-  return 2.0 * sum(world, xphi, true);
+  throw std::runtime_error(
+      "compute_density for DynamicUnrestrictedResponse not implemented");
+  // auto phi_phi = phi0;
+  // phi_phi.insert(phi_phi.end(), phi0.begin(), phi0.end());
+  // auto &x = rvec.flat;
+  //
+  // auto xphi = mul(world, x, phi_phi, true);
 }
 
 std::vector<poperatorT>
-make_bsh_operators(World &world, const ResponseManager &rm, const double freq,
-                   const Tensor<double> &orbital_energies, const int n,
-                   ResponseDebugLogger &logger,
+make_bsh_operators(World &world, const ResponseManager &response_manager,
+                   const double freq, const Tensor<double> &orbital_energies,
+                   const int n, ResponseDebugLogger &logger,
                    const StaticRestrictedResponse & /* vecs */) {
   auto bsh_x = std::vector<poperatorT>(n);
   double x_shifts = 0.0;
+  const double shift_factor = 0.05;
   if ((orbital_energies[static_cast<long>(n) - 1] + freq) >= 0.0) {
-    x_shifts = -.05 - (freq + orbital_energies[static_cast<long>(n) - 1]);
+    x_shifts =
+        -shift_factor - (freq + orbital_energies[static_cast<long>(n) - 1]);
   }
   bsh_x = ResponseSolverUtils::make_bsh_operators_response(
-      world, x_shifts, freq, orbital_energies, rm.params().lo());
+      world, x_shifts, freq, orbital_energies, response_manager.params().lo());
   return bsh_x;
 }
 
 std::vector<poperatorT>
-make_bsh_operators(World &world, const ResponseManager &rm, const double freq,
-                   const Tensor<double> &orbital_energies, const int n,
-                   ResponseDebugLogger &logger,
+make_bsh_operators(World &world, const ResponseManager &response_manager,
+                   const double freq, const Tensor<double> &orbital_energies,
+                   const int n, ResponseDebugLogger &logger,
                    const DynamicRestrictedResponse & /* vecs */) {
   auto bsh_x = std::vector<poperatorT>(2 * n);
   double x_shifts = 0.0;
+  const double shift_factor = 0.05;
   if ((orbital_energies[static_cast<long>(n) - 1] + freq) >= 0.0) {
-    x_shifts = -.05 - (freq + orbital_energies[static_cast<long>(n) - 1]);
+    x_shifts =
+        -shift_factor - (freq + orbital_energies[static_cast<long>(n) - 1]);
   }
   bsh_x = ResponseSolverUtils::make_bsh_operators_response(
-      world, x_shifts, freq, orbital_energies, rm.params().lo());
+      world, x_shifts, freq, orbital_energies, response_manager.params().lo());
   auto bsh_y = ResponseSolverUtils::make_bsh_operators_response(
-      world, 0.0, -freq, orbital_energies, rm.params().lo());
+      world, 0.0, -freq, orbital_energies, response_manager.params().lo());
 
   bsh_x.insert(bsh_x.end(), bsh_y.begin(), bsh_y.end());
   return bsh_x;
 }
 
 vector_real_function_3d CoupledResponseEquations(
-    World &world, const GroundStateData &gs,
-    const StaticRestrictedResponse &vecs, const vector_real_function_3d &vp,
-    const std::vector<poperatorT> &bsh_x, const ResponseManager &rm,
-    ResponseDebugLogger &logger) {
-  const auto &x = vecs.x_alpha;
-  const auto &all_x = vecs.flat;
+    World &world, const GroundStateData &g_s,
+    const StaticRestrictedResponse &vecs, const vector_real_function_3d &v_p,
+    const std::vector<poperatorT> &bsh_x,
+    const ResponseManager &response_manager, ResponseDebugLogger &logger) {
 
-  auto num_orbitals = gs.orbitals.size();
+  auto num_orbitals = g_s.orbitals.size();
 
   std::vector<int> state_index;
   std::vector<int> ii;
@@ -108,38 +111,38 @@ vector_real_function_3d CoupledResponseEquations(
   ResponseComputeGammaX tresponse;
   MacroTask gx_task(world, tresponse);
 
-  auto c_xc = gs.xcf_.hf_exchange_coefficient();
-  vector_real_function_3d k0;
-  vector_real_function_3d gx;
+  auto c_xc = g_s.xcf_.hf_exchange_coefficient();
+  vector_real_function_3d k_0;
+  vector_real_function_3d g_x;
 
   DEBUG_TIMED_BLOCK(world, &logger, "g0_task", {
-    k0 = g0_task(ii, state_index, all_x, gs.orbitals, true);
+    k_0 = g0_task(ii, state_index, vecs.flat, g_s.orbitals, true);
   });
   DEBUG_TIMED_BLOCK(world, &logger, "gx_task", {
-    gx = gx_task(ii, state_index, all_x, gs.orbitals, true);
+    g_x = gx_task(ii, state_index, vecs.flat, g_s.orbitals, true);
   });
 
-  auto v_local = gs.V_local * x;
-  auto v0x = v_local - c_xc * k0;
-  auto epsilonx = transform(world, x, gs.Hamiltonian_no_diag, true);
+  auto v_local = g_s.V_local * vecs.x_alpha;
+  auto v0x = v_local - c_xc * k_0;
+  auto epsilonx = transform(world, vecs.x_alpha, g_s.Hamiltonian_no_diag, true);
 
-  auto thetax = -2.0 * (v0x - epsilonx + gx + vp);
+  auto thetax = -2.0 * (v0x - epsilonx + g_x + v_p);
   truncate(world, thetax);
   auto rsh = apply(world, bsh_x, thetax);
-  rsh = gs.Qhat(rsh); // project out the ground state density from the response
+  rsh = g_s.Qhat(rsh); // project out the ground state density from the response
   return rsh;
 }
 
 vector_real_function_3d CoupledResponseEquations(
-    World &world, const GroundStateData &gs,
-    const DynamicRestrictedResponse &vecs, const vector_real_function_3d &vp,
-    const std::vector<poperatorT> &bsh_x, const ResponseManager &rm,
-    ResponseDebugLogger &logger) {
-  auto &x = vecs.x_alpha;
-  auto &y = vecs.y_alpha;
-  auto &all_x = vecs.flat;
+    World &world, const GroundStateData &g_s,
+    const DynamicRestrictedResponse &vecs, const vector_real_function_3d &v_p,
+    const std::vector<poperatorT> &bsh_x,
+    const ResponseManager &response_manager, ResponseDebugLogger &logger) {
+  const auto &xvec = vecs.x_alpha;
+  const auto &yvec = vecs.y_alpha;
+  const auto &all_x = vecs.flat;
 
-  auto num_orbitals = gs.orbitals.size();
+  auto num_orbitals = g_s.orbitals.size();
 
   std::vector<int> state_index;
   std::vector<int> ii;
@@ -155,27 +158,27 @@ vector_real_function_3d CoupledResponseEquations(
   ResponseComputeGammaX tresponse;
   MacroTask gx_task(world, tresponse);
 
-  vector_real_function_3d k0;
-  vector_real_function_3d gx;
+  vector_real_function_3d k_0;
+  vector_real_function_3d g_x;
 
   DEBUG_TIMED_BLOCK(world, &logger, "g0_task", {
-    k0 = g0_task(ii, state_index, all_x, gs.orbitals, false);
+    k_0 = g0_task(ii, state_index, all_x, g_s.orbitals, false);
   });
   DEBUG_TIMED_BLOCK(world, &logger, "gx_task", {
-    gx = gx_task(ii, state_index, all_x, gs.orbitals, false);
+    g_x = gx_task(ii, state_index, all_x, g_s.orbitals, false);
   });
 
-  auto c_xc = gs.xcf_.hf_exchange_coefficient();
-  auto v_local = gs.V_local * all_x;
-  auto v0x = v_local - c_xc * k0;
-  auto epsilonx = transform(world, x, gs.Hamiltonian_no_diag, true);
-  auto epsilony = transform(world, y, gs.Hamiltonian_no_diag, true);
+  auto c_xc = g_s.xcf_.hf_exchange_coefficient();
+  auto v_local = g_s.V_local * all_x;
+  auto v0x = v_local - c_xc * k_0;
+  auto epsilonx = transform(world, xvec, g_s.Hamiltonian_no_diag, true);
+  auto epsilony = transform(world, yvec, g_s.Hamiltonian_no_diag, true);
   epsilonx.insert(epsilonx.end(), epsilony.begin(), epsilony.end());
-  auto thetax = -2.0 * (v0x - epsilonx + gx + vp);
+  auto thetax = -2.0 * (v0x - epsilonx + g_x + v_p);
   truncate(world, thetax);
   auto rsh = apply(world, bsh_x, thetax);
   truncate(world, thetax);
-  rsh = gs.Qhat(rsh);
+  rsh = g_s.Qhat(rsh);
   return rsh;
 }
 
